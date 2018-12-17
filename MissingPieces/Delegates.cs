9e5c5dac7714c7a54c7eab597e66dc9bb1545b21
Roman Sakno <riman.sakno@bytex.solutions>
2018-12-17 15:17:34 +0200

    Introduce invoke async extensions for delegates

--- MissingPieces/Delegates.cs
+++ MissingPieces/Delegates.cs
@@ -1,10 +1,60 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Reflection;
+using System.Threading.Tasks;
 
 namespace MissingPieces
 {
 	public static class Delegates
 	{
+		public static Task InvokeAsync<E>(this EventHandler<E> handler, object sender, E args, bool parallel = true)
+		{
+			if (handler == null)
+				return Task.CompletedTask;
+			else if (parallel)
+			{
+				var handlers = handler?.GetInvocationList() ?? Array.Empty<EventHandler<E>>();
+				switch (handlers.LongLength)
+				{
+					case 0:
+						return Task.CompletedTask;
+					case 1:
+						return handler.InvokeAsync(sender, args, false);
+					default:
+						var tasks = new LinkedList<Task>();
+						foreach (EventHandler<E> h in handlers)
+							tasks.AddLast(h.InvokeAsync(sender, args, false));
+						return Task.WhenAll(tasks);
+				}
+			}
+			else
+				return Task.Factory.StartNew(() => handler(sender, args));
+		}
+
+		public static Task InvokeAsync(this EventHandler handler, object sender, EventArgs args, bool parallel = true)
+		{
+			if (handler == null)
+				return Task.CompletedTask;
+			else if (parallel)
+			{
+				var handlers = handler?.GetInvocationList() ?? Array.Empty<EventHandler>();
+				switch (handlers.LongLength)
+				{
+					case 0:
+						return Task.CompletedTask;
+					case 1:
+						return handler.InvokeAsync(sender, args, false);
+					default:
+						var tasks = new LinkedList<Task>();
+						foreach (EventHandler h in handlers)
+							tasks.AddLast(h.InvokeAsync(sender, args, false));
+						return Task.WhenAll(tasks);
+				}
+			}
+			else
+				return Task.Factory.StartNew(() => handler(sender, args));
+		}
+
 		public static EventHandler<O> Contravariant<I, O>(this EventHandler<I> handler)
 			where I : class
 			where O : class, I
