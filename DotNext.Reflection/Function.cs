42480c424533eec0f2981e67f53cbc436c5afc24
Roman Sakno <roman.sakno@gmail.com>
2019-01-13 21:36:17 +0200

    Extension methods for Function and Procedure

--- DotNext.Reflection/Function.cs
+++ DotNext.Reflection/Function.cs
@@ -41,5 +41,58 @@ namespace DotNext
         public static R Invoke<T, P, R>(this Function<T, ValueTuple<P>, R> function, in T instance, P arg)
             => function(in instance, new ValueTuple<P>(arg));
         
+        public static R Invoke<P1, P2, R>(this Function<(P1, P2), R> function, P1 arg1, P2 arg2)
+            => function((arg1, arg2));
+
+        public static R Invoke<T, P1, P2, R>(this Function<T, (P1, P2), R> function, in T instance, P1 arg1, P2 arg2)
+            => function(in instance, (arg1, arg2));
+        
+        public static R Invoke<T, P1, P2, P3, R>(this Function<T, (P1, P2, P3), R> function, in T instance, P1 arg1, P2 arg2, P3 arg3)
+            => function(in instance, (arg1, arg2, arg3));
+
+        public static R Invoke<P1, P2, P3, R>(this Function<(P1, P2, P3), R> function, P1 arg1, P2 arg2, P3 arg3)
+            => function((arg1, arg2, arg3));
+        
+        public static R Invoke<T, P1, P2, P3, P4, R>(this Function<T, (P1, P2, P3, P4), R> function, in T instance, P1 arg1, P2 arg2, P3 arg3, P4 arg4)
+            => function(in instance, (arg1, arg2, arg3, arg4));
+
+        public static R Invoke<P1, P2, P3, P4, R>(this Function<(P1, P2, P3, P4), R> function, P1 arg1, P2 arg2, P3 arg3, P4 arg4)
+            => function((arg1, arg2, arg3, arg4));
+        
+        public static R Invoke<T, P1, P2, P3, P4, P5, R>(this Function<T, (P1, P2, P3, P4, P5), R> function, in T instance, P1 arg1, P2 arg2, P3 arg3, P4 arg4, P5 arg5)
+            => function(in instance, (arg1, arg2, arg3, arg4, arg5));
+        
+        public static R Invoke<P1, P2, P3, P4, P5, R>(this Function<(P1, P2, P3, P4, P5), R> function, P1 arg1, P2 arg2, P3 arg3, P4 arg4, P5 arg5)
+            => function((arg1, arg2, arg3, arg4, arg5));
+        
+        public static R Invoke<T, P1, P2, P3, P4, P5, P6, R>(this Function<T, (P1, P2, P3, P4, P5, P6), R> function, in T instance, P1 arg1, P2 arg2, P3 arg3, P4 arg4, P5 arg5, P6 arg6)
+            => function(in instance, (arg1, arg2, arg3, arg4, arg5, arg6));
+
+        public static R Invoke<P1, P2, P3, P4, P5, P6, R>(this Function<(P1, P2, P3, P4, P5, P6), R> function, P1 arg1, P2 arg2, P3 arg3, P4 arg4, P5 arg5, P6 arg6)
+            => function((arg1, arg2, arg3, arg4, arg5, arg6));
+        
+        public static R Invoke<T, P1, P2, P3, P4, P5, P6, P7, R>(this Function<T, (P1, P2, P3, P4, P5, P6, P7), R> function, in T instance, P1 arg1, P2 arg2, P3 arg3, P4 arg4, P5 arg5, P6 arg6, P7 arg7)
+            => function(in instance, (arg1, arg2, arg3, arg4, arg5, arg6, arg7));
+        
+        public static R Invoke<P1, P2, P3, P4, P5, P6, P7, R>(this Function<(P1, P2, P3, P4, P5, P6, P7), R> function, P1 arg1, P2 arg2, P3 arg3, P4 arg4, P5 arg5, P6 arg6, P7 arg7)
+            => function((arg1, arg2, arg3, arg4, arg5, arg6, arg7));
+        
+        public static R Invoke<T, P1, P2, P3, P4, P5, P6, P7, P8, R>(this Function<T, (P1, P2, P3, P4, P5, P6, P7, P8), R> function, in T instance, P1 arg1, P2 arg2, P3 arg3, P4 arg4, P5 arg5, P6 arg6, P7 arg7, P8 arg8)
+            => function(in instance, (arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8));
+        
+        public static R Invoke<P1, P2, P3, P4, P5, P6, P7, P8, R>(this Function<(P1, P2, P3, P4, P5, P6, P7, P8), R> function, P1 arg1, P2 arg2, P3 arg3, P4 arg4, P5 arg5, P6 arg6, P7 arg7, P8 arg8)
+            => function((arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8));
+        
+        public static R Invoke<T, P1, P2, P3, P4, P5, P6, P7, P8, P9, R>(this Function<T, (P1, P2, P3, P4, P5, P6, P7, P8, P9), R> function, in T instance, P1 arg1, P2 arg2, P3 arg3, P4 arg4, P5 arg5, P6 arg6, P7 arg7, P8 arg8, P9 arg9)
+            => function(in instance, (arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9));
+        
+        public static R Invoke<P1, P2, P3, P4, P5, P6, P7, P8, P9, R>(this Function<(P1, P2, P3, P4, P5, P6, P7, P8, P9), R> function, P1 arg1, P2 arg2, P3 arg3, P4 arg4, P5 arg5, P6 arg6, P7 arg7, P8 arg8, P9 arg9)
+            => function((arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9));
+        
+        public static R Invoke<T, P1, P2, P3, P4, P5, P6, P7, P8, P9, P10, R>(this Function<T, (P1, P2, P3, P4, P5, P6, P7, P8, P9, P10), R> function, in T instance, P1 arg1, P2 arg2, P3 arg3, P4 arg4, P5 arg5, P6 arg6, P7 arg7, P8 arg8, P9 arg9, P10 arg10)
+            => function(in instance, (arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, arg10));
+        
+        public static R Invoke<P1, P2, P3, P4, P5, P6, P7, P8, P9, P10, R>(this Function<(P1, P2, P3, P4, P5, P6, P7, P8, P9, P10), R> function, P1 arg1, P2 arg2, P3 arg3, P4 arg4, P5 arg5, P6 arg6, P7 arg7, P8 arg8, P9 arg9, P10 arg10)
+            => function((arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, arg10));
     }
 }
--- DotNext.Reflection/Procedure.cs
+++ DotNext.Reflection/Procedure.cs
@@ -1,4 +1,6 @@
-﻿namespace DotNext
+﻿using System;
+
+namespace DotNext
 {
 	/// <summary>
 	/// Represents a static procedure with arbitrary number of arguments
@@ -13,7 +15,7 @@
 	/// Represents an instance procedure with arbitrary number of arguments
 	/// allocated on the stack.
 	/// </summary>
-	/// <param name="this">Hidden This parameter.</param>
+	/// <param name="this">Hidden <see langword="this"/> parameter.</param>
 	/// <param name="arguments">Procedure arguments in the form of public structure fields.</param>
 	/// <typeparam name="T">Type of instance to be passed into underlying method.</typeparam>
 	/// <typeparam name="A">Type of structure with procedure arguments allocated on the stack.</typeparam>
@@ -28,5 +30,65 @@
         public static A ArgList<T, A>(this Procedure<T, A> procedure)
             where A: struct
             => new A();
+		
+		public static void Invoke<T, P>(this Procedure<T, ValueTuple<P>> procedure, in T instance, P arg)
+			=> procedure(in instance, new ValueTuple<P>(arg));
+		
+		public static void Invoke<P>(this Procedure<ValueTuple<P>> procedure, P arg)
+			=> procedure(new ValueTuple<P>(arg));
+		
+		public static void Invoke<T, P1, P2>(this Procedure<T, (P1, P2)> procedure, in T instance, P1 arg1, P2 arg2)
+			=> procedure(in instance, (arg1, arg2));
+		
+		public static void Invoke<P1, P2>(this Procedure<(P1, P2)> procedure, P1 arg1, P2 arg2)
+			=> procedure((arg1, arg2));
+		
+		public static void Invoke<T, P1, P2, P3>(this Procedure<T, (P1, P2, P3)> procedure, in T instance, P1 arg1, P2 arg2, P3 arg3)
+			=> procedure(in instance, (arg1, arg2, arg3));
+		
+		public static void Invoke<P1, P2, P3>(this Procedure<(P1, P2, P3)> procedure, P1 arg1, P2 arg2, P3 arg3)
+			=> procedure((arg1, arg2, arg3));
+		
+		public static void Invoke<T, P1, P2, P3, P4>(this Procedure<T, (P1, P2, P3, P4)> procedure, in T instance, P1 arg1, P2 arg2, P3 arg3, P4 arg4)
+			=> procedure(in instance, (arg1, arg2, arg3, arg4));
+		
+		public static void Invoke<P1, P2, P3, P4>(this Procedure<(P1, P2, P3, P4)> procedure, P1 arg1, P2 arg2, P3 arg3, P4 arg4)
+			=> procedure((arg1, arg2, arg3, arg4));
+		
+		public static void Invoke<T, P1, P2, P3, P4, P5>(this Procedure<T, (P1, P2, P3, P4, P5)> procedure, in T instance, P1 arg1, P2 arg2, P3 arg3, P4 arg4, P5 arg5)
+			=> procedure(in instance, (arg1, arg2, arg3, arg4, arg5));
+		
+		public static void Invoke<P1, P2, P3, P4, P5>(this Procedure<(P1, P2, P3, P4, P5)> procedure, P1 arg1, P2 arg2, P3 arg3, P4 arg4, P5 arg5)
+			=> procedure((arg1, arg2, arg3, arg4, arg5));
+		
+		public static void Invoke<T, P1, P2, P3, P4, P5, P6>(this Procedure<T, (P1, P2, P3, P4, P5, P6)> procedure, in T instance, P1 arg1, P2 arg2, P3 arg3, P4 arg4, P5 arg5, P6 arg6)
+			=> procedure(in instance, (arg1, arg2, arg3, arg4, arg5, arg6));
+		
+		public static void Invoke<P1, P2, P3, P4, P5, P6>(this Procedure<(P1, P2, P3, P4, P5, P6)> procedure, P1 arg1, P2 arg2, P3 arg3, P4 arg4, P5 arg5, P6 arg6)
+			=> procedure((arg1, arg2, arg3, arg4, arg5, arg6));
+		
+		public static void Invoke<T, P1, P2, P3, P4, P5, P6, P7>(this Procedure<T, (P1, P2, P3, P4, P5, P6, P7)> procedure, in T instance, P1 arg1, P2 arg2, P3 arg3, P4 arg4, P5 arg5, P6 arg6, P7 arg7)
+			=> procedure(in instance, (arg1, arg2, arg3, arg4, arg5, arg6, arg7));
+		
+		public static void Invoke<P1, P2, P3, P4, P5, P6, P7>(this Procedure<(P1, P2, P3, P4, P5, P6, P7)> procedure, P1 arg1, P2 arg2, P3 arg3, P4 arg4, P5 arg5, P6 arg6, P7 arg7)
+			=> procedure((arg1, arg2, arg3, arg4, arg5, arg6, arg7));
+		
+		public static void Invoke<T, P1, P2, P3, P4, P5, P6, P7, P8>(this Procedure<T, (P1, P2, P3, P4, P5, P6, P7, P8)> procedure, in T instance, P1 arg1, P2 arg2, P3 arg3, P4 arg4, P5 arg5, P6 arg6, P7 arg7, P8 arg8)
+			=> procedure(in instance, (arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8));
+		
+		public static void Invoke<P1, P2, P3, P4, P5, P6, P7, P8>(this Procedure<(P1, P2, P3, P4, P5, P6, P7, P8)> procedure, P1 arg1, P2 arg2, P3 arg3, P4 arg4, P5 arg5, P6 arg6, P7 arg7, P8 arg8)
+			=> procedure((arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8));
+		
+		public static void Invoke<T, P1, P2, P3, P4, P5, P6, P7, P8, P9>(this Procedure<T, (P1, P2, P3, P4, P5, P6, P7, P8, P9)> procedure, in T instance, P1 arg1, P2 arg2, P3 arg3, P4 arg4, P5 arg5, P6 arg6, P7 arg7, P8 arg8, P9 arg9)
+			=> procedure(in instance, (arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9));
+		
+		public static void Invoke<P1, P2, P3, P4, P5, P6, P7, P8, P9>(this Procedure<(P1, P2, P3, P4, P5, P6, P7, P8, P9)> procedure, P1 arg1, P2 arg2, P3 arg3, P4 arg4, P5 arg5, P6 arg6, P7 arg7, P8 arg8, P9 arg9)
+			=> procedure((arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9));
+		
+		public static void Invoke<T, P1, P2, P3, P4, P5, P6, P7, P8, P9, P10>(this Procedure<T, (P1, P2, P3, P4, P5, P6, P7, P8, P9, P10)> procedure, in T instance, P1 arg1, P2 arg2, P3 arg3, P4 arg4, P5 arg5, P6 arg6, P7 arg7, P8 arg8, P9 arg9, P10 arg10)
+			=> procedure(in instance, (arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, arg10));
+		
+		public static void Invoke<P1, P2, P3, P4, P5, P6, P7, P8, P9, P10>(this Procedure<(P1, P2, P3, P4, P5, P6, P7, P8, P9, P10)> procedure, P1 arg1, P2 arg2, P3 arg3, P4 arg4, P5 arg5, P6 arg6, P7 arg7, P8 arg8, P9 arg9, P10 arg10)
+			=> procedure((arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, arg10));
 	}
 }
