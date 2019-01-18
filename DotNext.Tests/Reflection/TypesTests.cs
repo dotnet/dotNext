f0078867c41674ec378a62a14bd2237f507df210
Roman Sakno <riman.sakno@bytex.solutions>
2019-01-18 10:58:59 +0200

    More tests for reflection methods

--- DotNext.Tests/Reflection/TypesTests.cs
+++ DotNext.Tests/Reflection/TypesTests.cs
@@ -1,10 +1,16 @@
 using System;
+using System.Collections.Generic;
 using Xunit;
 
 namespace DotNext.Reflection
 {
 	public sealed class TypesTest : Assert
 	{
+        public sealed class MyList: List<string>
+        {
+
+        }
+
 		[Fact]
 		public void DelegateSignatureTest()
 		{
@@ -13,5 +19,21 @@ namespace DotNext.Reflection
 			Equal(typeof(int), signature.GetParameters()[0].ParameterType);
 			Equal(typeof(string), signature.ReturnParameter.ParameterType);
 		}
+
+        [Fact]
+        public void IsGenericInstanceOfTest()
+        {
+            True(typeof(Func<string>).IsGenericInstanceOf(typeof(Func<>)));
+            False(typeof(Func<string>).IsGenericInstanceOf(typeof(Func<int>)));
+            True(typeof(List<int>).IsGenericInstanceOf(typeof(List<>)));
+            True(typeof(MyList).IsGenericInstanceOf(typeof(IEnumerable<>)));
+        }
+
+        [Fact]
+        public void CollectionElementTest()
+        {
+            Equal(typeof(string), typeof(MyList).GetCollectionElementType(out var enumerable));
+            Equal(typeof(IEnumerable<string>), enumerable);
+        }
 	}
 }
