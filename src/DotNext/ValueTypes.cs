1b637d5f122dc4a38ae3313f1df5b4374ba13584
Roman Sakno <riman.sakno@bytex.solutions>
2019-02-11 12:52:05 +0200

    Improve bitcast performance

--- src/DotNext/ValueTypes.cs
+++ src/DotNext/ValueTypes.cs
@@ -3,37 +3,40 @@ using System.Runtime.CompilerServices;
 
 namespace DotNext
 {
-	using Runtime.InteropServices;
-
 	/// <summary>
 	/// Various extensions for value types.
 	/// </summary>
     public static class ValueTypes
     {
-		/// <summary>
-		/// Converts one structure into another without changing any bits.
-		/// </summary>
-		/// <param name="input">A value to convert.</param>
-		/// <typeparam name="I">Type of input struct.</typeparam>
-		/// <typeparam name="O"></typeparam>
-		/// <returns></returns>
-		[MethodImpl(MethodImplOptions.AggressiveInlining)]
-		public static unsafe O BitCast<I, O>(this I input)
-			where I : unmanaged
-			where O : unmanaged
-		{
-			if(ValueType<I>.Size >= ValueType<O>.Size)
-                return Unsafe.As<I, O>(ref input);
-            var output = new O();
-			Memory.Copy(&input, &output, ValueType<I>.Size);
-			return output;
-		}
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void BitCast<I, O>(this I input, out O output)
+            where I : unmanaged
+            where O : unmanaged
+        {
+            if (ValueType<I>.Size >= ValueType<O>.Size)
+                output = Unsafe.As<I, O>(ref input);
+            else
+            {
+                output = new O();
+                Unsafe.As<O, I>(ref output) = input;
+            }
+        }
 
-		[MethodImpl(MethodImplOptions.AggressiveInlining)]
-		public static unsafe void BitCast<I, O>(this I input, out O output)
-			where I: unmanaged
-			where O: unmanaged
-			=> output = BitCast<I, O>(input);
+        /// <summary>
+        /// Converts one structure into another without changing any bits.
+        /// </summary>
+        /// <param name="input">A value to convert.</param>
+        /// <typeparam name="I">Type of input struct.</typeparam>
+        /// <typeparam name="O"></typeparam>
+        /// <returns></returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static O BitCast<I, O>(this I input)
+            where I : unmanaged
+            where O : unmanaged
+        {
+            input.BitCast(out O output);
+            return output;
+        }
 
         public static bool OneOf<T>(this T value, params T[] values)
 			where T: struct, IEquatable<T>
