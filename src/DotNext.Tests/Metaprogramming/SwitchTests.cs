d53b82031cdee5e835c7c157cf852c8cb74620b0
Roman Sakno <roman.sakno@gmail.com>
2019-04-22 23:25:27 +0300

    Add test for switching over string values

--- src/DotNext.Tests/Metaprogramming/SwitchTests.cs
+++ src/DotNext.Tests/Metaprogramming/SwitchTests.cs
@@ -6,7 +6,7 @@ namespace DotNext.Metaprogramming
     public sealed class SwitchTests: Assert
     {
         [Fact]
-        public void IntConversionTest()
+        public static void IntConversion()
         {
             var lambda = LambdaBuilder<Func<int, string>>.Build(fun =>
             {
@@ -21,6 +21,24 @@ namespace DotNext.Metaprogramming
             Equal("Zero", lambda(0));
             Equal("One", lambda(1));
             Equal("Unknown", lambda(3));
+        }
+
+        [Fact]
+        public static void SwitchOverString()
+        {
+            var lambda = LambdaBuilder<Func<string, int>>.Build(fun =>
+            {
+                fun.Switch(fun.Parameters[0])
+                    .Case("Zero", 0)
+                    .Case("One", 1)
+                    .Default(int.MaxValue)
+                    .OfType<int>()
+                    .End();
+            })
+            .Compile();
+            Equal(0, lambda("Zero"));
+            Equal(1, lambda("One"));
+            Equal(int.MaxValue, lambda("Unknown"));
         }
     }
 }
