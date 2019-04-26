fd8da1d320df9104ced4555aaa4bc1fef9e8c9ba
Roman Sakno <roman.sakno@gmail.com>
2019-04-26 23:56:28 +0300

    EC builder is now a struct

--- src/DotNext.Tests/EqualityComparerBuilderTests.cs
+++ src/DotNext.Tests/EqualityComparerBuilderTests.cs
@@ -50,8 +50,7 @@ namespace DotNext
         [Fact]
         public static void ClassWithInnerArrayComparer()
         {
-            var hashCode = EqualityComparerBuilder.BuildGetHashCode<ComplexClass>();
-            var equality = EqualityComparerBuilder.BuildEquals<ComplexClass>();
+            new EqualityComparerBuilder<ComplexClass>() { SaltedHashCode = true }.Build(out var equality, out var hashCode);
             var obj = new ComplexClass(null, new UnsafeStruct(new IntPtr(42L), "Hello, world!"), new UnsafeStruct(new IntPtr(43L), null));
             True(equality(obj, new ComplexClass(null, new UnsafeStruct(new IntPtr(42L), "Hello, world!"), new UnsafeStruct(new IntPtr(43L), null))));
             False(equality(obj, null));
@@ -60,8 +59,7 @@ namespace DotNext
         [Fact]
         public static void StructArrayComparer()
         {
-            var hashCode = EqualityComparerBuilder.BuildGetHashCode<UnsafeStruct[]>();
-            var equality = EqualityComparerBuilder.BuildEquals<UnsafeStruct[]>();
+            new EqualityComparerBuilder<UnsafeStruct[]>().Build(out var equality, out var hashCode);
             var array = new[] { new UnsafeStruct(new IntPtr(42L), "Hello, world!"), new UnsafeStruct(new IntPtr(43L), null) };
             Equal(hashCode(array), hashCode(new[] { new UnsafeStruct(new IntPtr(42L), "Hello, world!"), new UnsafeStruct(new IntPtr(43L), null) }));
             True(equality(array, new[] { new UnsafeStruct(new IntPtr(42L), "Hello, world!"), new UnsafeStruct(new IntPtr(43L), null) }));
@@ -70,8 +68,7 @@ namespace DotNext
         [Fact]
         public static void UnsafeClassComparer()
         {
-            var hashCode = EqualityComparerBuilder.BuildGetHashCode<UnsafeClass>();
-            var equality = EqualityComparerBuilder.BuildEquals<UnsafeClass>();
+            new EqualityComparerBuilder<UnsafeClass>().Build(out var equality, out var hashCode);
             var obj = new UnsafeClass(new IntPtr(42L), "Hello, world!");
             Equal(hashCode(obj), hashCode(new UnsafeClass(new IntPtr(42L), "Hello, world!")));
             True(equality(obj, new UnsafeClass(new IntPtr(42L), "Hello, world!")));
@@ -80,8 +77,7 @@ namespace DotNext
         [Fact]
         public static void ArrayOfRefTypeComparer()
         {
-            var hashCode = EqualityComparerBuilder.BuildGetHashCode<string[]>();
-            var equality = EqualityComparerBuilder.BuildEquals<string[]>();
+            new EqualityComparerBuilder<string[]>().Build(out var equality, out var hashCode);
             var array = new string[] { "a", "b" };
             Equal(hashCode(array), hashCode(new string[] { "a", "b" }));
             True(equality(array, new string[] { "a", "b" }));
@@ -90,8 +86,7 @@ namespace DotNext
         [Fact]
         public static void LongEqualityComparer()
         {
-            var hashCode = EqualityComparerBuilder.BuildGetHashCode<long>();
-            var equality = EqualityComparerBuilder.BuildEquals<long>();
+            new EqualityComparerBuilder<long>().Build(out var equality, out var hashCode);
             Equal(hashCode(42L), hashCode(42L));
             True(equality(42L, 42L));
         }
@@ -99,8 +94,7 @@ namespace DotNext
         [Fact]
         public static void GuidEqualityComparer()
         {
-            var hashCode = EqualityComparerBuilder.BuildGetHashCode<Guid>();
-            var equality = EqualityComparerBuilder.BuildEquals<Guid>();
+            new EqualityComparerBuilder<Guid>().Build(out var equality, out var hashCode);
             var g = Guid.NewGuid();
             Equal(hashCode(g), hashCode(g));
             True(equality(g, g));
