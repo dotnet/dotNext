358a335d4d3f8e717fccb6d7014ba1bff96add04
Roman Sakno <riman.sakno@bytex.solutions>
2019-04-30 10:32:36 +0300

    Turn scope into statement

--- src/DotNext.Metaprogramming/Linq/Expressions/ForExpression.cs
+++ src/DotNext.Metaprogramming/Linq/Expressions/ForExpression.cs
@@ -3,42 +3,8 @@ using System.Linq.Expressions;
 
 namespace DotNext.Linq.Expressions
 {
-    using LoopContext = Metaprogramming.LoopContext;
-
     public sealed class ForExpression: Expression, ILoopExpression
     {
-        internal readonly struct LoopIteration: ILazyExpression<Action<ParameterExpression>>
-        {
-            private readonly ForExpression loop;
-
-            internal LoopIteration(ForExpression loop) => this.loop = loop;
-
-            ref Expression ILazyExpression<Action<ParameterExpression>>.GetBody(Action<ParameterExpression> action)
-            {
-                action(loop.LoopVar);
-                return ref loop.iteration;
-            }
-        }
-
-        internal readonly struct LoopBody: ILazyExpression<Action<ParameterExpression>>, ILazyExpression<Action<ParameterExpression, LoopContext>>
-        {
-            private readonly ForExpression loop;
-
-            internal LoopBody(ForExpression loop) => this.loop = loop;
-
-            ref Expression ILazyExpression<Action<ParameterExpression>>.GetBody(Action<ParameterExpression> action)
-            {
-                action(loop.LoopVar);
-                return ref loop.body;
-            }
-
-            ref Expression ILazyExpression<Action<ParameterExpression, LoopContext>>.GetBody(Action<ParameterExpression, LoopContext> action)
-            {
-                action(loop.LoopVar, new LoopContext(loop));
-                return ref loop.body;
-            }
-        }
-
         internal interface IBuilder
         {
             Expression MakeCondition(ParameterExpression loopVar);
@@ -51,17 +17,13 @@ namespace DotNext.Linq.Expressions
         internal ForExpression(Expression initialization, IBuilder builder)
         {
             Initialization = initialization;
-            LoopVar = Expression.Variable(initialization.Type, "loop_var");
+            LoopVar = Variable(initialization.Type, "loop_var");
             Test = builder.MakeCondition(LoopVar);
             ContinueLabel = Label(typeof(void), "continue");
             BreakLabel = Label(typeof(void), "break");
             body = builder.MakeBody(LoopVar);
             iteration = builder.MakeIteration(LoopVar);
         }
-
-        internal LoopBody GetBody() => new LoopBody(this);
-
-        internal LoopIteration GetIteration() => new LoopIteration(this);
 
         public Expression Iteration => iteration;
 
