bb867fcedb037c6308edcbb4990751f3bbcd38a9
Roman Sakno <riman.sakno@bytex.solutions>
2019-05-08 15:17:16 +0300

    Fixed index calculation

--- src/DotNext.Threading/Threading/ConcurrentObjectPool.cs
+++ src/DotNext.Threading/Threading/ConcurrentObjectPool.cs
@@ -79,7 +79,7 @@ namespace DotNext.Threading
             for (var spinner = new SpinWait(); ; spinner.SpinOnce())
             {
                 //apply selection using round-robin mechanism
-                var index = (counter.IncrementAndGet() % objects.Count) & int.MaxValue;
+                var index = (counter.IncrementAndGet() & int.MaxValue) % objects.Count;
                 //lock selected object if possible
                 var result = new Rental(objects[index], out var locked);
                 if (locked)
