450067bfe3285b23b84d9546e74862145553007d
Roman Sakno <roman.sakno@gmail.com>
2019-05-11 23:57:00 +0300

    Simplify timeout check

--- src/DotNext.Threading/Threading/AsyncLockHelpers.cs
+++ src/DotNext.Threading/Threading/AsyncLockHelpers.cs
@@ -1,14 +1,12 @@
 using System;
 using System.Diagnostics.CodeAnalysis;
 using System.Runtime.CompilerServices;
-using System.Threading;
 using System.Threading.Tasks;
 
 namespace DotNext.Threading
 {
     internal static class AsyncLockHelpers
     {
-        internal const TaskContinuationOptions CheckOnTimeoutOptions = TaskContinuationOptions.ExecuteSynchronously | TaskContinuationOptions.OnlyOnRanToCompletion;
         private static readonly Action<Task<bool>> CheckOnTimemout = task =>
         {
             if (!task.Result)
@@ -17,6 +15,6 @@ namespace DotNext.Threading
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         [SuppressMessage("Reliability", "CA2008", Justification = "Timeout check cannot cause deadlock so Current task scheduler is OK")]
-        internal static Task CheckOnTimeout(this Task<bool> continuation) => continuation.ContinueWith(CheckOnTimemout, CheckOnTimeoutOptions);
+        internal static Task CheckOnTimeout(this Task<bool> continuation) => continuation.ContinueWith(CheckOnTimemout, TaskContinuationOptions.ExecuteSynchronously | TaskContinuationOptions.OnlyOnRanToCompletion);
     }
 }
