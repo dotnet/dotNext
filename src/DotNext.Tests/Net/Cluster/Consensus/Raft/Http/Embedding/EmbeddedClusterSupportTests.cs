12661262468c948772445dc9dc0e65a74e302a81
Roman Sakno <riman.sakno@bytex.solutions>
2019-06-14 16:24:02 +0300

    Updated FxCop Analyzer

---
+++ src/DotNext.Tests/Net/Cluster/Consensus/Raft/Http/Embedding/EmbeddedClusterSupportTests.cs
@@ -0,0 +1,9 @@
+﻿using Xunit;
+
+namespace DotNext.Net.Cluster.Consensus.Raft.Http.Embedding
+{
+    public sealed class EmbeddedClusterSupportTests : Assert
+    {
+        
+    }
+}
---
+++ src/DotNext.Tests/Net/Cluster/Consensus/Raft/Http/Embedding/WebApplicationSetup.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DotNext.Net.Cluster.Consensus.Raft.Http.Embedding
+{
+    internal sealed class WebApplicationSetup
+    {
+    }
+}
