9207d3dcf60755dfbb6fce2442a77cd8c3af9f47
Roman Sakno <roman.sakno@gmail.com>
2019-07-15 02:03:25 +0300

    Starting implementation of synchronous replication

--- src/cluster/DotNext.Net.Cluster/Net/Cluster/Replication/IReplicationCluster.cs
+++ src/cluster/DotNext.Net.Cluster/Net/Cluster/Replication/IReplicationCluster.cs
@@ -1,4 +1,8 @@
-﻿namespace DotNext.Net.Cluster.Replication
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DotNext.Net.Cluster.Replication
 {
     using IMessage = Messaging.IMessage;
 
@@ -13,5 +17,15 @@
         /// Gets transaction log used for replication.
         /// </summary>
         IAuditTrail<LogEntry> AuditTrail { get; }
+
+        /// <summary>
+        /// Writes message into the cluster according with the specified concern.
+        /// </summary>
+        /// <param name="content">The message to be written into the cluster.</param>
+        /// <param name="concern">The value describing level of acknowledgment from cluster.</param>
+        /// <returns>The task representing asynchronous state of this operation.</returns>
+        /// <exception cref="InvalidOperationException">There is no consensus in cluster.</exception>
+        /// <exception cref="NotSupportedException">The specified level of acknowledgment is not supported.</exception>
+        Task WriteAsync(IMessage content, WriteConcern concern);
     }
 }
