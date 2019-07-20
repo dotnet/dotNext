37d77e8ed5070dc488a08455da63eaf71a722534
Roman Sakno <roman.sakno@gmail.com>
2019-07-20 22:02:02 +0300

    Benchmark for calling by pointer is extended

--- src/DotNext.Benchmarks/FunctionPointerBenchmark.cs
+++ src/DotNext.Benchmarks/FunctionPointerBenchmark.cs
@@ -9,19 +9,35 @@ namespace DotNext
     [Orderer(SummaryOrderPolicy.FastestToSlowest)]
     public class FunctionPointerBenchmark
     {
-        private static readonly Func<string, int> ParseMethod = int.Parse;
-        private static readonly FunctionPointer<string, int> ParseMethodPtr = new FunctionPointer<string, int>(ParseMethod);
+        private static readonly Func<string, int> ParseToIntMethod = int.Parse;
+        private static readonly FunctionPointer<string, int> ParseToIntMethodPtr = new FunctionPointer<string, int>(ParseToIntMethod);
+
+        private static readonly Func<string, Guid> ParseToGuidMethod = Guid.Parse;
+
+        private static readonly FunctionPointer<string, Guid> ParseToGuidMethodPtr = new FunctionPointer<string, Guid>(ParseToGuidMethod);
 
         [Benchmark]
         public void InvokeDelegate()
         {
-            ParseMethod("123");
+            ParseToIntMethod("123");
         }
 
         [Benchmark]
         public void InvokeFunctionPtr()
         {
-            ParseMethodPtr.Invoke("123");
+            ParseToIntMethodPtr.Invoke("123");
+        }
+
+        [Benchmark]
+        public void InvokeDelegateLargeReturn()
+        {
+            ParseToGuidMethod("{28a507b5-79b4-44d2-9fe9-969313d76361}");
+        }
+
+        [Benchmark]
+        public void InvokeFunctionPtrLargeReturn()
+        {
+            ParseToGuidMethodPtr.Invoke("{28a507b5-79b4-44d2-9fe9-969313d76361}");
         }
     }
 }
