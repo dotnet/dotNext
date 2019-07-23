16e67a8de7ba1edad7522d429a3c7c49c6b727d9
Roman Sakno <roman.sakno@gmail.com>
2019-07-23 21:34:11 +0300

    Test coverage for VolatileContainer

--- src/DotNext.Tests/Threading/VolatileContainerTests.cs
+++ src/DotNext.Tests/Threading/VolatileContainerTests.cs
@@ -6,12 +6,53 @@ namespace DotNext.Threading
     public sealed class VolatileContainerTests : Assert
     {
         [Fact]
-        public static void VolatileRead()
+        public static void ReadWrite()
         {
             var container = new VolatileContainer<Guid>();
             var value = Guid.NewGuid();
             container.Value = value;
             Equal(value, container.Value);
+        }
+
+        [Fact]
+        public static void AtomicExchange()
+        {
+            var container = new VolatileContainer<Guid>();
+            Equal(Guid.Empty, container.Value);
+
+            container.Exchange(Guid.NewGuid(), out var value);
+            Equal(Guid.Empty, value);
+            NotEqual(Guid.Empty, container.Value);
+        }
+
+        [Fact]
+        public static void AtomicUpdate()
+        {
+            var container = new VolatileContainer<Guid>();
+            container.GetAndUpdate((in Guid current, out Guid newValue) => newValue = Guid.NewGuid(), out var value);
+            Equal(Guid.Empty, value);
+            NotEqual(Guid.Empty, container.Value);
+        }
+
+        [Fact]
+        public static void CompareAndSet()
+        {
+            var container = new VolatileContainer<Guid>();
+            True(container.CompareAndSet(Guid.Empty, Guid.NewGuid()));
+            NotEqual(Guid.Empty, container.Value);
+            False(container.CompareAndSet(Guid.Empty, Guid.NewGuid()));
+            NotEqual(Guid.Empty, container.Value);
+        }
+
+        private static void Add(in decimal current, in decimal value, out decimal result) => result = current + value;
+
+        [Fact]
+        public static void Accumulation()
+        {
+            var container = new VolatileContainer<decimal>();
+            container.GetAndAccumulate(10M, Add, out var value);
+            Equal(decimal.Zero, value);
+            Equal(10M, container.Value);
         }
     }
 }
