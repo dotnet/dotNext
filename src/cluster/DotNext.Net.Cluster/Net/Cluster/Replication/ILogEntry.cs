ec06c3beb06e392c0c1c96741fdea19b695e0a47
sakno <roman.sakno@gmail.com>
2019-09-02 13:43:59 +0300

    Snapshot log record is introduced

---
+++ src/cluster/DotNext.Net.Cluster/Net/Cluster/Replication/ILogEntry.cs
@@ -0,0 +1,13 @@
+﻿namespace DotNext.Net.Cluster.Replication
+{
+    /// <summary>
+    /// Represents log entry in the audit trail.
+    /// </summary>
+    public interface ILogEntry : Messaging.IMessage
+    {
+        /// <summary>
+        /// Gets a value indicating that this entry is a snapshot entry.
+        /// </summary>
+        bool IsSnapshot { get; }
+    }
+}
