911880f97fb9892ce3dd5603f37dbb33ca8e7308
sakno <roman.sakno@gmail.com>
2019-09-02 14:42:37 +0300

    Test entry is not a snapshot

--- src/DotNext.Tests/Net/Cluster/Consensus/Raft/TestLogEntry.cs
+++ src/DotNext.Tests/Net/Cluster/Consensus/Raft/TestLogEntry.cs
@@ -1,6 +1,7 @@
 namespace DotNext.Net.Cluster.Consensus.Raft
 {
     using TextMessage = Messaging.TextMessage;
+    using ILogEntry = Replication.ILogEntry;
 
     internal sealed class TestLogEntry : TextMessage, IRaftLogEntry
     {
@@ -10,5 +11,7 @@ namespace DotNext.Net.Cluster.Consensus.Raft
         }
 
         public long Term { get; set; }
+
+        bool ILogEntry.IsSnapshot => false;
     }
 }
