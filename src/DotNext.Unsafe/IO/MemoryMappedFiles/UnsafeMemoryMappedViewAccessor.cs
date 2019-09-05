6740446150ea9dd322f7cf385bc298806d2a96a3
sakno <roman.sakno@gmail.com>
2019-09-05 17:51:16 +0300

    Working with memory-mapped files directly using pointer

---
+++ src/DotNext.Unsafe/IO/MemoryMappedFiles/UnsafeMemoryMappedViewAccessor.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO.MemoryMappedFiles;
+
+namespace DotNext.IO.MemoryMappedFiles
+{
+    using Runtime.InteropServices;
+
+    public unsafe readonly struct UnsafeMemoryMappedViewAccessor : IDisposable
+    {
+        private readonly MemoryMappedViewAccessor accessor;
+        private readonly byte* ptr;
+
+        internal UnsafeMemoryMappedViewAccessor(MemoryMappedViewAccessor accessor)
+        {
+            MemoryMappedFile f;
+            this.accessor = accessor;
+            ptr = default;
+            accessor.SafeMemoryMappedViewHandle.AcquirePointer(ref ptr);
+        }
+
+        public Pointer<byte> Pointer => new Pointer<byte>(ptr + accessor.PointerOffset);
+
+        public long Length => accessor.Capacity;
+
+        public Span<byte> Bytes => new Span<byte>(ptr, checked((int)accessor.Capacity));
+
+        public void Dispose()
+        {
+            accessor.SafeMemoryMappedViewHandle.ReleasePointer();
+            accessor.Dispose();
+        }
+    }
+}
