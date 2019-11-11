2b31f8b7af9667e5108a3b3169968958b20226e8
sakno <roman.sakno@gmail.com>
2019-11-11 11:13:58 +0200

    Added method for reversing bytes

--- src/DotNext/Runtime/Intrinsics.cs
+++ src/DotNext/Runtime/Intrinsics.cs
@@ -869,5 +869,12 @@ namespace DotNext.Runtime
         public static unsafe int GetHashCode32(void* source, long length, bool salted = true)
             => GetHashCode32(in Unsafe.AsRef<byte>(source), length, salted);
         #endregion
+
+        /// <summary>
+        /// Reverse bytes in the specified value of blittable type.
+        /// </summary>
+        /// <typeparam name="T">Blittable type.</typeparam>
+        /// <param name="value">The value which bytes should be reversed.</param>
+        public static void Reverse<T>(ref T value) where T : unmanaged => AsSpan(ref value).Reverse();
     }
 }
