ae11314dcd9798550afe8ead772ff329ea99e238
sakno <rvsakno@outlook.com>
2019-11-17 23:44:33 +0200

    Increased test coverage

--- src/DotNext.Tests/Threading/Channels/PersistentChannelTests.cs
+++ src/DotNext.Tests/Threading/Channels/PersistentChannelTests.cs
@@ -40,12 +40,15 @@ namespace DotNext.Threading.Channels
             Guid g1 = Guid.NewGuid(), g2 = Guid.NewGuid(), g3 = Guid.NewGuid();
             using (var channel = new GuidChannel(new PersistentChannelOptions { SingleReader = singleReader, SingleWriter = singleWriter }))
             {
+                False(channel.Writer.TryWrite(g1));
                 await channel.Writer.WriteAsync(g1);
                 await channel.Writer.WriteAsync(g2);
                 await channel.Writer.WriteAsync(g3);
                 Equal(g1, await channel.Reader.ReadAsync());
                 Equal(g2, await channel.Reader.ReadAsync());
-                Equal(g3, await channel.Reader.ReadAsync());
+                True(await channel.Reader.WaitToReadAsync());
+                True(channel.Reader.TryRead(out var last));
+                Equal(g3, last);
                 Equal(1D, channel.Ratio);
             }
         }
@@ -71,6 +74,29 @@ namespace DotNext.Threading.Channels
 
         [Fact]
         public static async Task PartitionOverflow()
+        {
+            var options = new PersistentChannelOptions
+            {
+                SingleReader = true,
+                SingleWriter = true,
+                RecordsPerPartition = 3
+            };
+            Guid g1 = Guid.NewGuid(), g2 = Guid.NewGuid(), g3 = Guid.NewGuid(), g4 = Guid.NewGuid();
+            using (var channel = new GuidChannel(options))
+            {
+                await channel.Writer.WriteAsync(g1);
+                await channel.Writer.WriteAsync(g2);
+                await channel.Writer.WriteAsync(g3);
+                await channel.Writer.WriteAsync(g4);
+                Equal(g1, await channel.Reader.ReadAsync());
+                Equal(g2, await channel.Reader.ReadAsync());
+                Equal(g3, await channel.Reader.ReadAsync());
+                Equal(g4, await channel.Reader.ReadAsync());
+            }
+        }
+
+        [Fact]
+        public static async Task PersistentPartitionOverflow()
         {
             var options = new PersistentChannelOptions
             {
