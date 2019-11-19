6c0a649328ee97e6e5cd20efeabe5e6321392e5f
sakno <roman.sakno@gmail.com>
2019-11-19 13:31:52 +0200

    Porting of PersistentChannel from 1.x

---
+++ src/DotNext.Threading/Threading/Channels/IChannel.cs
@@ -0,0 +1,14 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.IO;
+
+namespace DotNext.Threading.Channels
+{
+    using IO;
+
+    internal interface IChannel
+    {
+        DirectoryInfo Location { get; }
+
+        PartitionStream GetOrCreatePartition(ref ChannelCursor cursor, [NotNull]ref PartitionStream? partition, in FileCreationOptions options, bool deleteOnDispose);
+    }
+}
