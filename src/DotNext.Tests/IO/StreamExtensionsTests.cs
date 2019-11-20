9f5f666256fdccf0c5c55ce95814ddbad5bffe9d
sakno <roman.sakno@gmail.com>
2019-11-20 16:01:17 +0200

    Additional tests for stream parsing methods

--- src/DotNext.Tests/IO/StreamExtensionsTests.cs
+++ src/DotNext.Tests/IO/StreamExtensionsTests.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Diagnostics.CodeAnalysis;
 using System.IO;
 using System.Text;
@@ -54,13 +55,30 @@ namespace DotNext.IO
         private static async Task ReadStringUsingEncodingAsync(Encoding encoding, int bufferSize)
         {
             const string helloWorld = "Hello, world! $(@$)Hjdqgd!";
-            using (var ms = new MemoryStream())
-            {
-                await ms.WriteAsync(encoding.GetBytes(helloWorld));
-                ms.Position = 0;
-                var buffer = new byte[bufferSize];
-                Equal(helloWorld, await ms.ReadStringAsync(encoding.GetByteCount(helloWorld), encoding, buffer));
-            }
+            using var ms = new MemoryStream();
+            await ms.WriteAsync(encoding.GetBytes(helloWorld));
+            ms.Position = 0;
+            var buffer = new byte[bufferSize];
+            Equal(helloWorld, await ms.ReadStringAsync(encoding.GetByteCount(helloWorld), encoding, buffer));
+        }
+
+        private static async Task ReadStringUsingEncodingAsync(Encoding encoding)
+        {
+            const string helloWorld = "Hello, world! $(@$)Hjdqgd!";
+            using var ms = new MemoryStream();
+            await ms.WriteAsync(encoding.GetBytes(helloWorld));
+            ms.Position = 0;
+            Equal(helloWorld, await ms.ReadStringAsync(encoding.GetByteCount(helloWorld), encoding));
+        }
+
+        [Fact]
+        public static async Task ReadStringAsync()
+        {
+            await ReadStringUsingEncodingAsync(Encoding.UTF8);
+            await ReadStringUsingEncodingAsync(Encoding.Unicode);
+            await ReadStringUsingEncodingAsync(Encoding.UTF7);
+            await ReadStringUsingEncodingAsync(Encoding.UTF32);
+            await ReadStringUsingEncodingAsync(Encoding.ASCII);
         }
 
         [Theory]
@@ -68,7 +86,7 @@ namespace DotNext.IO
         [InlineData(10)]
         [InlineData(15)]
         [InlineData(1024)]
-        public static async Task ReadStringAsync(int bufferSize)
+        public static async Task ReadStringBufferedAsync(int bufferSize)
         {
             await ReadStringUsingEncodingAsync(Encoding.UTF8, bufferSize);
             await ReadStringUsingEncodingAsync(Encoding.Unicode, bufferSize);
@@ -80,20 +98,37 @@ namespace DotNext.IO
         private static void ReadWriteStringUsingEncoding(Encoding encoding, int bufferSize)
         {
             const string helloWorld = "Hello, world!&*(@&*(fghjwgfwffgw";
-            using (var ms = new MemoryStream())
-            {
-                var buffer = new byte[bufferSize];
-                ms.WriteString(helloWorld, encoding, buffer);
-                ms.Position = 0;
-                Equal(helloWorld, ms.ReadString(encoding.GetByteCount(helloWorld), encoding, buffer));
-            }
+            using var ms = new MemoryStream();
+            var buffer = new byte[bufferSize];
+            ms.WriteString(helloWorld, encoding, buffer);
+            ms.Position = 0;
+            Equal(helloWorld, ms.ReadString(encoding.GetByteCount(helloWorld), encoding, buffer));
+        }
+
+        private static void ReadWriteStringUsingEncoding(Encoding encoding)
+        {
+            const string helloWorld = "Hello, world!&*(@&*(fghjwgfwffgw";
+            using var ms = new MemoryStream();
+            ms.WriteString(helloWorld, encoding);
+            ms.Position = 0;
+            Equal(helloWorld, ms.ReadString(encoding.GetByteCount(helloWorld), encoding));
+        }
+
+        [Fact]
+        public static void ReadWriteString()
+        {
+            ReadWriteStringUsingEncoding(Encoding.UTF8);
+            ReadWriteStringUsingEncoding(Encoding.Unicode);
+            ReadWriteStringUsingEncoding(Encoding.UTF7);
+            ReadWriteStringUsingEncoding(Encoding.UTF32);
+            ReadWriteStringUsingEncoding(Encoding.ASCII);
         }
 
         [Theory]
         [InlineData(10)]
         [InlineData(15)]
         [InlineData(1024)]
-        public static void ReadWriteString(int bufferSize)
+        public static void ReadWriteBufferedString(int bufferSize)
         {
             ReadWriteStringUsingEncoding(Encoding.UTF8, bufferSize);
             ReadWriteStringUsingEncoding(Encoding.Unicode, bufferSize);
@@ -105,20 +140,37 @@ namespace DotNext.IO
         private static async Task ReadWriteStringUsingEncodingAsync(Encoding encoding, int bufferSize)
         {
             const string helloWorld = "Hello, world!&*(@&*(fghjwgfwffgw";
-            using (var ms = new MemoryStream())
-            {
-                var buffer = new byte[bufferSize];
-                await ms.WriteStringAsync(helloWorld, encoding, buffer);
-                ms.Position = 0;
-                Equal(helloWorld, await ms.ReadStringAsync(encoding.GetByteCount(helloWorld), encoding, buffer));
-            }
+            using var ms = new MemoryStream();
+            var buffer = new byte[bufferSize];
+            await ms.WriteStringAsync(helloWorld.AsMemory(), encoding, buffer);
+            ms.Position = 0;
+            Equal(helloWorld, await ms.ReadStringAsync(encoding.GetByteCount(helloWorld), encoding, buffer));
+        }
+
+        private static async Task ReadWriteStringUsingEncodingAsync(Encoding encoding)
+        {
+            const string helloWorld = "Hello, world!&*(@&*(fghjwgfwffgw";
+            using var ms = new MemoryStream();
+            await ms.WriteStringAsync(helloWorld.AsMemory(), encoding);
+            ms.Position = 0;
+            Equal(helloWorld, await ms.ReadStringAsync(encoding.GetByteCount(helloWorld), encoding));
+        }
+
+        [Fact]
+        public static async Task ReadWriteStringAsync()
+        {
+            await ReadWriteStringUsingEncodingAsync(Encoding.UTF8);
+            await ReadWriteStringUsingEncodingAsync(Encoding.Unicode);
+            await ReadWriteStringUsingEncodingAsync(Encoding.UTF7);
+            await ReadWriteStringUsingEncodingAsync(Encoding.UTF32);
+            await ReadWriteStringUsingEncodingAsync(Encoding.ASCII);
         }
 
         [Theory]
         [InlineData(10)]
         [InlineData(15)]
         [InlineData(1024)]
-        public static async Task ReadWriteStringAsync(int bufferSize)
+        public static async Task ReadWriteBufferedStringAsync(int bufferSize)
         {
             await ReadWriteStringUsingEncodingAsync(Encoding.UTF8, bufferSize);
             await ReadWriteStringUsingEncodingAsync(Encoding.Unicode, bufferSize);
@@ -130,12 +182,10 @@ namespace DotNext.IO
         [Fact]
         public static void SynchronousCopying()
         {
-            using (var source = new MemoryStream(new byte[] { 2, 4, 5 }))
-            using (var destination = new MemoryStream())
-            {
-                var buffer = new byte[2];
-                Equal(3L, source.CopyTo(destination, buffer));
-            }
+            using var source = new MemoryStream(new byte[] { 2, 4, 5 });
+            using var destination = new MemoryStream();
+            var buffer = new byte[2];
+            Equal(3L, source.CopyTo(destination, buffer));
         }
     }
 }
