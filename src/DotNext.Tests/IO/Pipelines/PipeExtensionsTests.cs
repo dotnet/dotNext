2c07e21dc49d37e6958939bb928a49515ed8577c
sakno <rvsakno@outlook.com>
2019-11-25 17:48:07 +0200

    Added tests for I/O pipeline

---
+++ src/DotNext.Tests/IO/Pipelines/PipeExtensionsTests.cs
@@ -0,0 +1,22 @@
+﻿using System.IO.Pipelines;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace DotNext.IO.Pipelines
+{
+    public sealed class PipeExtensionsTests : Assert
+    {
+        [Fact]
+        public static async Task EncodeDecodeValue()
+        {
+            static async void WriteValueAsync(decimal value, PipeWriter writer)
+            {
+                await writer.WriteAsync(value);
+            }
+
+            var pipe = new Pipe();
+            WriteValueAsync(20M, pipe.Writer);
+            Equal(10M, await pipe.Reader.ReadAsync<decimal>());
+        }
+    }
+}
