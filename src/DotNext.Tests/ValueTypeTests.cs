06ee709ddc4563b7880dbddb00b91e751eac086b
sakno <rvsakno@outlook.com>
2019-11-25 22:02:18 +0200

    Additional tests for BitwiseComparer singleton

--- src/DotNext.Tests/ValueTypeTests.cs
+++ src/DotNext.Tests/ValueTypeTests.cs
@@ -63,8 +63,10 @@ namespace DotNext
             var value1 = 10L;
             var value2 = 20L;
             False(BitwiseComparer<long>.Equals(value1, value2));
+            NotEqual(value1, value2, BitwiseComparer<long>.Instance);
             value2 = 10L;
             True(BitwiseComparer<long>.Equals(value1, value2));
+            Equal(value1, value2, BitwiseComparer<long>.Instance);
         }
 
         [Fact]
@@ -90,44 +92,54 @@ namespace DotNext
         [Fact]
         public static void BitwiseComparison()
         {
+            IComparer<decimal> comparer = BitwiseComparer<decimal>.Instance;
             var value1 = 40M;
             var value2 = 50M;
             Equal(value1.CompareTo(value2) < 0, BitwiseComparer<decimal>.Compare(value1, value2) < 0);
+            Equal(value1.CompareTo(value2) < 0, comparer.Compare(value1, value2) < 0);
             value2 = default;
             Equal(value1.CompareTo(value2) > 0, BitwiseComparer<decimal>.Compare(value1, value2) > 0);
+            Equal(value1.CompareTo(value2) > 0, comparer.Compare(value1, value2) > 0);
         }
 
         [Fact]
         public static void BitwiseHashCodeForInt()
         {
+            IEqualityComparer<int> comparer = BitwiseComparer<int>.Instance;
             var i = 20;
             var hashCode = BitwiseComparer<int>.GetHashCode(i, false);
             Equal(i, hashCode);
             hashCode = BitwiseComparer<int>.GetHashCode(i, true);
             NotEqual(i, hashCode);
+            Equal(hashCode, comparer.GetHashCode(i));
         }
 
         [Fact]
         public static void BitwiseHashCodeForLong()
         {
+            IEqualityComparer<long> comparer = BitwiseComparer<long>.Instance;
             var i = 20L;
             var hashCode = BitwiseComparer<long>.GetHashCode(i, false);
             Equal(i, hashCode);
             hashCode = BitwiseComparer<long>.GetHashCode(i, true);
             NotEqual(i, hashCode);
+            Equal(hashCode, comparer.GetHashCode(i));
         }
 
         [Fact]
         public static void BitwiseHashCodeForGuid()
         {
+            IEqualityComparer<Guid> comparer = BitwiseComparer<Guid>.Instance;
             var value = Guid.NewGuid();
-            BitwiseComparer<Guid>.GetHashCode(value, false);
+            Equal(BitwiseComparer<Guid>.GetHashCode(value, true), comparer.GetHashCode(value));
         }
 
         [Fact]
         public static void BitwiseCompare()
         {
             True(BitwiseComparer<int>.Compare(0, int.MinValue) < 0);
+            IComparer<int> comparer = BitwiseComparer<int>.Instance;
+            True(comparer.Compare(0, int.MinValue) < 0);
         }
 
         [Fact]
