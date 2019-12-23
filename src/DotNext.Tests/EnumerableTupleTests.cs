58b587b0f0aab81630dee132d28f537385f40637
sakno <rvsakno@outlook.com>
2019-12-23 17:05:07 +0200

    Functionality of copying tuple items into span

--- src/DotNext.Tests/EnumerableTupleTests.cs
+++ src/DotNext.Tests/EnumerableTupleTests.cs
@@ -155,5 +155,14 @@ namespace DotNext
             Contains(60M, array);
             Contains(70M, array);
         }
+
+        [Fact]
+        public static void CopyToMemory()
+        {
+            Span<decimal> values = new decimal[2];
+            Equal(2, (10M, 20M, 30M).AsEnumerable().CopyTo(values));
+            Equal(10M, values[0]);
+            Equal(20M, values[1]);
+        }
     }
 }
