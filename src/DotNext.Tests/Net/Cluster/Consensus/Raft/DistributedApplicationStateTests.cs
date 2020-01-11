a85b1c678ad9a03af58d1dfa79e0865b9c15f0b5
sakno <roman.sakno@gmail.com>
2020-01-11 15:36:18 +0200

    Check reconstruction of distributed locks

--- src/DotNext.Tests/Net/Cluster/Consensus/Raft/DistributedApplicationStateTests.cs
+++ src/DotNext.Tests/Net/Cluster/Consensus/Raft/DistributedApplicationStateTests.cs
@@ -91,5 +91,74 @@ namespace DotNext.Net.Cluster.Consensus.Raft
             True(engine.IsRegistered("lock1", id1, version1));
             False(engine.IsRegistered("lock1", id2, version1));
         }
+
+        [Fact]
+        public static async Task Compaction()
+        {
+            GenerateIds(out var id1, out var id2);
+            GenerateIds(out var id3, out var id4);
+            GenerateIds(out var id5, out var id6);
+            var version1 = Guid.NewGuid();
+            var version2 = Guid.NewGuid();
+            var version3 = Guid.NewGuid();
+            var version4 = Guid.NewGuid();
+            var version5 = Guid.NewGuid();
+            var dir = Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString());
+            IDistributedLockEngine engine;
+            using(var state = new DistributedApplicationState(dir, RecordsPerPartition))
+            {
+                engine = state;
+                //acquire 5 locks
+                True(await engine.RegisterAsync("lock1", new DistributedLock
+                {
+                    Owner = id1,
+                    Version = version1,
+                    LeaseTime = TimeSpan.FromHours(1),
+                    CreationTime = DateTimeOffset.Now
+                }, CancellationToken.None));
+                True(await engine.RegisterAsync("lock2", new DistributedLock
+                {
+                    Owner = id2,
+                    Version = version2,
+                    LeaseTime = TimeSpan.FromHours(1),
+                    CreationTime = DateTimeOffset.Now
+                }, CancellationToken.None));
+                True(await engine.RegisterAsync("lock3", new DistributedLock
+                {
+                    Owner = id3,
+                    Version = version3,
+                    LeaseTime = TimeSpan.FromHours(1),
+                    CreationTime = DateTimeOffset.Now
+                }, CancellationToken.None));
+                True(await engine.RegisterAsync("lock4", new DistributedLock
+                {
+                    Owner = id4,
+                    Version = version4,
+                    LeaseTime = TimeSpan.FromHours(1),
+                    CreationTime = DateTimeOffset.Now
+                }, CancellationToken.None));
+                True(await engine.RegisterAsync("lock5", new DistributedLock
+                {
+                    Owner = id5,
+                    Version = version5,
+                    LeaseTime = TimeSpan.FromHours(1),
+                    CreationTime = DateTimeOffset.Now
+                }, CancellationToken.None));
+                //commit all locks
+                await state.CommitAsync(CancellationToken.None);
+            }
+            //reconstruct lock state
+            using(var state = new DistributedApplicationState(dir, RecordsPerPartition))
+            {
+                engine = state;
+                await state.ReplayAsync();
+                True(engine.IsRegistered("lock1", id1, version1));
+                True(engine.IsRegistered("lock2", id2, version2));
+                True(engine.IsRegistered("lock3", id3, version3));
+                True(engine.IsRegistered("lock4", id4, version4));
+                True(engine.IsRegistered("lock5", id5, version5));
+                False(engine.IsRegistered("lock1", id6, version1));
+            }
+        }
     }
 }
