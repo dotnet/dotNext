cd55c5ef138ab6ff9df356cf64cb5b0052a370ff
sakno <roman.sakno@gmail.com>
2020-01-18 18:49:55 +0200

    Fixed Raft node example

--- src/examples/RaftNode/SimplePersistentState.cs
+++ src/examples/RaftNode/SimplePersistentState.cs
@@ -2,16 +2,16 @@
 using DotNext.Net.Cluster.Consensus.Raft;
 using Microsoft.Extensions.Configuration;
 using System;
-using System.IO;
 using System.Threading;
 using System.Threading.Tasks;
+using static DotNext.Threading.AtomicInt64;
 
 namespace RaftNode
 {
+
     internal sealed class SimplePersistentState : PersistentState, IValueProvider
     {
         internal const string LogLocation = "logLocation";
-        private const string ContentFile = "content.bin";
 
         private sealed class SimpleSnapshotBuilder : SnapshotBuilder
         {
@@ -26,14 +26,11 @@ namespace RaftNode
                 => writer.WriteAsync(value, token);
         }
 
-        private readonly FileStream content;
+        private long content;
 
         private SimplePersistentState(string path)
             : base(path, 50, new Options { InitialPartitionSize = 50 * 8 })
         {
-            content = new FileStream(Path.Combine(path, ContentFile), FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.Read, 1024, true);
-            //pre-allocate file for better performance
-            content.SetLength(sizeof(long));
         }
 
         public SimplePersistentState(IConfiguration configuration)
@@ -41,36 +38,15 @@ namespace RaftNode
         {
         }
 
-        async Task<long> IValueProvider.GetValueAsync()
-        {
-            using(await AcquireWriteLockAsync(CancellationToken.None).ConfigureAwait(false))
-            {
-                content.Position = 0;
-                return await content.ReadAsync<long>(Buffer).ConfigureAwait(false);
-            }
-        }
+        long IValueProvider.Value => content.VolatileRead();
 
         protected override async ValueTask ApplyAsync(LogEntry entry)
-        {
-            var value = await entry.ReadAsync<long>().ConfigureAwait(false);
-            content.Position = 0;
-            Console.WriteLine($"Accepting value {value}");
-            await content.WriteAsync(value, Buffer).ConfigureAwait(false);
-        }
-
-        protected override ValueTask FlushAsync() => new ValueTask(content.FlushAsync());
+            => content.VolatileWrite(await entry.ReadAsync<long>().ConfigureAwait(false));
 
         protected override SnapshotBuilder CreateSnapshotBuilder()
         {
             Console.WriteLine("Building snapshot");
             return new SimpleSnapshotBuilder();
-        }
-
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-                content.Dispose();
-            base.Dispose(disposing);
         }
     }
 }
