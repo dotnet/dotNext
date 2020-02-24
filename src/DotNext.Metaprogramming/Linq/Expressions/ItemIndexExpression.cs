c7358a9ff774d75a811fc739c635a945af9e729a
Roman Sakno <roman.sakno@gmail.com>
2020-02-24 21:14:12 +0200

    Index expression is introduced

--- src/DotNext.Metaprogramming/Linq/Expressions/ItemIndexExpression.cs
+++ src/DotNext.Metaprogramming/Linq/Expressions/ItemIndexExpression.cs
@@ -8,10 +8,32 @@ namespace DotNext.Linq.Expressions
     /// <summary>
     /// Expresses construction of <see cref="Index"/> value.
     /// </summary>
-    public sealed class OffsetExpression : Expression
+    public sealed class ItemIndexExpression : Expression
     {
-        public OffsetExpression(Expression value, bool fromEnd = false)
+        private readonly bool conversionRequired;
+
+        /// <summary>
+        /// Initializes a new index.
+        /// </summary>
+        /// <param name="value">The index value.</param>
+        /// <param name="fromEnd">A boolean indicating if the index is from the start (<see langword="false"/>) or from the end (<see langword="true"/>) of a collection.</param>
+        /// <exception cref="ArgumentException">Type of <paramref name="value"/> should be <see cref="int"/>, <see cref="short"/>, <see cref="byte"/> or <see cref="sbyte"/>.</exception>
+        public ItemIndexExpression(Expression value, bool fromEnd = false)
         {
+            switch(Type.GetTypeCode(value.Type))
+            {
+                case TypeCode.Byte:
+                case TypeCode.SByte:
+                case TypeCode.Int16:
+                case TypeCode.UInt16:
+                    conversionRequired = true;
+                    break;
+                case TypeCode.Int32:
+                    conversionRequired = false;
+                    break;
+                default:
+                    throw new ArgumentException(ExceptionMessages.TypeExpected<int>(), nameof(value));
+            }
             IsFromEnd = fromEnd;
             Value = value;
         }
@@ -53,7 +75,7 @@ namespace DotNext.Linq.Expressions
         {
             ConstructorInfo? ctor = typeof(Index).GetConstructor(new []{ typeof(int), typeof(bool) });
             Debug.Assert(!(ctor is null));
-            return New(ctor, Value, Constant(IsFromEnd));
+            return New(ctor, conversionRequired ? Convert(Value, typeof(int)) : Value, Constant(IsFromEnd));
         }
 
         /// <summary>
@@ -64,7 +86,7 @@ namespace DotNext.Linq.Expressions
         protected override Expression VisitChildren(ExpressionVisitor visitor)
         {
             var expression = visitor.Visit(Value);
-            return ReferenceEquals(expression, Value) ? this : new OffsetExpression(expression, IsFromEnd);
+            return ReferenceEquals(expression, Value) ? this : new ItemIndexExpression(expression, IsFromEnd);
         }
     }
 }
