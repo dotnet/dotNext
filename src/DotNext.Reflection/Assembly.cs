ed3d3a859ec9ed5e6cc87e204e5c9888ce17bb61
Roman Sakno <roman.sakno@gmail.com>
2020-04-04 19:40:36 +0300

    Disable private reflection for Reflection library

--- src/DotNext.Reflection/Assembly.cs
+++ src/DotNext.Reflection/Assembly.cs
@@ -1,5 +1,7 @@
 ﻿using DotNext.Runtime.CompilerServices;
 using System;
+using System.Runtime.CompilerServices;
 
 [assembly: CLSCompliant(true)]
+[assembly: DisablePrivateReflection]
 [assembly: RuntimeFeatures(DynamicCodeCompilation = true, RuntimeGenericInstantiation = true, PrivateReflection = true)]
