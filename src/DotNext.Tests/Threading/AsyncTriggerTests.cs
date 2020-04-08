a0177d380e23fde32b19bd869916583ca0ab98a2
Roman Sakno <roman.sakno@gmail.com>
2020-04-08 13:40:12 +0300

    Test for signal-and-wait

--- src/DotNext.Tests/Threading/AsyncTriggerTests.cs
+++ src/DotNext.Tests/Threading/AsyncTriggerTests.cs
@@ -58,5 +58,17 @@ namespace DotNext.Threading
             trigger.Signal(new ValueRefAction<int, int>(ModifyState), 42);
             True(valueNode.IsCompletedSuccessfully);
         }
+
+        [Fact]
+        public static void SignalAndWait()
+        {
+            using var trigger = new AsyncTrigger<int>(0);
+            var waitTask = trigger.SignalAndWaitAsync(10, i => i == 42);
+            False(waitTask.IsCompleted);
+            Equal(10, trigger.CurrentState);
+            trigger.Signal(42);
+            True(waitTask.IsCompletedSuccessfully);
+            Equal(42, trigger.CurrentState);
+        }
     }
 }
