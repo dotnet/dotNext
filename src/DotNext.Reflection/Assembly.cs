8d61b45a8eda6fbae9d88c0aefd4680dc099981c
Roman Sakno <roman.sakno@gmail.com>
2020-04-08 03:03:12 +0300

    Removed bad attribute for assembly

--- src/DotNext.Reflection/Assembly.cs
+++ src/DotNext.Reflection/Assembly.cs
@@ -1,7 +1,5 @@
 ﻿using DotNext.Runtime.CompilerServices;
 using System;
-using System.Runtime.CompilerServices;
 
 [assembly: CLSCompliant(true)]
-[assembly: DisablePrivateReflection]
 [assembly: RuntimeFeatures(DynamicCodeCompilation = true, RuntimeGenericInstantiation = true, PrivateReflection = true)]
