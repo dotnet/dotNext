fc7f42efc025b0d148d334ca3644e9484f3e6ea2
Roman Sakno <roman.sakno@gmail.com>
2020-04-11 19:07:32 +0300

    Check presence of Internet directly

--- src/DotNext.Tests/Net/NetworkInformation/MtuDiscoveryTests.cs
+++ src/DotNext.Tests/Net/NetworkInformation/MtuDiscoveryTests.cs
@@ -1,26 +1,38 @@
 using System.Diagnostics.CodeAnalysis;
 using System.Net;
+using System.Net.NetworkInformation;
 using System.Threading.Tasks;
+using Xunit;
 
 namespace DotNext.Net.NetworkInformation
 {
     [ExcludeFromCodeCoverage]
     public sealed class MtuDiscoveryTests : Test
     {
-        [EnvarDependentFact("InternetAccess", "true", "true")]
+        private const int PingTimeout = 2000;
+
+        [Fact]
         public static void PingToCloudflare()
         {
+            var address = IPAddress.Parse("1.1.1.1");
             using var discovery = new MtuDiscovery();
-            var result = discovery.Discover(IPAddress.Parse("1.1.1.1"), 2000, new MtuDiscoveryOptions());
-            NotNull(result);
+            if(discovery.Send(address, PingTimeout).Status == IPStatus.Success)
+            {
+                var result = discovery.Discover(address, PingTimeout, new MtuDiscoveryOptions());
+                NotNull(result);
+            }
         }
 
-        [EnvarDependentFact("InternetAccess", "true", "true")]
+        [Fact]
         public static async Task PingToOpenDNS()
         {
+            var address = IPAddress.Parse("208.67.222.222");
             using var discovery = new MtuDiscovery();
-            var result = await discovery.DiscoverAsync(IPAddress.Parse("208.67.222.222"), 2000, new MtuDiscoveryOptions());
-            NotNull(result);
+            if(discovery.Send(address, PingTimeout).Status == IPStatus.Success)
+            {
+                var result = await discovery.DiscoverAsync(address, PingTimeout, new MtuDiscoveryOptions());
+                NotNull(result);
+            }
         }
     }
 }
