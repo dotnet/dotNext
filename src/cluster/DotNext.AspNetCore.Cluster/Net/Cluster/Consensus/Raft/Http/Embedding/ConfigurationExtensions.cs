d913a892e2c6bd88913a91a90c1cdd4f9a11f6f9
Roman Sakno <roman.sakno@gmail.com>
2020-05-11 13:06:48 +0300

    Now custom config section can be used as config of local node

--- src/cluster/DotNext.AspNetCore.Cluster/Net/Cluster/Consensus/Raft/Http/Embedding/ConfigurationExtensions.cs
+++ src/cluster/DotNext.AspNetCore.Cluster/Net/Cluster/Consensus/Raft/Http/Embedding/ConfigurationExtensions.cs
@@ -44,6 +44,40 @@ namespace DotNext.Net.Cluster.Consensus.Raft.Http.Embedding
         public static IHostBuilder JoinCluster(this IHostBuilder builder)
             => builder.ConfigureServices(JoinCluster);
 
+        private static void JoinCluster(this Func<IConfiguration, IHostEnvironment, IConfiguration> memberConfig, HostBuilderContext context, IServiceCollection services)
+            => ConfigureLocalNode(services, memberConfig(context.Configuration, context.HostingEnvironment));
+
+        /// <summary>
+        /// Allows to inject <see cref="ICluster"/>, <see cref="IRaftCluster"/>, <see cref="IExpandableCluster"/>
+        /// to application services and establishes network communication with other cluster members.
+        /// </summary>
+        /// <remarks>
+        /// Should be called exactly after <see cref="GenericHostWebHostBuilderExtensions.ConfigureWebHost"/>
+        /// or <see cref="GenericHostBuilderExtensions.ConfigureWebHostDefaults"/>.
+        /// </remarks>
+        /// <param name="builder">The host builder.</param>
+        /// <param name="memberConfig">The delegate that allows to resolve location of local member configuration.</param>
+        /// <returns>The modified host builder.</returns>
+        public static IHostBuilder JoinCluster(this IHostBuilder builder, Func<IConfiguration, IHostEnvironment, IConfiguration> memberConfig)
+            => builder.ConfigureServices(memberConfig.JoinCluster);
+
+        private static void JoinCluster(this string memberConfigSection, HostBuilderContext context, IServiceCollection services)
+            => ConfigureLocalNode(services, context.Configuration.GetSection(memberConfigSection));
+
+        /// <summary>
+        /// Allows to inject <see cref="ICluster"/>, <see cref="IRaftCluster"/>, <see cref="IExpandableCluster"/>
+        /// to application services and establishes network communication with other cluster members.
+        /// </summary>
+        /// <remarks>
+        /// Should be called exactly after <see cref="GenericHostWebHostBuilderExtensions.ConfigureWebHost"/>
+        /// or <see cref="GenericHostBuilderExtensions.ConfigureWebHostDefaults"/>.
+        /// </remarks>
+        /// <param name="builder">The host builder.</param>
+        /// <param name="memberConfigSection">The name of local member configuration section.</param>
+        /// <returns>The modified host builder.</returns>
+        public static IHostBuilder JoinCluster(this IHostBuilder builder, string memberConfigSection)
+            => builder.ConfigureServices(memberConfigSection.JoinCluster);
+
         private static void ConfigureConsensusProtocolHandler(this RaftHttpCluster cluster, IApplicationBuilder builder)
             => builder.UseExceptionHandler(new ExceptionHandlerOptions { ExceptionHandler = RaftHttpConfigurator.WriteExceptionContent }).Run(cluster.ProcessRequest);
 
