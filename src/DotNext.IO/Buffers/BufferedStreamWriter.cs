08eac70300ed85c42dfe9dc5b9d0354f75276146
Roman Sakno <roman.sakno@gmail.com>
2020-05-30 12:17:55 +0300

    Test for stream writer

--- src/DotNext.IO/Buffers/BufferedStreamWriter.cs
+++ src/DotNext.IO/Buffers/BufferedStreamWriter.cs
@@ -7,11 +7,6 @@ namespace DotNext.Buffers
 {
     internal sealed class BufferedStreamWriter : IFlushableBufferWriter<byte>
     {
-        /// <summary>
-        /// Represents default initial buffer size.
-        /// </summary>
-        private const int DefaultInitialBufferSize = 256;
-
         private readonly Stream output;
         private readonly MemoryAllocator<byte> allocator;
         private MemoryOwner<byte> buffer;
@@ -23,43 +18,19 @@ namespace DotNext.Buffers
             this.allocator = allocator;
         }
 
-        private int FreeCapacity => buffer.Length - position;
-
-        private void Resize(int newSize)
-        {
-            var newBuffer = allocator(newSize);
-            buffer.Memory.CopyTo(newBuffer.Memory);
-            buffer.Dispose();
-            buffer = newBuffer;
-        }
-
-        private void CheckAndResizeBuffer(int sizeHint)
+        public Memory<byte> GetMemory(int sizeHint)
         {
             if (sizeHint < 0)
                 throw new ArgumentOutOfRangeException(nameof(sizeHint));
             if (sizeHint == 0)
                 sizeHint = 1;
-            if (sizeHint > buffer.Length)
+            if (sizeHint > buffer.Length - position)
             {
-                int currentLength = FreeCapacity, growBy = Math.Max(currentLength, sizeHint);
-                if (currentLength == 0)
-                    growBy = Math.Max(growBy, DefaultInitialBufferSize);
-                var newSize = currentLength + growBy;
-                if ((uint)newSize > int.MaxValue)
-                {
-                    newSize = currentLength + sizeHint;
-                    if ((uint)newSize > int.MaxValue)
-                        throw new OutOfMemoryException();
-                }
-
-                Resize(newSize);
+                buffer.Dispose();
+                buffer = allocator(sizeHint);
             }
-        }
 
-        public Memory<byte> GetMemory(int sizeHint)
-        {
-            CheckAndResizeBuffer(sizeHint);
-            return buffer.Memory.Slice(0, position);
+            return buffer.Memory.Slice(position);
         }
 
         public Span<byte> GetSpan(int sizeHint) => GetMemory(sizeHint).Span;
@@ -77,11 +48,13 @@ namespace DotNext.Buffers
 
             output.Write(buffer.Memory.Span.Slice(position, count));
             position += count;
+
             // release buffer if it is cannot be reused
             if (position == buffer.Length)
             {
                 buffer.Dispose();
                 buffer = default;
+                position = 0;
             }
         }
     }
