6a7a49d06970a140695d8931eb25a733ed9b84e1
Roman Sakno <roman.sakno@gmail.com>
2020-06-07 16:18:28 +0300

    Unit tests for additional memory renting methods

--- src/DotNext.Tests/Buffers/MemoryOwnerTests.cs
+++ src/DotNext.Tests/Buffers/MemoryOwnerTests.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Buffers;
 using System.Diagnostics.CodeAnalysis;
 using Xunit;
@@ -27,6 +28,25 @@ namespace DotNext.Buffers
         public static void RentFromMemoryPool()
         {
             using var owner = MemoryPool<byte>.Shared.ToAllocator().Invoke(10);
+            Equal(10, owner.Memory.Length);
+            owner[1] = 42;
+            Equal(42, owner.Memory.Span[1]);
+        }
+
+        [Fact]
+        public static void RentFromMemoryPool2()
+        {
+            using var owner = new MemoryOwner<byte>(MemoryPool<byte>.Shared.Rent, 10);
+            Equal(10, owner.Memory.Length);
+            owner[1] = 42;
+            Equal(42, owner.Memory.Span[1]);
+        }
+
+        [Fact]
+        public static void RentFromMemoryPool3()
+        {
+            Func<int, IMemoryOwner<byte>> provider = MemoryPool<byte>.Shared.Rent; 
+            using var owner = provider.ToAllocator().Invoke(10);
             Equal(10, owner.Memory.Length);
             owner[1] = 42;
             Equal(42, owner.Memory.Span[1]);
