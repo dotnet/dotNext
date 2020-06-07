ac2aaa6db1b24061000b362685e187974bbaeaaf
Roman Sakno <roman.sakno@gmail.com>
2020-06-07 23:23:55 +0300

    Representing MMF content as ReadOnlySequence

--- src/DotNext.Unsafe/IO/MemoryMappedFiles/MemoryMappedFileExtensions.cs
+++ src/DotNext.Unsafe/IO/MemoryMappedFiles/MemoryMappedFileExtensions.cs
@@ -1,3 +1,4 @@
+using System;
 using System.IO.MemoryMappedFiles;
 
 namespace DotNext.IO.MemoryMappedFiles
@@ -15,8 +16,16 @@ namespace DotNext.IO.MemoryMappedFiles
         /// <param name="size">The size of the view. Specify 0 (zero) to create a view that starts at offset and ends approximately at the end of the memory-mapped file.</param>
         /// <param name="access">the type of access allowed to the memory-mapped file.</param>
         /// <returns>The direct accessor.</returns>
+        /// <exception cref="ArgumentOutOfRangeException"><paramref name="offset"/> or <paramref name="size"/> is less than zero.</exception>
         public static MemoryMappedDirectAccessor CreateDirectAccessor(this MemoryMappedFile file, long offset = 0, long size = 0, MemoryMappedFileAccess access = MemoryMappedFileAccess.ReadWrite)
-            => new MemoryMappedDirectAccessor(file.CreateViewAccessor(offset, size, access));
+        {
+            if (offset < 0L)
+                throw new ArgumentOutOfRangeException(nameof(offset));
+            if (size < 0L)
+                throw new ArgumentOutOfRangeException(nameof(size));
+
+            return new MemoryMappedDirectAccessor(file.CreateViewAccessor(offset, size, access));
+        }
 
         /// <summary>
         /// Creates memory accessor to the virtual memory associated with the memory-mapped file.
@@ -29,7 +38,41 @@ namespace DotNext.IO.MemoryMappedFiles
         /// <param name="size">The size of the view. Specify 0 (zero) to create a view that starts at offset and ends approximately at the end of the memory-mapped file.</param>
         /// <param name="access">the type of access allowed to the memory-mapped file.</param>
         /// <returns>The direct accessor.</returns>
+        /// <exception cref="ArgumentOutOfRangeException"><paramref name="offset"/> or <paramref name="size"/> is less than zero.</exception>
         public static IMappedMemoryOwner CreateMemoryAccessor(this MemoryMappedFile file, long offset = 0, int size = 0, MemoryMappedFileAccess access = MemoryMappedFileAccess.ReadWrite)
-            => new MappedMemoryOwner(file.CreateViewAccessor(offset, size, access));
+        {
+            if (offset < 0L)
+                throw new ArgumentOutOfRangeException(nameof(offset));
+            if (size < 0)
+                throw new ArgumentOutOfRangeException(nameof(size));
+
+            return new MappedMemoryOwner(file.CreateViewAccessor(offset, size, access));
+        }
+
+        /// <summary>
+        /// Creates an accessor over memory-mapped file segments represented
+        /// as <see cref="System.Buffers.ReadOnlySequence{T}"/>.
+        /// </summary>
+        /// <param name="file">The memory-mapped file.</param>
+        /// <param name="segmentSize">
+        /// The size of single segment, in bytes, that can be returned by <see cref="System.Buffers.ReadOnlySequence{T}"/>
+        /// as contiguous block of memory. So this parameter defines actual amount of occupied virtual memory.
+        /// </param>
+        /// <param name="size">The observable length, in bytes, of memory-mapped file.</param>
+        /// <returns>The object providing access to memory-mapped file via <see cref="System.Buffers.ReadOnlySequence{T}"/>.</returns>
+        /// <exception cref="ArgumentOutOfRangeException">
+        /// <paramref name="segmentSize"/> is less than or equal to zero;
+        /// or <paramref name="size"/> is less than or equal to zero;
+        /// or <paramref name="segmentSize"/> is greater than <paramref name="size"/>.
+        /// </exception>
+        public static ReadOnlySequenceAccessor CreateSequenceAccessor(this MemoryMappedFile file, int segmentSize, long size)
+        {
+            if (segmentSize <= 0 || segmentSize > size)
+                throw new ArgumentOutOfRangeException(nameof(segmentSize));
+            if (size <= 0)
+                throw new ArgumentOutOfRangeException(nameof(size));
+
+            return new ReadOnlySequenceAccessor(file, segmentSize, size);
+        }
     }
 }
