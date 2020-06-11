bb487f398fa83068b4976191c21909b5b90a34d4
Roman Sakno <roman.sakno@gmail.com>
2020-06-11 22:09:33 +0300

    API for writing ReadOnlySequence to TextWriter

---
+++ src/DotNext.Tests/IO/TextWriterExtensionsTests.cs
@@ -0,0 +1,21 @@
+using System.Diagnostics.CodeAnalysis;
+using System.IO;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace DotNext.IO
+{
+    [ExcludeFromCodeCoverage]
+    public sealed class TextWriterExtensionsTests : Test
+    {
+        [Fact]
+        public static async Task WriteSequence()
+        {
+            const string value = "abcdefg";
+            var sequence = value.Split(3).ToReadOnlySequence();
+            await using var writer = new StringWriter();
+            await writer.WriteAsync(sequence);
+            Equal(value, writer.ToString());
+        }
+    }
+}
