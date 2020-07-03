34aa79473e844cf2e4bc576ea482368f78ff8ab6
Roman Sakno <roman.sakno@gmail.com>
2020-07-03 19:12:35 +0300

    Removed unsafe context

--- src/DotNext/StringExtensions.cs
+++ src/DotNext/StringExtensions.cs
@@ -77,7 +77,7 @@ namespace DotNext
         /// <param name="str">The string data.</param>
         /// <returns>The managed pointer to the first character in the string.</returns>
         [Obsolete("Use String.GetPinnableReference method instead")]
-        public static unsafe ref readonly char GetRawData(this string str)
+        public static ref readonly char GetRawData(this string str)
             => ref GetReference(str.AsSpan());
 
         /// <summary>
