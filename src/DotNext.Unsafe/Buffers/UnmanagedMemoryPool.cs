e61dfcf4ca710a629750c8177ce9fc836367f86a
Roman Sakno <roman.sakno@gmail.com>
2020-07-13 09:50:20 +0300

    Removed inline IL code

--- src/DotNext.Unsafe/Buffers/UnmanagedMemoryPool.cs
+++ src/DotNext.Unsafe/Buffers/UnmanagedMemoryPool.cs
@@ -2,10 +2,6 @@
 using System.Buffers;
 using System.Runtime.CompilerServices;
 using System.Threading;
-using static InlineIL.IL;
-using static InlineIL.IL.Emit;
-using static InlineIL.MethodRef;
-using static InlineIL.TypeRef;
 
 namespace DotNext.Buffers
 {
@@ -41,18 +37,7 @@ namespace DotNext.Buffers
         /// <param name="zeroMem"><see langword="true"/> to set all bits in the memory to zero; otherwise, <see langword="false"/>.</param>
         /// <returns>The unmanaged memory allocator.</returns>
         public static MemoryAllocator<T> GetAllocator(bool zeroMem)
-        {
-            Push(CreateProvider(zeroMem));
-            Ldftn(Method(Type<UnmanagedMemoryPool<T>>(), nameof(Allocate), Type<Func<int, IUnmanagedMemoryOwner<T>>>(), Type<int>()));
-            Newobj(Constructor(Type<MemoryAllocator<T>>(), Type<object>(), Type<IntPtr>()));
-            return Return<MemoryAllocator<T>>();
-
-            static Func<int, IUnmanagedMemoryOwner<T>> CreateProvider(bool zeroMem)
-                => new Func<int, IUnmanagedMemoryOwner<T>>(length => Allocate(length, zeroMem));
-        }
-
-        private static MemoryOwner<T> Allocate(Func<int, IUnmanagedMemoryOwner<T>> provider, int length)
-            => new MemoryOwner<T>(provider, length);
+            => new Func<int, IUnmanagedMemoryOwner<T>>(length => Allocate(length, zeroMem)).ToAllocator();
 
         [MethodImpl(MethodImplOptions.Synchronized)]
         private void RemoveTracking(IUnmanagedMemoryOwner<T> owner)
