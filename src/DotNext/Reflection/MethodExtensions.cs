a72670866271cbf486d6a5f4f2e9acbd30de7738
Roman Sakno <roman.sakno@gmail.com>
2020-09-03 12:35:48 +0300

    Safe way to invoke method via reflection

--- src/DotNext/Reflection/MethodExtensions.cs
+++ src/DotNext/Reflection/MethodExtensions.cs
@@ -59,5 +59,32 @@ namespace DotNext.Reflection
         /// <returns><see langword="true"/>, if both methods have the same number of formal parameters, parameters are equal by type and return types are equal; otherwise, <see langword="false"/>.</returns>
         public static bool SignatureEquals(this MethodInfo method, MethodInfo other, bool respectCallingConvention = false)
             => SignatureEquals(method.As<MethodBase>(), other, respectCallingConvention) && method.ReturnType == other.ReturnType;
+    
+        /// <summary>
+        /// Invokes the method or constructor represented by the current instance, using the specified arguments.
+        /// </summary>
+        /// <remarks>
+        /// This method never throws an exception.
+        /// Examine <see cref="Result{T}.IsSuccessful"/> or <see cref="Result{T}.Error"/> instead.
+        /// </remarks>
+        /// <param name="method">The method or constructor to invoke.</param>
+        /// <param name="obj">
+        /// The object on which to invoke the method or constructor. If a method is static,
+        /// this argument is ignored. If a constructor is static, this argument must be <see langword="null"/>
+        /// or an instance of the class that defines the constructor.
+        /// </param>
+        /// <param name="args">An argument list for the invoked method or constructor.</param>
+        /// <returns>An object containing the return value of the invoked method, or <see langword="null"/> in the case of a constructor.</returns>
+        public static Result<object?> TryInvoke(this MethodBase method, object? obj, params object?[] args)
+        {
+            try
+            {
+                return method.Invoke(obj, args);
+            }
+            catch (Exception e)
+            {
+                return new Result<object?>(e);
+            }
+        }
     }
 }
