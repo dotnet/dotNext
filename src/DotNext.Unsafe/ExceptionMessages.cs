26b67e1045ed22e98aaacfb0b893147f3aeb1811
Roman Sakno <roman.sakno@gmail.com>
2020-09-11 14:58:19 +0300

    Migration to new resource extraction API

--- src/DotNext.Unsafe/ExceptionMessages.cs
+++ src/DotNext.Unsafe/ExceptionMessages.cs
@@ -1,29 +1,28 @@
-﻿using System;
-using System.Diagnostics.CodeAnalysis;
+﻿using System.Diagnostics.CodeAnalysis;
 using System.Reflection;
 using System.Resources;
 
 namespace DotNext
 {
-    [SuppressMessage("Globalization", "CA1304", Justification = "This is culture-specific resource strings")]
-    [SuppressMessage("Globalization", "CA1305", Justification = "This is culture-specific resource strings")]
+    using static Resources.ResourceManagerExtensions;
+
     [ExcludeFromCodeCoverage]
     internal static class ExceptionMessages
     {
         private static readonly ResourceManager Resources = new ResourceManager("DotNext.ExceptionMessages", Assembly.GetExecutingAssembly());
 
-        internal static string NullSource => Resources.GetString("NullSource");
+        internal static string NullSource => (string)Resources.Get();
 
-        internal static string NullDestination => Resources.GetString("NullDestination");
+        internal static string NullDestination => (string)Resources.Get();
 
-        internal static string WrongTargetTypeSize => Resources.GetString("WrongTargetTypeSize");
+        internal static string WrongTargetTypeSize => (string)Resources.Get();
 
-        internal static string NullPtr => Resources.GetString("NullPtr");
+        internal static string NullPtr => (string)Resources.Get();
 
-        internal static string StreamNotReadable => Resources.GetString("StreamNotReadable");
+        internal static string StreamNotReadable => (string)Resources.Get();
 
-        internal static string StreamNotWritable => Resources.GetString("StreamNotWritable");
+        internal static string StreamNotWritable => (string)Resources.Get();
 
-        internal static string SegmentVeryLarge => Resources.GetString("SegmentVeryLarge");
+        internal static string SegmentVeryLarge => (string)Resources.Get();
     }
 }
