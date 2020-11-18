9e97c117612f63d0aaae36d1f0dc8c8b2b19ddec
sakno <roman.sakno@gmail.com>
2020-11-18 23:29:41 +0200

    Fixed typo in docs

--- src/DotNext.Unsafe/Runtime/InteropServices/IUnmanagedMemory.cs
+++ src/DotNext.Unsafe/Runtime/InteropServices/IUnmanagedMemory.cs
@@ -19,26 +19,26 @@ namespace DotNext.Runtime.InteropServices
         /// <summary>
         /// Sets all bits of allocated memory to zero.
         /// </summary>
-        /// <exception cref="ObjectDisposedException">The underlying unmanaged memory is released.</exception>
+        /// <exception cref="ObjectDisposedException">The underlying unmanaged memory has been released.</exception>
         void Clear() => Pointer.Clear(Size);
 
         /// <summary>
         /// Gets a pointer to the allocated unmanaged memory.
         /// </summary>
-        /// <exception cref="ObjectDisposedException">The underlying unmanaged memory is released.</exception>
+        /// <exception cref="ObjectDisposedException">The underlying unmanaged memory has been released.</exception>
         Pointer<byte> Pointer { get; }
 
         /// <summary>
         /// Gets a span of bytes from the current instance.
         /// </summary>
-        /// <exception cref="ObjectDisposedException">The underlying unmanaged memory is released.</exception>
+        /// <exception cref="ObjectDisposedException">The underlying unmanaged memory has been released.</exception>
         Span<byte> Bytes { get; }
 
         /// <summary>
         /// Represents unmanaged memory as stream.
         /// </summary>
         /// <returns>The stream of unmanaged memory.</returns>
-        /// <exception cref="ObjectDisposedException">The underlying unmanaged memory is released.</exception>
+        /// <exception cref="ObjectDisposedException">The underlying unmanaged memory has been released.</exception>
         Stream AsStream();
 
         /// <inheritdoc/>
@@ -48,7 +48,7 @@ namespace DotNext.Runtime.InteropServices
         /// Copies bytes from the memory location to the stream.
         /// </summary>
         /// <param name="destination">The destination stream.</param>
-        /// <exception cref="ObjectDisposedException">The underlying unmanaged memory is released.</exception>
+        /// <exception cref="ObjectDisposedException">The underlying unmanaged memory has been released.</exception>
         void WriteTo(Stream destination) => Pointer.WriteTo(destination, Size);
 
         /// <summary>
@@ -57,7 +57,7 @@ namespace DotNext.Runtime.InteropServices
         /// <param name="destination">The destination stream.</param>
         /// <param name="token">The token that can be used to cancel operation.</param>
         /// <returns>The task instance representing asynchronous state of the copying process.</returns>
-        /// <exception cref="ObjectDisposedException">The underlying unmanaged memory is released.</exception>
+        /// <exception cref="ObjectDisposedException">The underlying unmanaged memory has been released.</exception>
         /// <exception cref="OperationCanceledException">The operation has been canceled.</exception>
         ValueTask WriteToAsync(Stream destination, CancellationToken token = default) => Pointer.WriteToAsync(destination, Size, token);
 
@@ -65,7 +65,7 @@ namespace DotNext.Runtime.InteropServices
         /// Copies bytes from the given stream to the memory location identified by this object asynchronously.
         /// </summary>
         /// <param name="source">The source stream.</param>
-        /// <exception cref="ObjectDisposedException">The underlying unmanaged memory is released.</exception>
+        /// <exception cref="ObjectDisposedException">The underlying unmanaged memory has been released.</exception>
         long ReadFrom(Stream source) => Pointer.ReadFrom(source, Size);
 
         /// <summary>
@@ -73,7 +73,7 @@ namespace DotNext.Runtime.InteropServices
         /// </summary>
         /// <param name="source">The source stream.</param>
         /// <param name="token">The token that can be used to cancel operation.</param>
-        /// <exception cref="ObjectDisposedException">The underlying unmanaged memory is released.</exception>
+        /// <exception cref="ObjectDisposedException">The underlying unmanaged memory has been released.</exception>
         /// <exception cref="OperationCanceledException">The operation has been canceled.</exception>
         ValueTask<long> ReadFromAsync(Stream source, CancellationToken token = default) => Pointer.ReadFromAsync(source, Size, token);
 
@@ -81,14 +81,14 @@ namespace DotNext.Runtime.InteropServices
         /// Copies elements from the current memory location to the specified memory location.
         /// </summary>
         /// <param name="destination">The target memory location.</param>
-        /// <exception cref="ObjectDisposedException">The underlying unmanaged memory is released.</exception>
+        /// <exception cref="ObjectDisposedException">The underlying unmanaged memory has been released.</exception>
         void WriteTo(Pointer<byte> destination) => Pointer.WriteTo(destination, Size);
 
         /// <summary>
         /// Copies bytes from the source memory to the memory identified by this object.
         /// </summary>
         /// <param name="source">The pointer to the source unmanaged memory.</param>
-        /// <exception cref="ObjectDisposedException">The underlying unmanaged memory is released.</exception>
+        /// <exception cref="ObjectDisposedException">The underlying unmanaged memory has been released.</exception>
         void ReadFrom(Pointer<byte> source) => source.WriteTo(Pointer, Size);
 
         /// <summary>
@@ -96,7 +96,7 @@ namespace DotNext.Runtime.InteropServices
         /// </summary>
         /// <param name="other">The block of memory to be compared.</param>
         /// <returns><see langword="true"/>, if both memory blocks have the same bytes; otherwise, <see langword="false"/>.</returns>
-        /// <exception cref="ObjectDisposedException">The underlying unmanaged memory is released.</exception>
+        /// <exception cref="ObjectDisposedException">The underlying unmanaged memory has been released.</exception>
         bool BitwiseEquals(IUnmanagedMemory other) => Size == other.Size && Pointer.BitwiseEquals(other.Pointer, Size);
 
         /// <summary>
@@ -104,7 +104,7 @@ namespace DotNext.Runtime.InteropServices
         /// </summary>
         /// <param name="other">The block of memory to be compared.</param>
         /// <returns>Comparison result which has the semantics as return type of <see cref="IComparable.CompareTo(object)"/>.</returns>
-        /// <exception cref="ObjectDisposedException">The underlying unmanaged memory is released.</exception>
+        /// <exception cref="ObjectDisposedException">The underlying unmanaged memory has been released.</exception>
         int BitwiseCompare(IUnmanagedMemory other) => Size == other.Size ? Pointer.BitwiseCompare(other.Pointer, Size) : Size.CompareTo(other.Size);
     }
 
