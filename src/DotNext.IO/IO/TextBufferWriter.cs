4f1cf46a82d8f2cd681fb38927000f2e4e5dc74e
sakno <roman.sakno@gmail.com>
2020-12-10 22:55:31 +0200

    Overrides additional methods to avoid inefficient async operations

--- src/DotNext.IO/IO/TextBufferWriter.cs
+++ src/DotNext.IO/IO/TextBufferWriter.cs
@@ -73,7 +73,40 @@ namespace DotNext.IO
 
         public override void Write(ReadOnlySpan<char> buffer) => writeImpl(buffer, writer);
 
+        public override void WriteLine() => Write(new ReadOnlySpan<char>(CoreNewLine));
+
+        public override Task WriteLineAsync()
+        {
+            var result = Task.CompletedTask;
+            try
+            {
+                WriteLine();
+            }
+            catch (Exception e)
+            {
+                result = Task.FromException(e);
+            }
+
+            return result;
+        }
+
         public override void Write(char value) => Write(CreateReadOnlySpan(ref value, 1));
+
+        public override Task WriteLineAsync(char value)
+        {
+            var result = Task.CompletedTask;
+            try
+            {
+                Write(value);
+                WriteLine();
+            }
+            catch (Exception e)
+            {
+                result = Task.FromException(e);
+            }
+
+            return result;
+        }
 
         public override void Write(bool value) => Write(value ? bool.TrueString : bool.FalseString);
 
@@ -83,6 +116,22 @@ namespace DotNext.IO
         public override void Write(char[]? buffer) => Write(new ReadOnlySpan<char>(buffer));
 
         public override void Write(string? value) => Write(value.AsSpan());
+
+        public override Task WriteLineAsync(string? value)
+        {
+            var result = Task.CompletedTask;
+            try
+            {
+                Write(value);
+                WriteLine();
+            }
+            catch (Exception e)
+            {
+                result = Task.FromException(e);
+            }
+
+            return result;
+        }
 
         public override void Write(decimal value)
             => writer.WriteDecimal(value, string.Empty, FormatProvider);
@@ -162,6 +211,12 @@ namespace DotNext.IO
             }
         }
 
+        public override void WriteLine(object? value)
+        {
+            Write(value);
+            WriteLine();
+        }
+
         public override void WriteLine(ReadOnlySpan<char> buffer)
         {
             Write(buffer);
@@ -224,6 +279,22 @@ namespace DotNext.IO
         public override Task WriteAsync(char[] buffer, int index, int count)
             => WriteAsync(buffer.AsMemory(index, count), CancellationToken.None);
 
+        public override Task WriteLineAsync(char[] buffer, int index, int count)
+        {
+            var result = Task.CompletedTask;
+            try
+            {
+                Write(buffer, index, count);
+                WriteLine();
+            }
+            catch (Exception e)
+            {
+                result = Task.FromException(e);
+            }
+
+            return result;
+        }
+
         public override Task WriteLineAsync(ReadOnlyMemory<char> buffer, CancellationToken token)
         {
             Task result;
@@ -246,6 +317,69 @@ namespace DotNext.IO
 
             return result;
         }
+
+#if !NETSTANDARD2_1
+        public override void Write(StringBuilder? sb)
+        {
+            if (sb.IsNullOrEmpty())
+                return;
+
+            foreach (var chunk in sb.GetChunks())
+                writeImpl(chunk.Span, writer);
+        }
+
+        public override void WriteLine(StringBuilder? sb)
+        {
+            Write(sb);
+            WriteLine();
+        }
+
+        public override Task WriteAsync(StringBuilder? value, CancellationToken token)
+        {
+            Task result;
+            if (token.IsCancellationRequested)
+            {
+                result = Task.FromCanceled(token);
+            }
+            else
+            {
+                result = Task.CompletedTask;
+                try
+                {
+                    Write(value);
+                }
+                catch (Exception e)
+                {
+                    result = Task.FromException(e);
+                }
+            }
+
+            return result;
+        }
+
+        public override Task WriteLineAsync(StringBuilder? value, CancellationToken token)
+        {
+            Task result;
+            if (token.IsCancellationRequested)
+            {
+                result = Task.FromCanceled(token);
+            }
+            else
+            {
+                result = Task.CompletedTask;
+                try
+                {
+                    WriteLine(value);
+                }
+                catch (Exception e)
+                {
+                    result = Task.FromException(e);
+                }
+            }
+
+            return result;
+        }
+#endif
 
         public override string ToString()
             => writer is IGrowableBuffer<char> buffer ? buffer.BuildString() : writer.ToString() ?? string.Empty;
