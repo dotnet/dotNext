1f002e3c3862c59b71c1960aaf421ce65d2b5a50
sakno <roman.sakno@gmail.com>
2020-12-14 23:37:27 +0200

    Removed obsolete tests

--- src/DotNext.Tests/Buffers/ChunkSequenceTests.cs
+++ src/DotNext.Tests/Buffers/ChunkSequenceTests.cs
@@ -1,9 +1,6 @@
 ﻿using System;
 using System.Buffers;
 using System.Diagnostics.CodeAnalysis;
-using System.IO;
-using System.Text;
-using System.Threading.Tasks;
 using Xunit;
 
 namespace DotNext.Buffers
@@ -11,105 +8,6 @@ namespace DotNext.Buffers
     [ExcludeFromCodeCoverage]
     public sealed class ChunkSequenceTests : Test
     {
-        [Fact]
-        [Obsolete("This is the test for deprecated data type")]
-        public static void EmptySequence()
-        {
-            var sequence = default(ChunkSequence<char>);
-            Empty(sequence);
-        }
-
-        [Fact]
-        [Obsolete("This is the test for deprecated method")]
-        public static void AsReadOnlySequence()
-        {
-            var sequence = StringExtensions.Split("abcde", 2).ToReadOnlySequence();
-            Equal(5, sequence.Length);
-        }
-
-        [Fact]
-        [Obsolete("This is the test for deprecated method")]
-        public static void SequenceEnumeration()
-        {
-            var index = 0;
-            foreach (var segment in StringExtensions.Split("abcde", 2))
-                switch (index++)
-                {
-                    case 0:
-                        var array = segment.Span;
-                        Equal(2, array.Length);
-                        Equal('a', array[0]);
-                        Equal('b', array[1]);
-                        break;
-                    case 1:
-                        array = segment.Span;
-                        Equal(2, array.Length);
-                        Equal('c', array[0]);
-                        Equal('d', array[1]);
-                        break;
-                    case 2:
-                        array = segment.Span;
-                        Equal(1, array.Length);
-                        Equal('e', array[0]);
-                        break;
-                }
-            Equal(3, index);
-        }
-
-        [Fact]
-        [Obsolete("This is the test for deprecated method")]
-        public static void SplitMemory()
-        {
-            var sequence = (ReadOnlySequence<char>)StringExtensions.Split("abcde", 2);
-            False(sequence.IsSingleSegment);
-            var index = 0;
-            foreach (var segment in sequence)
-                switch (index++)
-                {
-                    case 0:
-                        var array = segment.Span;
-                        Equal(2, array.Length);
-                        Equal('a', array[0]);
-                        Equal('b', array[1]);
-                        break;
-                    case 1:
-                        array = segment.Span;
-                        Equal(2, array.Length);
-                        Equal('c', array[0]);
-                        Equal('d', array[1]);
-                        break;
-                    case 2:
-                        array = segment.Span;
-                        Equal(1, array.Length);
-                        Equal('e', array[0]);
-                        break;
-                }
-            Equal(3, index);
-        }
-
-        [Fact]
-        [Obsolete("This is the test for deprecated method")]
-        public static async Task CopyByteChunksToStream()
-        {
-            var bytes = new ChunkSequence<byte>(Encoding.UTF8.GetBytes("Hello, world!"), 3);
-            using var content = new MemoryStream();
-            await ChunkSequence.CopyToAsync(bytes, content).ConfigureAwait(false);
-            content.Seek(0, SeekOrigin.Begin);
-            using var reader = new StreamReader(content, Encoding.UTF8, false, 1024, true);
-            Equal("Hello, world!", reader.ReadToEnd());
-        }
-
-        [Fact]
-        [Obsolete("This is the test for deprecated method")]
-        public static async Task CopyCharChunksToStream()
-        {
-            var bytes = new ChunkSequence<char>("Hello, world!".AsMemory(), 3);
-            var sb = new StringBuilder();
-            using (var writer = new StringWriter(sb))
-                await ChunkSequence.CopyToAsync(bytes, writer).ConfigureAwait(false);
-            Equal("Hello, world!", sb.ToString());
-        }
-
         [Fact]
         public static void Concatenation()
         {
