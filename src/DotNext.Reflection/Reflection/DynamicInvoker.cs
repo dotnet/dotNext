8f9d31f325c4183670c8a0fc1315886c33213cf2
sakno <roman.sakno@gmail.com>
2020-12-15 00:09:12 +0200

    Added support of 6 args

--- src/DotNext.Reflection/Reflection/DynamicInvoker.cs
+++ src/DotNext.Reflection/Reflection/DynamicInvoker.cs
@@ -42,8 +42,8 @@ namespace DotNext.Reflection
         /// Invokes the delegate.
         /// </summary>
         /// <param name="invoker">The delegate to invoke.</param>
-        /// <param name="arg">The first argument.</param>
         /// <param name="target">Target object; for static members should be <see langword="null"/>.</param>
+        /// <param name="arg">The first argument.</param>
         /// <returns>The result of member invocation.</returns>
         public static object? Invoke(this DynamicInvoker invoker, object? target, object? arg)
             => invoker(target, MemoryMarshal.CreateSpan(ref arg, 1));
@@ -52,13 +52,79 @@ namespace DotNext.Reflection
         /// Invokes the delegate.
         /// </summary>
         /// <param name="invoker">The delegate to invoke.</param>
-        /// <param name="arg1">The first argument.</param>
-        /// <param name="arg2">The first argument.</param>
         /// <param name="target">Target object; for static members should be <see langword="null"/>.</param>
+        /// <param name="arg1">The first argument.</param>
+        /// <param name="arg2">The second argument.</param>
         /// <returns>The result of member invocation.</returns>
         public static object? Invoke(this DynamicInvoker invoker, object? target, object? arg1, object? arg2)
         {
             var args = (arg1, arg2);
+            return invoker(target, Span.AsSpan(ref args));
+        }
+
+        /// <summary>
+        /// Invokes the delegate.
+        /// </summary>
+        /// <param name="invoker">The delegate to invoke.</param>
+        /// <param name="target">Target object; for static members should be <see langword="null"/>.</param>
+        /// <param name="arg1">The first argument.</param>
+        /// <param name="arg2">The second argument.</param>
+        /// <param name="arg3">The third argument.</param>
+        /// <returns>The result of member invocation.</returns>
+        public static object? Invoke(this DynamicInvoker invoker, object? target, object? arg1, object? arg2, object? arg3)
+        {
+            var args = (arg1, arg2, arg3);
+            return invoker(target, Span.AsSpan(ref args));
+        }
+
+        /// <summary>
+        /// Invokes the delegate.
+        /// </summary>
+        /// <param name="invoker">The delegate to invoke.</param>
+        /// <param name="target">Target object; for static members should be <see langword="null"/>.</param>
+        /// <param name="arg1">The first argument.</param>
+        /// <param name="arg2">The second argument.</param>
+        /// <param name="arg3">The third argument.</param>
+        /// <param name="arg4">The fourth argument.</param>
+        /// <returns>The result of member invocation.</returns>
+        public static object? Invoke(this DynamicInvoker invoker, object? target, object? arg1, object? arg2, object? arg3, object? arg4)
+        {
+            var args = (arg1, arg2, arg3, arg4);
+            return invoker(target, Span.AsSpan(ref args));
+        }
+
+        /// <summary>
+        /// Invokes the delegate.
+        /// </summary>
+        /// <param name="invoker">The delegate to invoke.</param>
+        /// <param name="target">Target object; for static members should be <see langword="null"/>.</param>
+        /// <param name="arg1">The first argument.</param>
+        /// <param name="arg2">The second argument.</param>
+        /// <param name="arg3">The third argument.</param>
+        /// <param name="arg4">The fourth argument.</param>
+        /// <param name="arg5">The fifth argument.</param>
+        /// <returns>The result of member invocation.</returns>
+        public static object? Invoke(this DynamicInvoker invoker, object? target, object? arg1, object? arg2, object? arg3, object? arg4, object? arg5)
+        {
+            var args = (arg1, arg2, arg3, arg4, arg5);
+            return invoker(target, Span.AsSpan(ref args));
+        }
+
+        /// <summary>
+        /// Invokes the delegate.
+        /// </summary>
+        /// <param name="invoker">The delegate to invoke.</param>
+        /// <param name="target">Target object; for static members should be <see langword="null"/>.</param>
+        /// <param name="arg1">The first argument.</param>
+        /// <param name="arg2">The second argument.</param>
+        /// <param name="arg3">The third argument.</param>
+        /// <param name="arg4">The fourth argument.</param>
+        /// <param name="arg5">The fifth argument.</param>
+        /// <param name="arg6">The fifth argument.</param>
+        /// <returns>The result of member invocation.</returns>
+        public static object? Invoke(this DynamicInvoker invoker, object? target, object? arg1, object? arg2, object? arg3, object? arg4, object? arg5, object? arg6)
+        {
+            var args = (arg1, arg2, arg3, arg4, arg5);
             return invoker(target, Span.AsSpan(ref args));
         }
     }
