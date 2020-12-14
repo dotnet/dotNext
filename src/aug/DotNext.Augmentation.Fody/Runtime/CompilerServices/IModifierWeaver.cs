7a7e6ffe9dc3541cadff7805c91ac013249ea583
sakno <roman.sakno@gmail.com>
2020-12-14 22:26:43 +0200

    Removed obsolete packages

--- src/aug/DotNext.Augmentation.Fody/Runtime/CompilerServices/IModifierWeaver.cs
+++
@@ -1,9 +0,0 @@
-﻿using Mono.Cecil;
-
-namespace DotNext.Runtime.CompilerServices
-{
-    internal interface IModifierWeaver
-    {
-        void AttachModifier(TypeReference modifierType, bool required);
-    }
-}
