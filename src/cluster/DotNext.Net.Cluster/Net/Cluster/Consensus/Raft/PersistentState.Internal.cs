b6b662c849ea8e071295d3b045e16aa36f0e0dd9
sakno <roman.sakno@gmail.com>
2020-12-18 20:56:39 +0200

    Use delegating stream to hide the underlying storage

--- src/cluster/DotNext.Net.Cluster/Net/Cluster/Consensus/Raft/PersistentState.Internal.cs
+++ src/cluster/DotNext.Net.Cluster/Net/Cluster/Consensus/Raft/PersistentState.Internal.cs
@@ -48,11 +48,11 @@ namespace DotNext.Net.Cluster.Consensus.Raft
             internal static int Size => Unsafe.SizeOf<SnapshotMetadata>();
         }
 
-        private abstract class ConcurrentStorageAccess : Disposable, IAsyncDisposable, IFlushable
+        private abstract class ConcurrentStorageAccess : Stream, IFlushable
         {
             // do not derive from FileStream because some virtual methods
             // assumes that they are overridden and do async calls inefficiently
-            private protected readonly FileStream fs;
+            private readonly FileStream fs;
             private readonly StreamSegment[] readers;   // a pool of read-only streams that can be shared between multiple readers in parallel
 
             [SuppressMessage("Reliability", "CA2000", Justification = "All streams are disposed in Dispose method")]
@@ -71,11 +71,89 @@ namespace DotNext.Net.Cluster.Consensus.Raft
                 }
             }
 
-            internal long Length => fs.Length;
+            public sealed override bool CanRead => fs.CanRead;
 
-            public Task FlushAsync(CancellationToken token = default) => fs.FlushAsync(token);
+            public sealed override bool CanWrite => fs.CanWrite;
 
-            public void Flush() => fs.Flush(true);
+            public sealed override bool CanSeek => fs.CanSeek;
+
+            public sealed override bool CanTimeout => fs.CanTimeout;
+
+            public sealed override long Length => fs.Length;
+
+            public sealed override long Position
+            {
+                get => fs.Position;
+                set => fs.Position = value;
+            }
+
+            public sealed override int ReadTimeout
+            {
+                get => fs.ReadTimeout;
+                set => fs.ReadTimeout = value;
+            }
+
+            public sealed override int WriteTimeout
+            {
+                get => fs.WriteTimeout;
+                set => fs.WriteTimeout = value;
+            }
+
+            public sealed override void SetLength(long length) => fs.SetLength(length);
+
+            public sealed override long Seek(long offset, SeekOrigin origin)
+                => fs.Seek(offset, origin);
+
+            public sealed override IAsyncResult BeginRead(byte[] buffer, int offset, int count, AsyncCallback? callback, object? state)
+                => fs.BeginRead(buffer, offset, count, callback, state);
+
+            public sealed override int EndRead(IAsyncResult asyncResult)
+                => fs.EndRead(asyncResult);
+
+            public sealed override int Read(Span<byte> buffer)
+                => fs.Read(buffer);
+
+            public sealed override int Read(byte[] buffer, int offset, int count)
+                => fs.Read(buffer, offset, count);
+
+            public sealed override Task<int> ReadAsync(byte[] buffer, int offset, int count, CancellationToken token)
+                => fs.ReadAsync(buffer, offset, count, token);
+
+            public sealed override ValueTask<int> ReadAsync(Memory<byte> buffer, CancellationToken token)
+                => fs.ReadAsync(buffer, token);
+
+            public sealed override int ReadByte() => fs.ReadByte();
+
+            public sealed override IAsyncResult BeginWrite(byte[] buffer, int offset, int count, AsyncCallback? callback, object? state)
+                => fs.BeginWrite(buffer, offset, count, callback, state);
+
+            public sealed override void EndWrite(IAsyncResult asyncResult)
+                => fs.EndWrite(asyncResult);
+
+            public sealed override void Write(ReadOnlySpan<byte> buffer)
+                => fs.Write(buffer);
+
+            public sealed override void Write(byte[] buffer, int offset, int count)
+                => fs.Write(buffer, offset, count);
+
+            public sealed override void WriteByte(byte value)
+                => fs.WriteByte(value);
+
+            public sealed override Task WriteAsync(byte[] buffer, int offset, int count, CancellationToken cancellationToken)
+                => fs.WriteAsync(buffer, offset, count, cancellationToken);
+
+            public sealed override ValueTask WriteAsync(ReadOnlyMemory<byte> buffer, CancellationToken cancellationToken = default)
+                => fs.WriteAsync(buffer, cancellationToken);
+
+            public sealed override Task CopyToAsync(Stream destination, int bufferSize, CancellationToken cancellationToken)
+                => fs.CopyToAsync(destination, bufferSize, cancellationToken);
+
+            public sealed override void CopyTo(Stream destination, int bufferSize)
+                => fs.CopyTo(destination, bufferSize);
+
+            public sealed override Task FlushAsync(CancellationToken token = default) => fs.FlushAsync(token);
+
+            public sealed override void Flush() => fs.Flush(true);
 
             internal string FileName => fs.Name;
 
@@ -103,7 +181,7 @@ namespace DotNext.Net.Cluster.Consensus.Raft
                 base.Dispose(disposing);
             }
 
-            public async ValueTask DisposeAsync()
+            public override async ValueTask DisposeAsync()
             {
                 foreach (Stream? reader in readers)
                 {
--- src/cluster/DotNext.Net.Cluster/Net/Cluster/Consensus/Raft/PersistentState.Partition.cs
+++ src/cluster/DotNext.Net.Cluster/Net/Cluster/Consensus/Raft/PersistentState.Partition.cs
@@ -42,7 +42,7 @@ namespace DotNext.Net.Cluster.Consensus.Raft
 
             internal long LastIndex => FirstIndex + Capacity - 1;
 
-            internal void Allocate(long initialSize) => fs.SetLength(initialSize + PayloadOffset);
+            internal void Allocate(long initialSize) => SetLength(initialSize + PayloadOffset);
 
             private void PopulateCache(Span<byte> buffer, Span<LogEntryMetadata> lookupCache)
             {
@@ -51,7 +51,7 @@ namespace DotNext.Net.Cluster.Consensus.Raft
                     count = Math.Min(buffer.Length / LogEntryMetadata.Size, lookupCache.Length - index);
                     var maxBytes = count * LogEntryMetadata.Size;
                     var source = buffer.Slice(0, maxBytes);
-                    if (fs.Read(source) < maxBytes)
+                    if (Read(source) < maxBytes)
                         throw new EndOfStreamException();
                     var destination = AsBytes(lookupCache.Slice(index));
                     source.CopyTo(destination);
@@ -107,8 +107,8 @@ namespace DotNext.Net.Cluster.Consensus.Raft
                 else if (lookupCache.IsEmpty)
                 {
                     // read content offset and the length of the previous entry
-                    fs.Position = (index - 1) * LogEntryMetadata.Size;
-                    metadata = await fs.ReadAsync<LogEntryMetadata>(buffer).ConfigureAwait(false);
+                    Position = (index - 1) * LogEntryMetadata.Size;
+                    metadata = await this.ReadAsync<LogEntryMetadata>(buffer).ConfigureAwait(false);
                     Debug.Assert(metadata.Offset > 0, "Previous entry doesn't exist for unknown reason");
                     offset = metadata.Length + metadata.Offset;
                 }
@@ -120,13 +120,13 @@ namespace DotNext.Net.Cluster.Consensus.Raft
                 }
 
                 // write content
-                fs.Position = offset;
-                await entry.WriteToAsync(fs, buffer).ConfigureAwait(false);
-                metadata = LogEntryMetadata.Create(entry, offset, fs.Position - offset);
+                Position = offset;
+                await entry.WriteToAsync(this, buffer).ConfigureAwait(false);
+                metadata = LogEntryMetadata.Create(entry, offset, Position - offset);
 
                 // record new log entry to the allocation table
-                fs.Position = index * LogEntryMetadata.Size;
-                await fs.WriteAsync(metadata, buffer).ConfigureAwait(false);
+                Position = index * LogEntryMetadata.Size;
+                await this.WriteAsync(metadata, buffer).ConfigureAwait(false);
 
                 // update cache
                 if (!lookupCache.IsEmpty)
@@ -166,17 +166,17 @@ namespace DotNext.Net.Cluster.Consensus.Raft
             }
 
             internal override void PopulateCache(in DataAccessSession session)
-                => Index = fs.Length > 0L ? fs.Read<SnapshotMetadata>().Index : 0L;
+                => Index = Length > 0L ? this.Read<SnapshotMetadata>().Index : 0L;
 
             private async ValueTask WriteAsync<TEntry>(TEntry entry, long index, Memory<byte> buffer, CancellationToken token)
                 where TEntry : notnull, IRaftLogEntry
             {
                 Index = index;
-                fs.Position = SnapshotMetadata.Size;
-                await entry.WriteToAsync(fs, buffer, token).ConfigureAwait(false);
-                var metadata = SnapshotMetadata.Create(entry, index, fs.Length - SnapshotMetadata.Size);
-                fs.Position = 0;
-                await fs.WriteAsync(metadata, buffer, token).ConfigureAwait(false);
+                Position = SnapshotMetadata.Size;
+                await entry.WriteToAsync(this, buffer, token).ConfigureAwait(false);
+                var metadata = SnapshotMetadata.Create(entry, index, Length - SnapshotMetadata.Size);
+                Position = 0;
+                await this.WriteAsync(metadata, buffer, token).ConfigureAwait(false);
             }
 
             internal ValueTask WriteAsync<TEntry>(in DataAccessSession session, TEntry entry, long index, CancellationToken token)
