3b4f804e852472bfc9e931b640543a1767d63877
sakno <roman.sakno@gmail.com>
2020-12-21 13:08:54 +0200

    Added stress test for sparse buffer

--- src/DotNext.Tests/Buffers/SparseBufferWriterTests.cs
+++ src/DotNext.Tests/Buffers/SparseBufferWriterTests.cs
@@ -5,6 +5,8 @@ using Xunit;
 
 namespace DotNext.Buffers
 {
+    using static IO.StreamSource;
+
     [ExcludeFromCodeCoverage]
     public sealed class SparseBufferWriterTests : Test
     {
@@ -17,6 +19,23 @@ namespace DotNext.Buffers
             var sequence = ToReadOnlySequence(new ReadOnlyMemory<byte>(RandomBytes(5000)), 1000);
             writer.Write(in sequence, copyMemory);
             Equal(sequence.ToArray(), writer.ToReadOnlySequence().ToArray());
+        }
+
+        [Theory]
+        [InlineData(1000)]
+        [InlineData(10_000)]
+        [InlineData(100_000)]
+        public static void StressTest(int totalSize)
+        {
+            using var writer = new SparseBufferWriter<byte>();
+            using var output = writer.AsStream();
+            var data = RandomBytes(2048);
+            for (int remaining = totalSize, take; remaining > 0; remaining -= take)
+            {
+                take = Math.Min(remaining, data.Length);
+                output.Write(data, 0, take);
+                remaining -= take;
+            }
         }
     }
 }
