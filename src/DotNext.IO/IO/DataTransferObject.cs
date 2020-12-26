90331c4a7c85a047481e9191f673d1ec1b9aed76
sakno <roman.sakno@gmail.com>
2020-12-26 17:40:53 +0200

    Ability to obtain DTO as raw memory

--- src/DotNext.IO/IO/DataTransferObject.cs
+++ src/DotNext.IO/IO/DataTransferObject.cs
@@ -9,8 +9,7 @@ using System.Threading.Tasks;
 
 namespace DotNext.IO
 {
-    using ByteBuffer = Buffers.MemoryOwner<byte>;
-    using ByteBufferWriter = Buffers.PooledArrayBufferWriter<byte>;
+    using Buffers;
     using PipeBinaryWriter = Pipelines.PipeBinaryWriter;
 
     /// <summary>
@@ -42,8 +41,8 @@ namespace DotNext.IO
                 where TReader : IAsyncBinaryReader
             {
                 using var writer = capacity.HasValue ?
-                    new ByteBufferWriter(capacity.GetValueOrDefault()) :
-                    new ByteBufferWriter();
+                    new PooledArrayBufferWriter<byte>(capacity.GetValueOrDefault()) :
+                    new PooledArrayBufferWriter<byte>();
                 await reader.CopyToAsync(writer, token).ConfigureAwait(false);
                 return writer.WrittenCount == 0 ? string.Empty : encoding.GetString(writer.WrittenMemory.Span);
             }
@@ -64,6 +63,35 @@ namespace DotNext.IO
                 await reader.CopyToAsync(ms, token).ConfigureAwait(false);
                 ms.Seek(0, SeekOrigin.Begin);
                 return ms.ToArray();
+            }
+        }
+
+        [StructLayout(LayoutKind.Auto)]
+        private readonly struct MemoryDecoder : IDataTransferObject.IDecoder<MemoryOwner<byte>>
+        {
+            private readonly MemoryAllocator<byte>? allocator;
+
+            internal MemoryDecoder(MemoryAllocator<byte>? allocator)
+                => this.allocator = allocator;
+
+            public async ValueTask<MemoryOwner<byte>> ReadAsync<TReader>(TReader reader, CancellationToken token)
+                where TReader : IAsyncBinaryReader
+            {
+                using var writer = new PooledBufferWriter<byte>(allocator);
+                await reader.CopyToAsync(writer, token).ConfigureAwait(false);
+
+                MemoryOwner<byte> result;
+                if (writer.WrittenCount > 0)
+                {
+                    result = allocator.Invoke(writer.WrittenCount, true);
+                    writer.WrittenMemory.CopyTo(result.Memory);
+                }
+                else
+                {
+                    result = default;
+                }
+
+                return result;
             }
         }
 
@@ -115,7 +143,7 @@ namespace DotNext.IO
         public static async ValueTask WriteToAsync<TObject>(this TObject dto, Stream output, int bufferSize = DefaultBufferSize, CancellationToken token = default)
             where TObject : notnull, IDataTransferObject
         {
-            using var buffer = new ByteBuffer(ArrayPool<byte>.Shared, bufferSize);
+            using var buffer = new MemoryOwner<byte>(ArrayPool<byte>.Shared, bufferSize);
             await WriteToAsync(dto, output, buffer.Memory, token).ConfigureAwait(false);
         }
 
@@ -173,7 +201,7 @@ namespace DotNext.IO
             => dto.GetObjectDataAsync<string, TextDecoder>(new TextDecoder(encoding, capacity), token).AsTask();
 
         /// <summary>
-        /// Converts DTO to array of bytes.
+        /// Converts DTO to an array of bytes.
         /// </summary>
         /// <typeparam name="TObject">The type of data transfer object.</typeparam>
         /// <param name="dto">Data transfer object to read from.</param>
@@ -183,6 +211,19 @@ namespace DotNext.IO
         public static Task<byte[]> ToByteArrayAsync<TObject>(this TObject dto, CancellationToken token = default)
             where TObject : notnull, IDataTransferObject
             => dto.GetObjectDataAsync<byte[], ArrayDecoder>(ArrayDecoder.Instance, token).AsTask();
+
+        /// <summary>
+        /// Converts DTO to a block of memory.
+        /// </summary>
+        /// <typeparam name="TObject">The type of data transfer object.</typeparam>
+        /// <param name="dto">Data transfer object to read from.</param>
+        /// <param name="allocator">The memory allocator.</param>
+        /// <param name="token">The token that can be used to cancel asynchronous operation.</param>
+        /// <returns>The content of the object.</returns>
+        /// <exception cref="OperationCanceledException">The operation has been canceled.</exception>
+        public static Task<MemoryOwner<byte>> ToMemoryAsync<TObject>(this TObject dto, MemoryAllocator<byte>? allocator = null, CancellationToken token = default)
+            where TObject : notnull, IDataTransferObject
+            => dto.GetObjectDataAsync<MemoryOwner<byte>, MemoryDecoder>(new MemoryDecoder(allocator), token).AsTask();
 
         /// <summary>
         /// Converts DTO to value of blittable type.
