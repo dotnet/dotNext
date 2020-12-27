c079a472443ef7661573e0a5ab91551857525ca2
sakno <roman.sakno@gmail.com>
2020-12-27 15:52:15 +0200

    Removed unused namespaces

--- src/cluster/DotNext.Net.Cluster/Net/Cluster/Consensus/Raft/PersistentState.Backup.cs
+++ src/cluster/DotNext.Net.Cluster/Net/Cluster/Consensus/Raft/PersistentState.Backup.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Diagnostics.CodeAnalysis;
 using System.IO;
 using System.IO.Compression;
 using System.Threading;
--- src/cluster/DotNext.Net.Cluster/Net/Cluster/Consensus/Raft/PersistentState.Options.cs
+++ src/cluster/DotNext.Net.Cluster/Net/Cluster/Consensus/Raft/PersistentState.Options.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Buffers;
 using System.IO.Compression;
 
 namespace DotNext.Net.Cluster.Consensus.Raft
