ba25e5a9ff3509b233a2ba7bf79b80e4cb4bb6e6
sakno <roman.sakno@gmail.com>
2020-12-28 15:06:16 +0200

    Fixed adding formatter to the dictionary

--- src/cluster/DotNext.Net.Cluster/Net/Cluster/Consensus/Raft/Commands/CommandRegistry.Builder.cs
+++ src/cluster/DotNext.Net.Cluster/Net/Cluster/Consensus/Raft/Commands/CommandRegistry.Builder.cs
@@ -37,9 +37,10 @@ namespace DotNext.Net.Cluster.Consensus.Raft.Commands
             /// <param name="handler">The command handler.</param>
             /// <param name="formatter">Serializer/deserializer of the command type.</param>
             /// <typeparam name="TCommand">The type of the command supported by the handler.</typeparam>
+            /// <returns>This builder</returns>
             /// <exception cref="ArgumentNullException"><paramref name="handler"/> or <paramref name="formatter"/> is <see langword="null"/>.</exception>
             /// <exception cref="GenericArgumentException">Type <typaparamref name="TCommand"/> is not annotated with <see cref="CommandAttribute"/> attribute.</exception>
-            public void Add<TCommand>(Func<TCommand, CancellationToken, ValueTask> handler, ICommandFormatter<TCommand> formatter)
+            public Builder Add<TCommand>(Func<TCommand, CancellationToken, ValueTask> handler, ICommandFormatter<TCommand> formatter)
                 where TCommand : struct 
             {
                 if (handler is null)
@@ -50,6 +51,7 @@ namespace DotNext.Net.Cluster.Consensus.Raft.Commands
                 var id = typeof(TCommand).GetCustomAttribute<CommandAttribute>()?.Id ?? throw new GenericArgumentException<TCommand>(ExceptionMessages.MissingCommandAttribute<TCommand>());
                 interpreters.Add(id, new CommandHandler<TCommand>(formatter, handler));
                 formatters.Add(typeof(TCommand), FormatterInfo.Create(formatter, id));
+                return this;
             }
 
             /// <summary>
@@ -60,9 +62,10 @@ namespace DotNext.Net.Cluster.Consensus.Raft.Commands
             /// </remarks>
             /// <param name="handler">The command handler.</param>
             /// <typeparam name="TCommand">The type of the command supported by the handler.</typeparam>
+            /// <returns>This builder</returns>
             /// <exception cref="ArgumentNullException"><paramref name="handler"/> is <see langword="null"/>.</exception>
             /// <exception cref="GenericArgumentException">Type <typaparamref name="TCommand"/> is not annotated with <see cref="CommandAttribute"/> attribute or <see cref="CommandAttribute.Formatter"/> refers to the invalid formatter.</exception>
-            public void Add<TCommand>(Func<TCommand, CancellationToken, ValueTask> handler)
+            public Builder Add<TCommand>(Func<TCommand, CancellationToken, ValueTask> handler)
                 where TCommand : struct
             {
                 if (handler is null)
@@ -72,9 +75,13 @@ namespace DotNext.Net.Cluster.Consensus.Raft.Commands
                 if (attr is null || attr.Formatter is null)
                     throw new GenericArgumentException<TCommand>(ExceptionMessages.MissingCommandAttribute<TCommand>());
 
-                var formatter = Activator.CreateInstance(attr.Formatter) ?? throw new GenericArgumentException<TCommand>(ExceptionMessages.MissingCommandAttribute<TCommand>());
+                var formatter = new FormatterInfo(attr);
+                if (formatter.IsEmpty)
+                    throw new GenericArgumentException<TCommand>(ExceptionMessages.MissingCommandFormatter<TCommand>());
+                formatters.Add(typeof(TCommand), formatter);
                 var interp = Activator.CreateInstance(typeof(CommandHandler<>).MakeGenericType(typeof(TCommand)), formatter, handler);
                 interpreters.Add(attr.Id, Cast<CommandHandler>(interp));
+                return this;
             }
 
             /// <summary>
