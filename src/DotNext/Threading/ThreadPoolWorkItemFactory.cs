c9ac479baa2d307ef376abe44d3e1d744b910237
sakno <roman.sakno@gmail.com>
2021-01-08 13:29:55 +0200

    Added support of work item with 3 args

--- src/DotNext/Threading/ThreadPoolWorkItemFactory.cs
+++ src/DotNext/Threading/ThreadPoolWorkItemFactory.cs
@@ -32,6 +32,17 @@ namespace DotNext.Threading
             void IThreadPoolWorkItem.Execute() => invoker(Item1, Item2);
         }
 
+        private sealed class ThreadPoolWorkItem<T1, T2, T3> : Tuple<T1, T2, T3>, IThreadPoolWorkItem
+        {
+            private readonly delegate*<T1, T2, T3, void> invoker;
+
+            internal ThreadPoolWorkItem(delegate*<T1, T2, T3, void> invoker, T1 arg1, T2 arg2, T3 arg3)
+                : base(arg1, arg2, arg3)
+                => this.invoker = invoker;
+
+            void IThreadPoolWorkItem.Execute() => invoker(Item1, Item2, Item3);
+        }
+
         /// <summary>
         /// Creates thread pool work item.
         /// </summary>
@@ -62,6 +73,24 @@ namespace DotNext.Threading
                 throw new ArgumentNullException(nameof(workItem));
 
             return new ThreadPoolWorkItem<T1, T2>(workItem, arg1, arg2);
+        }
+
+        /// <summary>
+        /// Creates thread pool work item.
+        /// </summary>
+        /// <param name="workItem">The pointer to the method implementing work item logic.</param>
+        /// <param name="arg1">The first argument to be passed to the method implementing work item logic.</param>
+        /// <param name="arg2">The second argument to be passed to the method implementing work item logic.</param>
+        /// <typeparam name="T1">The type of the work item first argument.</typeparam>
+        /// <typeparam name="T2">The type of the work item second argument.</typeparam>
+        /// <typeparam name="T3">The type of the work item third argument.</typeparam>
+        /// <returns>The work item.</returns>
+        public static IThreadPoolWorkItem Create<T1, T2, T3>(delegate*<T1, T2, T3, void> workItem, T1 arg1, T2 arg2, T3 arg3)
+        {
+            if (workItem == null)
+                throw new ArgumentNullException(nameof(workItem));
+
+            return new ThreadPoolWorkItem<T1, T2, T3>(workItem, arg1, arg2, arg3);
         }
     }
 #endif
