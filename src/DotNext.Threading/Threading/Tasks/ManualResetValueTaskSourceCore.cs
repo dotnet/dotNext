a2f3d0b85c5420f0e9076ddb575483eb4bcda9fa
sakno <roman.sakno@gmail.com>
2021-01-08 13:06:02 +0200

    Removed pragma usage

--- src/DotNext.Threading/Threading/Tasks/ManualResetValueTaskSourceCore.cs
+++ src/DotNext.Threading/Threading/Tasks/ManualResetValueTaskSourceCore.cs
@@ -7,12 +7,10 @@ namespace DotNext.Threading.Tasks
     internal static class ManualResetValueTaskSourceCore
     {
         // cached to avoid allocations
-        private static readonly Action<object?> Continuation = Invoke;
+        private static readonly Action<object?> Continuation = Invoke!;
 
-#nullable disable
         private static void Invoke(object state)
             => Unsafe.As<Action>(state).Invoke();
-#nullable restore
 
         internal static void OnCompleted<TResult>(this ref ManualResetValueTaskSourceCore<TResult> source, Action callback, short token, ValueTaskSourceOnCompletedFlags flags)
             => source.OnCompleted(Continuation, callback, token, flags);
