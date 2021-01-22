3ade617528e3382f9b812885c7f4bae15215b8bf
sakno <roman.sakno@gmail.com>
2021-01-22 13:38:45 +0200

    Removed Enum member API

---
+++ src/DotNext.Tests/Reflection/EnumTypeTests.cs
@@ -0,0 +1,30 @@
+using System;
+using Xunit;
+
+namespace DotNext.Reflection
+{
+    public sealed class EnumTypeTests : Test
+    {
+        private sealed class TestEnumValueAttribute : Attribute
+        {
+        }
+
+        private enum EnumWithAttributes
+        {
+            None = 0,
+
+            [TestEnumValue]
+            WithAttribute
+        }
+
+        [Fact]
+        public static void CustomAttributes()
+        {
+            Null(EnumWithAttributes.None.GetCustomAttribute<EnumWithAttributes, TestEnumValueAttribute>());
+            Empty(EnumWithAttributes.None.GetCustomAttributes<EnumWithAttributes, TestEnumValueAttribute>());
+
+            NotNull(EnumWithAttributes.WithAttribute.GetCustomAttribute<EnumWithAttributes, TestEnumValueAttribute>());
+            NotEmpty(EnumWithAttributes.WithAttribute.GetCustomAttributes<EnumWithAttributes, TestEnumValueAttribute>());
+        }
+    }
+}
