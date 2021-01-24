02dc313f542e9690cd0e198b3e6d576276dde1e7
sakno <roman.sakno@gmail.com>
2021-01-24 22:00:09 +0200

    Merge two classes

--- src/DotNext.IO/IO/TextStreamExtensions.cs
+++ src/DotNext.IO/IO/TextStreamExtensions.cs
@@ -49,5 +49,19 @@ namespace DotNext.IO
         /// <returns>The reader over the sequence of characters.</returns>
         public static TextReader AsTextReader(this ReadOnlySequence<char> sequence)
             => new TextBufferReader(sequence);
+
+        /// <summary>
+        /// Asynchronously writes a linked regions of characters to the text stream.
+        /// </summary>
+        /// <param name="writer">The stream to write into.</param>
+        /// <param name="chars">The linked regions of characters.</param>
+        /// <param name="token">The token that can be used to cancel the operation.</param>
+        /// <returns>The task representing asynchronous execution of this method.</returns>
+        /// <exception cref="OperationCanceledException">The operation has been canceled.</exception>
+        public static async ValueTask WriteAsync(this TextWriter writer, ReadOnlySequence<char> chars, CancellationToken token = default)
+        {
+            foreach (var segment in chars)
+                await writer.WriteAsync(segment, token).ConfigureAwait(false);
+        }
     }
 }
