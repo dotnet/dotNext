f2c53f9be1d7ba9bc2e483fd48fce058e5a1301a
sakno <roman.sakno@gmail.com>
2021-01-28 12:51:48 +0200

    Test coverage for stream wrappers

---
+++ src/DotNext.Tests/IO/StreamConsumerTests.cs
@@ -0,0 +1,40 @@
+using System;
+using System.IO;
+using Xunit;
+
+namespace DotNext.IO
+{
+    using Buffers;
+
+    public sealed class StreamConsumerTests : Test
+    {
+        [Fact]
+        public static void BasicMethods()
+        {
+            Throws<ArgumentNullException>(() => new StreamConsumer(null));
+            var consumer = new StreamConsumer();
+            True(consumer.IsEmpty);
+            Equal(new StreamConsumer(), consumer);
+            True(new StreamConsumer() == consumer);
+            False(new StreamConsumer() != consumer);
+            Equal(0, consumer.GetHashCode());
+
+            consumer = new StreamConsumer(Stream.Null);
+            False(consumer.IsEmpty);
+            NotEqual(0, consumer.GetHashCode());
+            NotEqual(new StreamConsumer(), consumer);
+            False(new StreamConsumer() == consumer);
+            True(new StreamConsumer() != consumer);
+        }
+
+        [Fact]
+        public static void WriteMethods()
+        {
+            using var ms = new MemoryStream();
+            IReadOnlySpanConsumer<byte> consumer = new StreamConsumer(ms);
+            consumer.Invoke(new ReadOnlySpan<byte>(new byte[] { 1, 2}));
+            consumer.Invoke(new ReadOnlyMemory<byte>(new byte[] { 3, 4 }), default);
+            Equal(new byte[] { 1, 2, 3, 4}, ms.ToArray());
+        }
+    }
+}
---
+++ src/DotNext.Tests/IO/TextConsumerTests.cs
@@ -0,0 +1,40 @@
+using System;
+using System.IO;
+using Xunit;
+
+namespace DotNext.IO
+{
+    using Buffers;
+
+    public sealed class TextConsumerTests : Test
+    {
+        [Fact]
+        public static void BasicMethods()
+        {
+            Throws<ArgumentNullException>(() => new TextConsumer(null));
+            var consumer = new TextConsumer();
+            True(consumer.IsEmpty);
+            Equal(new TextConsumer(), consumer);
+            True(new TextConsumer() == consumer);
+            False(new TextConsumer() != consumer);
+            Equal(0, consumer.GetHashCode());
+
+            consumer = new TextConsumer(TextWriter.Null);
+            False(consumer.IsEmpty);
+            NotEqual(0, consumer.GetHashCode());
+            NotEqual(new TextConsumer(), consumer);
+            False(new TextConsumer() == consumer);
+            True(new TextConsumer() != consumer);
+        }
+
+        [Fact]
+        public static void WriteMethods()
+        {
+            using var ms = new StringWriter();
+            IReadOnlySpanConsumer<char> consumer = new TextConsumer(ms);
+            consumer.Invoke(new ReadOnlySpan<char>(new char[] { '1', '2' }));
+            consumer.Invoke(new ReadOnlyMemory<char>(new char[] { '3', '4' }), default);
+            Equal("1234", ms.ToString());
+        }
+    }
+}
