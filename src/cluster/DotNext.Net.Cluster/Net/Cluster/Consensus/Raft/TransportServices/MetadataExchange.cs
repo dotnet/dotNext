a90b82e449630214eafbfb03dd9958f380005c51
sakno <roman.sakno@gmail.com>
2021-04-11 20:41:25 +0300

    Fixed dangerous use of non-short circuit logic.

--- src/cluster/DotNext.Net.Cluster/Net/Cluster/Consensus/Raft/TransportServices/MetadataExchange.cs
+++ src/cluster/DotNext.Net.Cluster/Net/Cluster/Consensus/Raft/TransportServices/MetadataExchange.cs
@@ -76,7 +76,7 @@ namespace DotNext.Net.Cluster.Consensus.Raft.TransportServices
         public override async ValueTask<bool> ProcessInboundMessageAsync(PacketHeaders headers, ReadOnlyMemory<byte> payload, EndPoint endpoint, CancellationToken token)
         {
             var flushResult = await Writer.WriteAsync(payload, token).ConfigureAwait(false);
-            return !(flushResult.IsCanceled | flushResult.IsCompleted | headers.Control == FlowControl.StreamEnd);
+            return !(flushResult.IsCanceled || flushResult.IsCompleted || headers.Control == FlowControl.StreamEnd);
         }
 
         public override ValueTask<(PacketHeaders, int, bool)> CreateOutboundMessageAsync(Memory<byte> payload, CancellationToken token)
