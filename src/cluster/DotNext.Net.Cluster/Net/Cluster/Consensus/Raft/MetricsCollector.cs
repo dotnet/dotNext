97db9f94c2748063f15ab80959ba9263c75e7b8a
sakno <roman.sakno@gmail.com>
2021-05-03 21:43:26 +0300

    Added support of ETW counters

--- src/cluster/DotNext.Net.Cluster/Net/Cluster/Consensus/Raft/MetricsCollector.cs
+++ src/cluster/DotNext.Net.Cluster/Net/Cluster/Consensus/Raft/MetricsCollector.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Diagnostics.Tracing;
 
 namespace DotNext.Net.Cluster.Consensus.Raft
 {
@@ -30,31 +31,98 @@ namespace DotNext.Net.Cluster.Consensus.Raft
         }
 
         /// <summary>
+        /// Sets counter that allows to track the broadcast time.
+        /// </summary>
+        public EventCounter? BroadcastTimeCounter
+        {
+            private get;
+#if NETSTANDARD2_1
+            set;
+#else
+            init;
+#endif
+        }
+
+        void ILeaderStateMetrics.ReportBroadcastTime(TimeSpan value)
+        {
+            BroadcastTimeCounter?.WriteMetric(value.TotalMilliseconds);
+            ReportBroadcastTime(value);
+        }
+
+        /// <summary>
         /// Reports that node becomes a candidate.
         /// </summary>
         public virtual void MovedToCandidateState()
+            => CandidateStateCounter?.Increment();
+
+        /// <summary>
+        /// Sets counter that allows to track the number of transitions to candidate state.
+        /// </summary>
+        public IncrementingEventCounter? CandidateStateCounter
         {
+            private get;
+#if NETSTANDARD2_1
+            set;
+#else
+            init;
+#endif
         }
 
         /// <summary>
         /// Reports that node becomes a follower.
         /// </summary>
         public virtual void MovedToFollowerState()
+            => FollowerStateCounter?.Increment();
+
+        /// <summary>
+        /// Sets counter that allows to track the number of transitions to follower state.
+        /// </summary>
+        public IncrementingEventCounter? FollowerStateCounter
         {
+            private get;
+#if NETSTANDARD2_1
+            set;
+#else
+            init;
+#endif
         }
 
         /// <summary>
         /// Reports that node becomes a leader.
         /// </summary>
         public virtual void MovedToLeaderState()
+            => LeaderStateCounter?.Increment();
+
+        /// <summary>
+        /// Sets counter that allows to track the number of transitions to leader state.
+        /// </summary>
+        public IncrementingEventCounter? LeaderStateCounter
         {
+            private get;
+#if NETSTANDARD2_1
+            set;
+#else
+            init;
+#endif
         }
 
         /// <summary>
         /// Reports that node receives a heartbeat from leader node.
         /// </summary>
         public virtual void ReportHeartbeat()
+            => HeartbeatCounter?.Increment();
+
+        /// <summary>
+        /// Sets counter that allows to track the number of received hearbeats from leader state.
+        /// </summary>
+        public IncrementingEventCounter? HeartbeatCounter
         {
+            private get;
+#if NETSTANDARD2_1
+            set;
+#else
+            init;
+#endif
         }
     }
 }
