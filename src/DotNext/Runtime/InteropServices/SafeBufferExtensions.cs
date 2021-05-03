150718a3c2abfa7293d491d38a869a4b2d44acc1
sakno <roman.sakno@gmail.com>
2021-05-03 22:40:40 +0300

    Added CLS compliant version of AcquirePointer

---
+++ src/DotNext/Runtime/InteropServices/SafeBufferExtensions.cs
@@ -0,0 +1,24 @@
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+namespace DotNext.Runtime.InteropServices
+{
+    /// <summary>
+    /// Represents extensions for <see cref="SafeBuffer"/> type.
+    /// </summary>
+    public static class SafeBufferExtensions
+    {
+        /// <summary>
+        /// Obtains managed pointer from buffer for a block of memory.
+        /// </summary>
+        /// <param name="buffer">The buffer.</param>
+        /// <returns>A managed pointer to the buffer; or <see cref="Unsafe.NullRef{T}"/> if buffer is invalid.</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static unsafe ref byte AcquirePointer(this SafeBuffer buffer)
+        {
+            byte* ptr = null;
+            buffer.AcquirePointer(ref ptr);
+            return ref ptr[0];
+        }
+    }
+}
