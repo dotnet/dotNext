c691a8f353b7c83eeb57f5683cf49cf1007038f0
sakno <roman.sakno@gmail.com>
2021-05-19 22:56:10 +0300

    Introduced empty DTO

---
+++ src/DotNext.IO/IO/EmptyDataTransferObject.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DotNext.IO
+{
+    internal sealed class EmptyDataTransferObject : IDataTransferObject
+    {
+        internal static readonly EmptyDataTransferObject Instance = new();
+
+        private EmptyDataTransferObject()
+        {
+        }
+
+        bool IDataTransferObject.TryGetMemory(out ReadOnlyMemory<byte> memory)
+        {
+            memory = ReadOnlyMemory<byte>.Empty;
+            return true;
+        }
+
+        bool IDataTransferObject.IsReusable => true;
+
+        long? IDataTransferObject.Length => 0L;
+
+        ValueTask IDataTransferObject.WriteToAsync<TWriter>(TWriter writer, CancellationToken token)
+            => new();
+
+        ValueTask<TResult> IDataTransferObject.TransformAsync<TResult, TTransformation>(TTransformation transformation, CancellationToken token)
+            => transformation.TransformAsync(EmptyBinaryReader.Instance, token);
+    }
+}
