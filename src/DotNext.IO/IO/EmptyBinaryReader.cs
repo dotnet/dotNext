a5bdad6c5a5d2bf3466249d3bd12815c7fd014ef
sakno <roman.sakno@gmail.com>
2021-05-19 22:48:56 +0300

    Empty reader can be resolved as empty span

--- src/DotNext.IO/IO/EmptyBinaryReader.cs
+++ src/DotNext.IO/IO/EmptyBinaryReader.cs
@@ -28,27 +28,30 @@ namespace DotNext.IO
             => ValueTask.FromException(new EndOfStreamException());
 #endif
 
+        private static Task GetCompletedOrCanceledTask(CancellationToken token)
+            => token.IsCancellationRequested ? Task.FromCanceled(token) : Task.CompletedTask;
+
         public ValueTask<T> ReadAsync<T>(CancellationToken token)
             where T : unmanaged
             => EndOfStream<T>();
 
         public ValueTask ReadAsync(Memory<byte> output, CancellationToken token)
-            => EndOfStream();
+            => output.IsEmpty ? new() : EndOfStream();
 
         public ValueTask<MemoryOwner<byte>> ReadAsync(LengthFormat lengthFormat, MemoryAllocator<byte>? allocator, CancellationToken token)
             => EndOfStream<MemoryOwner<byte>>();
 
         public ValueTask<string> ReadStringAsync(int length, DecodingContext context, CancellationToken token)
-            => EndOfStream<string>();
+            => length == 0 ? new(string.Empty) : EndOfStream<string>();
 
         public ValueTask<string> ReadStringAsync(LengthFormat lengthFormat, DecodingContext context, CancellationToken token)
             => EndOfStream<string>();
 
         public Task CopyToAsync(Stream output, CancellationToken token)
-            => token.IsCancellationRequested ? Task.FromCanceled(token) : Task.CompletedTask;
+            => GetCompletedOrCanceledTask(token);
 
         public Task CopyToAsync(PipeWriter output, CancellationToken token)
-            => token.IsCancellationRequested ? Task.FromCanceled(token) : Task.CompletedTask;
+            => GetCompletedOrCanceledTask(token);
 
         ValueTask<long> IAsyncBinaryReader.ReadInt64Async(bool littleEndian, CancellationToken token)
             => EndOfStream<long>();
@@ -113,8 +116,14 @@ namespace DotNext.IO
         ValueTask<BigInteger> IAsyncBinaryReader.ReadBigIntegerAsync(int length, bool littleEndian, CancellationToken token)
             => EndOfStream<BigInteger>();
 
-        private static Task GetCompletedOrCanceledTask(CancellationToken token)
-            => token.IsCancellationRequested ? Task.FromCanceled(token) : Task.CompletedTask;
+        ValueTask IAsyncBinaryReader.SkipAsync(int length, CancellationToken token)
+            => length == 0 ? new() : EndOfStream();
+
+        bool IAsyncBinaryReader.TryGetSpan(out ReadOnlySpan<byte> bytes)
+        {
+            bytes = ReadOnlySpan<byte>.Empty;
+            return true;
+        }
 
         Task IAsyncBinaryReader.CopyToAsync(IBufferWriter<byte> writer, CancellationToken token)
             => GetCompletedOrCanceledTask(token);
