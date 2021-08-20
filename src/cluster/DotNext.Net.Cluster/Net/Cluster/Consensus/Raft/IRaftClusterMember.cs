756588c89ad71a2ad6d5e8262c947925ecc33756
sakno <roman.sakno@gmail.com>
2021-08-20 14:52:10 +0300

    Fixed typo

--- src/cluster/DotNext.Net.Cluster/Net/Cluster/Consensus/Raft/IRaftClusterMember.cs
+++ src/cluster/DotNext.Net.Cluster/Net/Cluster/Consensus/Raft/IRaftClusterMember.cs
@@ -37,7 +37,7 @@ namespace DotNext.Net.Cluster.Consensus.Raft
             => token.IsCancellationRequested ? Task.FromCanceled<Result<bool>>(token) : Task.FromResult<Result<bool>>(new(term, true));
 
         /// <summary>
-        /// Transfers transaction log entry to the member.
+        /// Transfers log entries to the member.
         /// </summary>
         /// <typeparam name="TEntry">The type of the log entry.</typeparam>
         /// <typeparam name="TList">The type of the log entries list.</typeparam>
@@ -48,7 +48,7 @@ namespace DotNext.Net.Cluster.Consensus.Raft
         /// <param name="commitIndex">Last entry known to be committed by the local node.</param>
         /// <param name="token">The token that can be used to cancel asynchronous operation.</param>
         /// <returns><see langword="true"/> if message is handled successfully by this member; <see langword="false"/> if message is rejected due to invalid Term/Index number.</returns>
-        /// <exception cref="MemberUnavailableException">The member is unreachable through network.</exception>
+        /// <exception cref="MemberUnavailableException">The member is unreachable through the network.</exception>
         Task<Result<bool>> AppendEntriesAsync<TEntry, TList>(long term, TList entries, long prevLogIndex, long prevLogTerm, long commitIndex, CancellationToken token)
             where TEntry : IRaftLogEntry
             where TList : IReadOnlyList<TEntry>;
@@ -61,7 +61,7 @@ namespace DotNext.Net.Cluster.Consensus.Raft
         /// <param name="snapshotIndex">The index of the last included log entry in the snapshot.</param>
         /// <param name="token">The token that can be used to cancel asynchronous operation.</param>
         /// <returns><see langword="true"/> if snapshot is installed successfully; otherwise, <see langword="false"/>.</returns>
-        /// <exception cref="MemberUnavailableException">The member is unreachable through network.</exception>
+        /// <exception cref="MemberUnavailableException">The member is unreachable through the network.</exception>
         Task<Result<bool>> InstallSnapshotAsync(long term, IRaftLogEntry snapshot, long snapshotIndex, CancellationToken token);
 
         /// <summary>
