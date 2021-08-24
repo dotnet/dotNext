e561853b0af12184d093ea0eee04d75e3afc7c00
sakno <roman.sakno@gmail.com>
2021-08-25 01:43:19 +0300

    Added support of timeout

--- src/DotNext.Threading/Threading/LinkedTokenSourceFactory.cs
+++ src/DotNext.Threading/Threading/LinkedTokenSourceFactory.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Threading;
 
 namespace DotNext.Threading
@@ -8,7 +9,7 @@ namespace DotNext.Threading
     public static class LinkedTokenSourceFactory
     {
         /// <summary>
-        /// Links two cancellation token.
+        /// Links two cancellation tokens.
         /// </summary>
         /// <param name="first">The first cancellation token. Can be modified by this method.</param>
         /// <param name="second">The second cancellation token.</param>
@@ -16,9 +17,13 @@ namespace DotNext.Threading
         public static CancellationTokenSource? LinkTo(this ref CancellationToken first, CancellationToken second)
         {
             var result = default(CancellationTokenSource);
-            if (first == second)
+            if (first == second || first.IsCancellationRequested || second.IsCancellationRequested)
             {
                 // nothing to do, just return from this method
+            }
+            else if (first.IsCancellationRequested || second.IsCancellationRequested)
+            {
+                result = null;
             }
             else if (!first.CanBeCanceled)
             {
@@ -28,6 +33,66 @@ namespace DotNext.Threading
             {
                 result = CancellationTokenSource.CreateLinkedTokenSource(first, second);
                 first = result.Token;
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Links cancellation token with the timeout.
+        /// </summary>
+        /// <param name="token">The first cancellation token. Can be modified by this method.</param>
+        /// <param name="timeout">The timeout to link.</param>
+        /// <returns>The linked token source; or <see langword="null"/> if <paramref name="token"/> is canceled or <paramref name="timeout"/> is <see cref="System.Threading.Timeout.InfiniteTimeSpan"/>.</returns>
+        public static CancellationTokenSource? LinkTo(this ref CancellationToken token, TimeSpan timeout)
+        {
+            CancellationTokenSource? result;
+
+            if (token.IsCancellationRequested || timeout < TimeSpan.Zero)
+            {
+                result = null;
+            }
+            else
+            {
+                result = token.CanBeCanceled ? CancellationTokenSource.CreateLinkedTokenSource(token) : new CancellationTokenSource();
+                result.CancelAfter(timeout);
+                token = result.Token;
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Links two cancellation tokens with the given timeout.
+        /// </summary>
+        /// <param name="first">The first cancellation token. Can be modified by this method.</param>
+        /// <param name="second">The second cancellation token.</param>
+        /// <param name="timeout">The timeout to link.</param>
+        /// <returns>The linked token source; or <see langword="null"/> if not needed.</returns>
+        public static CancellationTokenSource? LinkTo(this ref CancellationToken first, CancellationToken second, TimeSpan timeout)
+        {
+            CancellationTokenSource? result;
+
+            if (first == second)
+            {
+                result = LinkTo(ref first, timeout);
+            }
+            else if (timeout < TimeSpan.Zero || !second.CanBeCanceled)
+            {
+                result = LinkTo(ref first, second);
+            }
+            else if (first.IsCancellationRequested || second.IsCancellationRequested)
+            {
+                result = null;
+            }
+            else if (!first.CanBeCanceled)
+            {
+                result = LinkTo(ref second, timeout);
+                first = second;
+            }
+            else
+            {
+                result = null;
             }
 
             return result;
