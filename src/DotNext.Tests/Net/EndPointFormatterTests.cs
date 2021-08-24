636fa0e95630aed299402f105c090d8ff98be313
sakno <roman.sakno@gmail.com>
2021-08-25 00:46:27 +0300

    Test coverage for EndPoint ser/deser methods

---
+++ src/DotNext.Tests/Net/EndPointFormatterTests.cs
@@ -0,0 +1,39 @@
+using System.Collections.Generic;
+using System.Net;
+using Xunit;
+
+namespace DotNext.Net
+{
+    using Buffers;
+    using IO;
+
+    public sealed class EndPointFormatterTests : Test
+    {
+        public static IEnumerable<object[]> GetTestEndPoints()
+        {
+            yield return new object[] { new DnsEndPoint("host", 3262) };
+            yield return new object[] { new IPEndPoint(IPAddress.Parse("192.168.0.1"), 3263) };
+            yield return new object[] { new IPEndPoint(IPAddress.Parse("2001:0db8:0000:0000:0000:8a2e:0370:7334"), 3264) };
+        }
+
+        [Theory]
+        [MemberData(nameof(GetTestEndPoints))]
+        public static void SerializeDeserializeDns(EndPoint expected)
+        {
+            byte[] data;
+            var writer = new BufferWriterSlim<byte>(32);
+            try
+            {
+                writer.WriteEndPoint(expected);
+                data = writer.WrittenSpan.ToArray();
+            }
+            finally
+            {
+                writer.Dispose();
+            }
+
+            var reader = IAsyncBinaryReader.Create(data);
+            Equal(expected, reader.ReadEndPoint());
+        }
+    }
+}
