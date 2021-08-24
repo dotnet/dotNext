5db27e9e8397059c68684869e3065166f88ca3e4
sakno <roman.sakno@gmail.com>
2021-08-24 19:03:36 +0300

    Added support of TaskComplectionSource

--- src/DotNext/Disposable.cs
+++ src/DotNext/Disposable.cs
@@ -56,6 +56,16 @@ namespace DotNext
         protected bool TrySetDisposedException<T>(TaskCompletionSource<T> source)
             => source.TrySetException(new ObjectDisposedException(ObjectName));
 
+#if !NETSTANDARD2_1
+        /// <summary>
+        /// Attempts to complete the task with <see cref="ObjectDisposedException"/> exception.
+        /// </summary>
+        /// <param name="source">The task completion source.</param>
+        /// <returns><see langword="true"/> if operation was successful; otherwise, <see langword="false"/>.</returns>
+        protected bool TrySetDisposedException(TaskCompletionSource source)
+            => source.TrySetException(new ObjectDisposedException(ObjectName));
+#endif
+
         /// <summary>
         /// Releases managed and unmanaged resources associated with this object.
         /// </summary>
