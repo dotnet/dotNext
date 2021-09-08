5eb294acb8ab4dc16d4b75628c356a8740c27fdd
sakno <roman.sakno@gmail.com>
2021-09-08 21:30:47 +0300

    Code cleanup

--- src/DotNext/Runtime/CompilerServices/RuntimeFeaturesAttribute.cs
+++ src/DotNext/Runtime/CompilerServices/RuntimeFeaturesAttribute.cs
@@ -1,37 +1,35 @@
-using System;
 using System.ComponentModel;
 using System.Diagnostics;
 
-namespace DotNext.Runtime.CompilerServices
+namespace DotNext.Runtime.CompilerServices;
+
+/// <summary>
+/// Indicates that the code inside of method, type, module or assembly relies
+/// on specific runtime features.
+/// </summary>
+/// <remarks>
+/// This attribute informs the developer about potential portability and performance
+/// issues associated with the marked program element.
+/// </remarks>
+[Conditional("DEBUG")]
+[CLSCompliant(false)]
+[EditorBrowsable(EditorBrowsableState.Never)]
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct | AttributeTargets.Method | AttributeTargets.Constructor | AttributeTargets.Assembly | AttributeTargets.Module, AllowMultiple = false, Inherited = true)]
+public sealed class RuntimeFeaturesAttribute : Attribute
 {
     /// <summary>
-    /// Indicates that the code inside of method, type, module or assembly relies
-    /// on specific runtime features.
+    /// Indicates that code relies on dynamic IL code generation or compilation of Expression Trees.
     /// </summary>
-    /// <remarks>
-    /// This attribute informs the developer about potential portability and performance
-    /// issues associated with the marked program element.
-    /// </remarks>
-    [Conditional("DEBUG")]
-    [CLSCompliant(false)]
-    [EditorBrowsable(EditorBrowsableState.Never)]
-    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct | AttributeTargets.Method | AttributeTargets.Constructor | AttributeTargets.Assembly | AttributeTargets.Module, AllowMultiple = false, Inherited = true)]
-    public sealed class RuntimeFeaturesAttribute : Attribute
-    {
-        /// <summary>
-        /// Indicates that code relies on dynamic IL code generation or compilation of Expression Trees.
-        /// </summary>
-        public bool DynamicCodeCompilation { get; set; }
+    public bool DynamicCodeCompilation { get; set; }
 
-        /// <summary>
-        /// Indicates that code relies on <see cref="System.Reflection.MethodInfo.MakeGenericMethod(Type[])"/>
-        /// or <see cref="Type.MakeGenericType(Type[])"/> calls.
-        /// </summary>
-        public bool RuntimeGenericInstantiation { get; set; }
+    /// <summary>
+    /// Indicates that code relies on <see cref="System.Reflection.MethodInfo.MakeGenericMethod(Type[])"/>
+    /// or <see cref="Type.MakeGenericType(Type[])"/> calls.
+    /// </summary>
+    public bool RuntimeGenericInstantiation { get; set; }
 
-        /// <summary>
-        /// Indicates that code relies on reflection of private or internal members.
-        /// </summary>
-        public bool PrivateReflection { get; set; }
-    }
+    /// <summary>
+    /// Indicates that code relies on reflection of private or internal members.
+    /// </summary>
+    public bool PrivateReflection { get; set; }
 }
