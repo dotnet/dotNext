55678d8eea67ab9958da1b5b2453cc29704503c7
sakno <roman.sakno@gmail.com>
2021-09-08 23:00:23 +0300

    Migration to logging generator

--- src/cluster/DotNext.Net.Cluster/Net/Cluster/Discovery/HyParView/LogMessages.cs
+++ src/cluster/DotNext.Net.Cluster/Net/Cluster/Discovery/HyParView/LogMessages.cs
@@ -1,31 +1,27 @@
-using System;
 using System.Net;
-using System.Reflection;
-using System.Resources;
-using System.Runtime.CompilerServices;
 using Microsoft.Extensions.Logging;
 
 namespace DotNext.Net.Cluster.Discovery.HyParView
 {
-    // TODO: Migrate to logger codegen in .NET 6
-    internal static class LogMessages
+    internal static partial class LogMessages
     {
         private const string EventIdPrefix = "DotNext.Net.Cluster.HyParView";
         private const int EventIdOffset = 76000;
-        private static readonly ResourceManager Resources = new("DotNext.Net.Cluster.Discovery.HyParView.LogMessages", Assembly.GetExecutingAssembly());
 
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        private static void Log<T>(this ILogger logger, LogLevel level, int eventId, T args, Exception? e = null, [CallerMemberName] string resourceName = "")
-            where T : struct, ITuple
-        {
-            if (logger.IsEnabled(level))
-                logger.Log(level, new(EventIdOffset + eventId, string.Concat(EventIdPrefix, ".", resourceName)), e, Resources.GetString(resourceName)!, args.ToArray());
-        }
+        [LoggerMessage(
+            EventIdOffset,
+            LogLevel.Warning,
+            "Command {CommandType} processing failed",
+            EventName = EventIdPrefix + "." + nameof(FailedToProcessCommand)
+        )]
+        public static partial void FailedToProcessCommand(this ILogger logger, int commandType, Exception e);
 
-        internal static void FailedToProcessCommand(this ILogger logger, int commandType, Exception e)
-            => logger.Log(LogLevel.Warning, 0, ValueTuple.Create(commandType), e);
-
-        internal static void PeerCommunicationFailed(this ILogger logger, EndPoint peer, Exception e)
-            => logger.Log(LogLevel.Warning, 1, ValueTuple.Create(peer), e);
+        [LoggerMessage(
+            EventIdOffset + 1,
+            LogLevel.Warning,
+            "Communication with peer {Peer} has failed",
+            EventName = EventIdPrefix + "." + nameof(PeerCommunicationFailed)
+        )]
+        public static partial void PeerCommunicationFailed(this ILogger logger, EndPoint peer, Exception e);
     }
 }
