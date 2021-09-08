6aae64d6ea4b40b275dc5c5b8d1c429e413bc4fc
sakno <roman.sakno@gmail.com>
2021-09-08 21:27:15 +0300

    Code cleanup

--- src/DotNext.Tests/IO/FileBufferingWriterTests.cs
+++ src/DotNext.Tests/IO/FileBufferingWriterTests.cs
@@ -1,11 +1,6 @@
-using System;
 using System.Buffers;
-using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
-using System.IO;
 using System.Text.Json;
-using System.Threading;
-using System.Threading.Tasks;
 using Xunit;
 
 namespace DotNext.IO
