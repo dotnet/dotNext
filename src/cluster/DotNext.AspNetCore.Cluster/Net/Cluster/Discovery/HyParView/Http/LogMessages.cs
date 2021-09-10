86219cc1dfbfb316250bf3356ed26e2a17b22fe2
sakno <roman.sakno@gmail.com>
2021-09-11 00:06:20 +0300

    Migration to logger source gen

--- src/cluster/DotNext.AspNetCore.Cluster/Net/Cluster/Discovery/HyParView/Http/LogMessages.cs
+++ src/cluster/DotNext.AspNetCore.Cluster/Net/Cluster/Discovery/HyParView/Http/LogMessages.cs
@@ -7,21 +7,17 @@ using Microsoft.Extensions.Logging;
 
 namespace DotNext.Net.Cluster.Discovery.HyParView.Http
 {
-    internal static class LogMessages
+    internal static partial class LogMessages
     {
         private const string EventIdPrefix = "DotNext.Net.HyParView.Http";
         private const int EventIdOffset = 77000;
-        private static readonly ResourceManager Resources = new("DotNext.Net.Cluster.Discovery.HyParView.Http.LogMessages", Assembly.GetExecutingAssembly());
 
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        private static void Log<T>(this ILogger logger, LogLevel level, int eventId, T args, Exception? e = null, [CallerMemberName] string resourceName = "")
-            where T : struct, ITuple
-        {
-            if (logger.IsEnabled(level))
-                logger.Log(level, new(EventIdOffset + eventId, string.Concat(EventIdPrefix, ".", resourceName)), e, Resources.GetString(resourceName)!, args.ToArray());
-        }
-
-        internal static void NoContactNodeProvider(this ILogger logger)
-            => logger.Log(LogLevel.Warning, 0, ValueTuple.Create());
+        [LoggerMessage(
+            EventIdOffset,
+            LogLevel.Warning,
+            "No contact node provider, the current peer remains alone",
+            EventName = EventIdPrefix + "." + nameof(NoContactNodeProvider)
+        )]
+        public static partial void NoContactNodeProvider(this ILogger logger);
     }
 }
