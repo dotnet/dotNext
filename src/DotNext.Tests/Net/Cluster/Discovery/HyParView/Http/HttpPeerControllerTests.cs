4ad317b9d489a751e8c8e04509b04d653ae961ee
sakno <roman.sakno@gmail.com>
2021-09-21 21:31:26 +0300

    Added test cases covering removal of nodes from the mesh

--- src/DotNext.Tests/Net/Cluster/Discovery/HyParView/Http/HttpPeerControllerTests.cs
+++ src/DotNext.Tests/Net/Cluster/Discovery/HyParView/Http/HttpPeerControllerTests.cs
@@ -1,5 +1,4 @@
 using System.Diagnostics.CodeAnalysis;
-using System.Net;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -30,39 +29,6 @@ namespace DotNext.Net.Cluster.Discovery.HyParView.Http
                 .ConfigureLogging(static builder => builder.AddDebug().SetMinimumLevel(LogLevel.Debug))
                 .JoinMesh()
                 .Build();
-        }
-
-        private sealed class MembershipChangeEventListener : IPeerLifetime
-        {
-            private readonly TaskCompletionSource<EndPoint> discovered, gone;
-
-            internal MembershipChangeEventListener()
-            {
-                discovered = new(TaskCreationOptions.RunContinuationsAsynchronously);
-                gone = new(TaskCreationOptions.RunContinuationsAsynchronously);
-            }
-
-            void IPeerLifetime.OnStart(PeerController controller)
-            {
-                controller.PeerDiscovered += OnPeerDiscovered;
-                controller.PeerGone += OnPeerGone;
-            }
-
-            void IPeerLifetime.OnStop(PeerController controller)
-            {
-                controller.PeerDiscovered -= OnPeerDiscovered;
-                controller.PeerGone -= OnPeerGone;
-            }
-
-            private void OnPeerDiscovered(PeerController sender, PeerEventArgs args)
-                => discovered.TrySetResult(args.PeerAddress);
-
-            private void OnPeerGone(PeerController sender, PeerEventArgs args)
-                => gone.TrySetResult(args.PeerAddress);
-
-            internal Task<EndPoint> DiscoveryTask => discovered.Task;
-
-            internal Task<EndPoint> DisconnectionTask => gone.Task;
         }
 
         [Fact]
