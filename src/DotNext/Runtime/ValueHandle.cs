e009b5305c23677520a37a08e5931e57e5cf5110
sakno <roman.sakno@gmail.com>
2021-09-23 22:12:59 +0300

    Fixed documentation

--- src/DotNext/Runtime/ValueHandle.cs
+++ src/DotNext/Runtime/ValueHandle.cs
@@ -105,7 +105,7 @@ namespace DotNext.Runtime
         }
 
         /// <summary>
-        /// Gets a reference to the field.
+        /// Gets a reference to a memory location where the value is stored.
         /// </summary>
         public ref TValue Value
         {
