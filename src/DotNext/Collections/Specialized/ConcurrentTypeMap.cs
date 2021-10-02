6f2d5890f883ed80db26cac851e2bd33b3eb670c
sakno <roman.sakno@gmail.com>
2021-10-02 23:52:46 +0300

    Optimized index calculation

--- src/DotNext/Collections/Specialized/ConcurrentTypeMap.cs
+++ src/DotNext/Collections/Specialized/ConcurrentTypeMap.cs
@@ -1,7 +1,6 @@
 using System.Diagnostics.CodeAnalysis;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-using Debug = System.Diagnostics.Debug;
 
 namespace DotNext.Collections.Specialized;
 
@@ -116,14 +115,6 @@ public class ConcurrentTypeMap<TValue> : ITypeMap<TValue>
         }
     }
 
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    private static ref Entry Get<TKey>(Entry[] entries)
-    {
-        Debug.Assert(ITypeMap<TValue>.GetIndex<TKey>() < entries.Length);
-
-        return ref Unsafe.Add(ref MemoryMarshal.GetArrayDataReference(entries), ITypeMap<TValue>.GetIndex<TKey>());
-    }
-
     /// <inheritdoc />
     void ITypeMap<TValue>.Add<TKey>(TValue value)
     {
@@ -131,25 +122,19 @@ public class ConcurrentTypeMap<TValue> : ITypeMap<TValue>
             throw new GenericArgumentException<TKey>(ExceptionMessages.KeyAlreadyExists);
     }
 
-    /// <summary>
-    /// Attempts to associate a value with the type.
-    /// </summary>
-    /// <typeparam name="TKey">The type acting as a key.</typeparam>
-    /// <param name="value">The value associated with the type.</param>
-    /// <returns><see langword="true"/> if the value is added; otherwise, <see langword="false"/>.</returns>
-    public bool TryAdd<TKey>(TValue value)
+    private bool TryAdd(int index, TValue value)
     {
         for (bool added; ;)
         {
             var entries = this.entries;
 
-            if (ITypeMap<TValue>.GetIndex<TKey>() >= entries.Length)
+            if (index >= entries.Length)
             {
                 Resize(entries);
                 continue;
             }
 
-            ref var entry = ref Get<TKey>(entries);
+            ref var entry = ref Unsafe.Add(ref MemoryMarshal.GetArrayDataReference(entries), index);
 
             lock (entry.Lock)
             {
@@ -172,23 +157,27 @@ public class ConcurrentTypeMap<TValue> : ITypeMap<TValue>
     }
 
     /// <summary>
-    /// Associates the value with the specified type.
+    /// Attempts to associate a value with the type.
     /// </summary>
     /// <typeparam name="TKey">The type acting as a key.</typeparam>
-    /// <param name="value">The value to set.</param>
-    public void Set<TKey>(TValue value)
+    /// <param name="value">The value associated with the type.</param>
+    /// <returns><see langword="true"/> if the value is added; otherwise, <see langword="false"/>.</returns>
+    public bool TryAdd<TKey>(TValue value)
+        => TryAdd(ITypeMap<TValue>.GetIndex<TKey>(), value);
+
+    private void Set(int index, TValue value)
     {
         for (Entry[] entries; ;)
         {
             entries = this.entries;
 
-            if (ITypeMap<TValue>.GetIndex<TKey>() >= entries.Length)
+            if (index >= entries.Length)
             {
                 Resize(entries);
                 continue;
             }
 
-            ref var entry = ref Get<TKey>(entries);
+            ref var entry = ref Unsafe.Add(ref MemoryMarshal.GetArrayDataReference(entries), index);
 
             lock (entry.Lock)
             {
@@ -203,37 +192,39 @@ public class ConcurrentTypeMap<TValue> : ITypeMap<TValue>
     }
 
     /// <summary>
+    /// Associates the value with the specified type.
+    /// </summary>
+    /// <typeparam name="TKey">The type acting as a key.</typeparam>
+    /// <param name="value">The value to set.</param>
+    public void Set<TKey>(TValue value)
+        => Set(ITypeMap<TValue>.GetIndex<TKey>(), value);
+
+    /// <summary>
     /// Determines whether the map has association between the value and the specified type.
     /// </summary>
     /// <typeparam name="TKey">The type acting as a key.</typeparam>
     /// <returns><see langword="true"/> if there is a value associated with <typeparamref name="TKey"/>; otherwise, <see langword="false"/>.</returns>
     public bool ContainsKey<TKey>()
     {
-        var snapshot = entries;
-        return ITypeMap<TValue>.GetIndex<TKey>() < snapshot.Length && Get<TKey>(snapshot).HasValue;
+        return ContainsKey(entries, ITypeMap<TValue>.GetIndex<TKey>());
+
+        static bool ContainsKey(Entry[] entries, int index)
+            => index < entries.Length && Unsafe.Add(ref MemoryMarshal.GetArrayDataReference(entries), index).HasValue;
     }
 
-    /// <summary>
-    /// Adds a value to the map if the key does not already exist.
-    /// Returns the new value, or the existing value if the key already exists.
-    /// </summary>
-    /// <typeparam name="TKey">The type acting as a key.</typeparam>
-    /// <param name="value">The value associated with the type.</param>
-    /// <param name="added"><see langword="true"/> if the value is added; <see langword="false"/> if the value is already exist.</param>
-    /// <returns>The existing value; or <paramref name="value"/> if added.</returns>
-    public TValue GetOrAdd<TKey>(TValue value, out bool added)
+    private TValue GetOrAdd(int index, TValue value, out bool added)
     {
         for (Entry[] entries; ;)
         {
             entries = this.entries;
 
-            if (ITypeMap<TValue>.GetIndex<TKey>() >= entries.Length)
+            if (index >= entries.Length)
             {
                 Resize(entries);
                 continue;
             }
 
-            ref var entry = ref Get<TKey>(entries);
+            ref var entry = ref Unsafe.Add(ref MemoryMarshal.GetArrayDataReference(entries), index);
 
             lock (entry.Lock)
             {
@@ -257,26 +248,29 @@ public class ConcurrentTypeMap<TValue> : ITypeMap<TValue>
     }
 
     /// <summary>
-    /// Adds a value to the map if the key does not already exist, or updates the existing value.
+    /// Adds a value to the map if the key does not already exist.
+    /// Returns the new value, or the existing value if the key already exists.
     /// </summary>
     /// <typeparam name="TKey">The type acting as a key.</typeparam>
     /// <param name="value">The value associated with the type.</param>
-    /// <returns><see langword="true"/> if <paramref name="value"/> is added;
-    /// <see langword="false"/> if the existing value is updated with <paramref name="value"/>.
-    /// </returns>
-    public bool AddOrUpdate<TKey>(TValue value)
+    /// <param name="added"><see langword="true"/> if the value is added; <see langword="false"/> if the value is already exist.</param>
+    /// <returns>The existing value; or <paramref name="value"/> if added.</returns>
+    public TValue GetOrAdd<TKey>(TValue value, out bool added)
+        => GetOrAdd(ITypeMap<TValue>.GetIndex<TKey>(), value, out added);
+
+    private bool AddOrUpdate(int index, TValue value)
     {
         for (bool added; ;)
         {
             var entries = this.entries;
 
-            if (ITypeMap<TValue>.GetIndex<TKey>() >= entries.Length)
+            if (index >= entries.Length)
             {
                 Resize(entries);
                 continue;
             }
 
-            ref var entry = ref Get<TKey>(entries);
+            ref var entry = ref Unsafe.Add(ref MemoryMarshal.GetArrayDataReference(entries), index);
 
             lock (entry.Lock)
             {
@@ -292,24 +286,29 @@ public class ConcurrentTypeMap<TValue> : ITypeMap<TValue>
     }
 
     /// <summary>
-    /// Replaces the existing value with a new value.
+    /// Adds a value to the map if the key does not already exist, or updates the existing value.
     /// </summary>
     /// <typeparam name="TKey">The type acting as a key.</typeparam>
-    /// <param name="value">A new value.</param>
-    /// <returns>The replaced value.</returns>
-    public Optional<TValue> Replace<TKey>(TValue value)
+    /// <param name="value">The value associated with the type.</param>
+    /// <returns><see langword="true"/> if <paramref name="value"/> is added;
+    /// <see langword="false"/> if the existing value is updated with <paramref name="value"/>.
+    /// </returns>
+    public bool AddOrUpdate<TKey>(TValue value)
+        => AddOrUpdate(ITypeMap<TValue>.GetIndex<TKey>(), value);
+
+    private Optional<TValue> Replace(int index, TValue value)
     {
         for (Optional<TValue> result; ;)
         {
             var entries = this.entries;
 
-            if (ITypeMap<TValue>.GetIndex<TKey>() >= entries.Length)
+            if (index >= entries.Length)
             {
                 Resize(entries);
                 continue;
             }
 
-            ref var entry = ref Get<TKey>(entries);
+            ref var entry = ref Unsafe.Add(ref MemoryMarshal.GetArrayDataReference(entries), index);
 
             lock (entry.Lock)
             {
@@ -325,12 +324,15 @@ public class ConcurrentTypeMap<TValue> : ITypeMap<TValue>
     }
 
     /// <summary>
-    /// Attempts to remove the value from the map.
+    /// Replaces the existing value with a new value.
     /// </summary>
     /// <typeparam name="TKey">The type acting as a key.</typeparam>
-    /// <param name="value">The value of the removed element.</param>
-    /// <returns><see langword="true"/> if the element successfully removed; otherwise, <see langword="false"/>.</returns>
-    public bool Remove<TKey>([MaybeNullWhen(false)] out TValue value)
+    /// <param name="value">A new value.</param>
+    /// <returns>The replaced value.</returns>
+    public Optional<TValue> Replace<TKey>(TValue value)
+        => Replace(ITypeMap<TValue>.GetIndex<TKey>(), value);
+
+    private bool Remove(int index, [MaybeNullWhen(false)] out TValue value)
     {
         bool result;
 
@@ -338,14 +340,14 @@ public class ConcurrentTypeMap<TValue> : ITypeMap<TValue>
         {
             entries = this.entries;
 
-            if (ITypeMap<TValue>.GetIndex<TKey>() >= entries.Length)
+            if (index >= entries.Length)
             {
                 value = default;
                 result = false;
                 break;
             }
 
-            ref var entry = ref Get<TKey>(entries);
+            ref var entry = ref Unsafe.Add(ref MemoryMarshal.GetArrayDataReference(entries), index);
 
             lock (entry.Lock)
             {
@@ -366,16 +368,19 @@ public class ConcurrentTypeMap<TValue> : ITypeMap<TValue>
     /// Attempts to remove the value from the map.
     /// </summary>
     /// <typeparam name="TKey">The type acting as a key.</typeparam>
+    /// <param name="value">The value of the removed element.</param>
+    /// <returns><see langword="true"/> if the element successfully removed; otherwise, <see langword="false"/>.</returns>
+    public bool Remove<TKey>([MaybeNullWhen(false)] out TValue value)
+        => Remove(ITypeMap<TValue>.GetIndex<TKey>(), out value);
+
+    /// <summary>
+    /// Attempts to remove the value from the map.
+    /// </summary>
+    /// <typeparam name="TKey">The type acting as a key.</typeparam>
     /// <returns><see langword="true"/> if the element successfully removed; otherwise, <see langword="false"/>.</returns>
     public bool Remove<TKey>() => Remove<TKey>(out _);
 
-    /// <summary>
-    /// Attempts to get the value associated with the specified type.
-    /// </summary>
-    /// <typeparam name="TKey">The type acting as a key.</typeparam>
-    /// <param name="value">The value associated with the type.</param>
-    /// <returns><see langword="true"/> if there is a value associated with <typeparamref name="TKey"/>; otherwise, <see langword="false"/>.</returns>
-    public bool TryGetValue<TKey>([MaybeNullWhen(false)] out TValue value)
+    private bool TryGetValue(int index, [MaybeNullWhen(false)] out TValue value)
     {
         bool result;
 
@@ -383,14 +388,14 @@ public class ConcurrentTypeMap<TValue> : ITypeMap<TValue>
         {
             entries = this.entries;
 
-            if (ITypeMap<TValue>.GetIndex<TKey>() >= entries.Length)
+            if (index >= entries.Length)
             {
                 value = default;
                 result = false;
                 break;
             }
 
-            ref var entry = ref Get<TKey>(entries);
+            ref var entry = ref Unsafe.Add(ref MemoryMarshal.GetArrayDataReference(entries), index);
 
             lock (entry.Lock)
             {
@@ -405,6 +410,15 @@ public class ConcurrentTypeMap<TValue> : ITypeMap<TValue>
 
         return result;
     }
+
+    /// <summary>
+    /// Attempts to get the value associated with the specified type.
+    /// </summary>
+    /// <typeparam name="TKey">The type acting as a key.</typeparam>
+    /// <param name="value">The value associated with the type.</param>
+    /// <returns><see langword="true"/> if there is a value associated with <typeparamref name="TKey"/>; otherwise, <see langword="false"/>.</returns>
+    public bool TryGetValue<TKey>([MaybeNullWhen(false)] out TValue value)
+        => TryGetValue(ITypeMap<TValue>.GetIndex<TKey>(), out value);
 
     /// <summary>
     /// Removes all elements from this map.
