a86d59a1b8af247294220656995eaa615456d32a
sakno <roman.sakno@gmail.com>
2021-10-20 23:14:50 +0300

    Reuse event hub

--- src/cluster/DotNext.Net.Cluster/Net/Cluster/Consensus/Raft/TransportServices/ServerExchange.Entries.cs
+++ src/cluster/DotNext.Net.Cluster/Net/Cluster/Consensus/Raft/TransportServices/ServerExchange.Entries.cs
@@ -8,6 +8,7 @@ namespace DotNext.Net.Cluster.Consensus.Raft.TransportServices;
 using IO;
 using IO.Log;
 using static Runtime.Intrinsics;
+using AsyncEventHub = Threading.AsyncEventHub;
 
 [StructLayout(LayoutKind.Auto)]
 internal readonly struct ReceivedLogEntry : IRaftLogEntry
@@ -50,146 +51,32 @@ internal readonly struct ReceivedLogEntry : IRaftLogEntry
 
 internal partial class ServerExchange : ILogEntryProducer<ReceivedLogEntry>
 {
-    private sealed class EntriesExchangeCoordinator : Disposable
+    private sealed class EntriesExchangeCoordinator : AsyncEventHub
     {
-        private sealed class EventSource : TaskCompletionSource
-        {
-            public EventSource()
-                : base(TaskCreationOptions.RunContinuationsAsynchronously)
-            {
-            }
-        }
-
-        private readonly ReaderWriterLockSlim rwLock;
-        private (EventSource ReceiveEntriesFinished, EventSource ReadyToReceiveEntry, EventSource AppendEntriesReceived, EventSource ReceivingEntry, EventSource EntryReceived) sources;
-
         internal EntriesExchangeCoordinator()
+            : base(5)
         {
-            sources.AsSpan().Initialize();
-            rwLock = new(LockRecursionPolicy.NoRecursion);
         }
 
-        private TaskCompletionSource this[State state] => state switch
+        private static int GetIndex(State state) => state switch
         {
-            State.ReceivingEntriesFinished => sources.ReceiveEntriesFinished,
-            State.ReadyToReceiveEntry => sources.ReadyToReceiveEntry,
-            State.AppendEntriesReceived => sources.AppendEntriesReceived,
-            State.ReceivingEntry => sources.ReceivingEntry,
-            State.EntryReceived => sources.EntryReceived,
+            State.ReceivingEntriesFinished => 0,
+            State.ReadyToReceiveEntry => 1,
+            State.AppendEntriesReceived => 2,
+            State.ReceivingEntry => 3,
+            State.EntryReceived => 4,
             _ => throw new ArgumentOutOfRangeException(nameof(state)),
         };
 
-        internal void Signal(State state)
-        {
-            rwLock.EnterWriteLock();
-
-            try
-            {
-                foreach (ref var source in sources.AsSpan())
-                {
-                    if (source.Task.IsCompleted)
-                        source = new();
-                }
-
-                this[state].SetResult();
-            }
-            finally
-            {
-                rwLock.ExitWriteLock();
-            }
-        }
-
-        internal Task WaitAsync(State state)
-        {
-            rwLock.EnterReadLock();
-            try
-            {
-                return this[state].Task;
-            }
-            finally
-            {
-                rwLock.ExitReadLock();
-            }
-        }
-
-        internal Task WaitAnyAsync(ReadOnlySpan<State> states, CancellationToken token = default)
-        {
-            Task result;
-
-            if (states.IsEmpty)
-            {
-                result = Task.CompletedTask;
-                goto exit;
-            }
-
-            rwLock.EnterReadLock();
-            try
-            {
-                switch (states.Length)
-                {
-                    case 1:
-                        result = this[states[0]].Task;
-                        break;
-                    case 2:
-                        result = Task.WhenAny(this[states[0]].Task, this[states[1]].Task);
-                        break;
-                    default:
-                        var tasks = new Task[states.Length];
-                        var index = 0;
-
-                        foreach (var state in states)
-                            tasks[index++] = this[state].Task;
-
-                        result = Task.WhenAny(tasks);
-                        break;
-                }
-            }
-            finally
-            {
-                rwLock.ExitReadLock();
-            }
-
-        exit:
-            return result.WaitAsync(token);
-        }
+        internal void Pulse(State state) => Pulse(GetIndex(state));
 
         [SuppressMessage("StyleCop.CSharp.LayoutRules", "SA1500", Justification = "False positive")]
         internal Task WaitAnyAsync(State state1, State state2, CancellationToken token = default)
-            => WaitAnyAsync(stackalloc State[] { state1, state2 }, token);
+            => WaitAnyAsync(stackalloc int[] { GetIndex(state1), GetIndex(state2) }, token);
 
         [SuppressMessage("StyleCop.CSharp.LayoutRules", "SA1500", Justification = "False positive")]
         internal Task WaitAnyAsync(State state1, State state2, State state3, CancellationToken token = default)
-            => WaitAnyAsync(stackalloc State[] { state1, state2, state3 }, token);
-
-        internal void CancelSuspendedCallers()
-        {
-            rwLock.EnterWriteLock();
-            try
-            {
-                foreach (ref var source in sources.AsSpan())
-                {
-                    source.TrySetCanceled();
-                    source = new();
-                }
-            }
-            finally
-            {
-                rwLock.ExitWriteLock();
-            }
-        }
-
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                foreach (var source in sources.AsSpan())
-                    TrySetDisposedException(source);
-
-                rwLock.Dispose();
-            }
-
-            base.Dispose(disposing);
-        }
+            => WaitAnyAsync(stackalloc int[] { GetIndex(state1), GetIndex(state2), GetIndex(state3) }, token);
     }
 
     private EntriesExchangeCoordinator? entriesExchangeCoordinator;
@@ -212,7 +99,7 @@ internal partial class ServerExchange : ILogEntryProducer<ReceivedLogEntry>
         if (remainingCount <= 0)
         {
             // resume wait thread to finalize response
-            entriesExchangeCoordinator.Signal(state = State.ReceivingEntriesFinished);
+            entriesExchangeCoordinator.Pulse(state = State.ReceivingEntriesFinished);
             return false;
         }
 
@@ -227,7 +114,7 @@ internal partial class ServerExchange : ILogEntryProducer<ReceivedLogEntry>
 
         // informs that we are ready to receive a new log entry
         runnningIndex += 1;
-        entriesExchangeCoordinator.Signal(state = State.ReadyToReceiveEntry);
+        entriesExchangeCoordinator.Pulse(state = State.ReadyToReceiveEntry);
 
         // waits for the log entry
         await entriesExchangeCoordinator.WaitAnyAsync(State.ReceivingEntry, State.EntryReceived).ConfigureAwait(false);
@@ -238,8 +125,16 @@ internal partial class ServerExchange : ILogEntryProducer<ReceivedLogEntry>
     private void BeginReceiveEntries(ReadOnlySpan<byte> announcement, CancellationToken token)
     {
         runnningIndex = -1;
-        entriesExchangeCoordinator = new();
-        entriesExchangeCoordinator.Signal(state = State.AppendEntriesReceived);
+        if (entriesExchangeCoordinator is null)
+        {
+            entriesExchangeCoordinator = new();
+        }
+        else
+        {
+            entriesExchangeCoordinator.Reset();
+        }
+
+        entriesExchangeCoordinator.Pulse(state = State.AppendEntriesReceived);
         EntriesExchange.ParseAnnouncement(announcement, out var sender, out var term, out var prevLogIndex, out var prevLogTerm, out var commitIndex, out remainingCount, out var configState);
 
         task = server.AppendEntriesAsync(sender, term, this, prevLogIndex, prevLogTerm, commitIndex, configState?.Fingerprint, configState?.ApplyConfig ?? false, token);
@@ -261,7 +156,7 @@ internal partial class ServerExchange : ILogEntryProducer<ReceivedLogEntry>
             state = State.ReceivingEntry;
         }
 
-        entriesExchangeCoordinator.Signal(state);
+        entriesExchangeCoordinator.Pulse(state);
 
         return true;
     }
@@ -283,7 +178,7 @@ internal partial class ServerExchange : ILogEntryProducer<ReceivedLogEntry>
         if (completed)
         {
             await Writer.CompleteAsync().ConfigureAwait(false);
-            entriesExchangeCoordinator.Signal(state = State.EntryReceived);
+            entriesExchangeCoordinator.Pulse(state = State.EntryReceived);
         }
 
         return true;
