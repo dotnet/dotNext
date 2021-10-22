d793067364293be6cc822b34566330089980fa57
sakno <roman.sakno@gmail.com>
2021-10-22 11:40:26 +0300

    Added conversion of integers to bit vector

--- src/DotNext/Numerics/BitVector.cs
+++ src/DotNext/Numerics/BitVector.cs
@@ -8,47 +8,61 @@ namespace DotNext.Numerics;
 /// </summary>
 public static class BitVector
 {
-    private interface IValueBuilder<TResult>
-        where TResult : unmanaged
+    private interface IBitVector<TValue>
+        where TValue : unmanaged
     {
-        void SetBit(int position, bool bit);
+        bool this[int position] { get; set; }
 
-        TResult Result { get; }
+        TValue Value { get; set; }
     }
 
     [StructLayout(LayoutKind.Auto)]
-    private struct UInt32Builder : IValueBuilder<uint>
+    private struct UInt32Vector : IBitVector<uint>
     {
         private uint result;
 
-        void IValueBuilder<uint>.SetBit(int position, bool bit)
-            => result = (result & ~(1U << position)) | ((uint)bit.ToInt32() << position);
+        bool IBitVector<uint>.this[int position]
+        {
+            readonly get => ValueTypeExtensions.ToBoolean((int)((result >> position) & 1U));
+            set => result = (result & ~(1U << position)) | ((uint)value.ToInt32() << position);
+        }
 
-        readonly uint IValueBuilder<uint>.Result => result;
+        uint IBitVector<uint>.Value
+        {
+            readonly get => result;
+            set => result = value;
+        }
     }
 
     [StructLayout(LayoutKind.Auto)]
-    private struct UInt64Builder : IValueBuilder<ulong>
+    private struct UInt64Vector : IBitVector<ulong>
     {
         private ulong result;
 
-        void IValueBuilder<ulong>.SetBit(int position, bool bit)
-            => result = (result & ~(1UL << position)) | ((ulong)bit.ToInt32() << position);
+        bool IBitVector<ulong>.this[int position]
+        {
+            readonly get => ValueTypeExtensions.ToBoolean((int)((result >> position) & 1UL));
+            set => result = (result & ~(1UL << position)) | ((ulong)value.ToInt32() << position);
+        }
 
-        readonly ulong IValueBuilder<ulong>.Result => result;
+        ulong IBitVector<ulong>.Value
+        {
+            readonly get => result;
+            set => result = value;
+        }
     }
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    private static TResult VectorToScalar<TResult, TBuilder>(ReadOnlySpan<bool> bits)
-        where TResult : unmanaged
-        where TBuilder : struct, IValueBuilder<TResult>
+    private static TValue VectorToScalar<TValue, TVector>(ReadOnlySpan<bool> bits)
+        where TValue : unmanaged
+        where TVector : struct, IBitVector<TValue>
     {
-        var result = new TBuilder();
+        var result = new TVector();
 
         for (var position = 0; position < bits.Length; position++)
-            result.SetBit(position, bits[position]);
+            result[position] = bits[position];
 
-        return result.Result;
+        return result.Value;
     }
 
     /// <summary>
@@ -57,7 +71,16 @@ public static class BitVector
     /// <param name="bits">A sequence of bits.</param>
     /// <returns>8-bit unsigned integer reconstructed from the bits.</returns>
     public static byte ToByte(ReadOnlySpan<bool> bits)
-        => (byte)VectorToScalar<uint, UInt32Builder>(bits.TrimLength(8));
+        => (byte)VectorToScalar<uint, UInt32Vector>(bits.TrimLength(8));
+
+    /// <summary>
+    /// Converts bit vector to 8-bit signed integer.
+    /// </summary>
+    /// <param name="bits">A sequence of bits.</param>
+    /// <returns>8-bit unsigned integer reconstructed from the bits.</returns>
+    [CLSCompliant(false)]
+    public static sbyte ToSByte(ReadOnlySpan<bool> bits)
+        => (sbyte)VectorToScalar<uint, UInt32Vector>(bits.TrimLength(8));
 
     /// <summary>
     /// Converts bit vector to 16-bit signed integer.
@@ -65,7 +88,7 @@ public static class BitVector
     /// <param name="bits">A sequence of bits.</param>
     /// <returns>16-bit signed integer reconstructed from the bits.</returns>
     public static short ToInt16(ReadOnlySpan<bool> bits)
-        => (short)VectorToScalar<uint, UInt32Builder>(bits.TrimLength(16));
+        => (short)VectorToScalar<uint, UInt32Vector>(bits.TrimLength(16));
 
     /// <summary>
     /// Converts bit vector to 16-bit unsigned integer.
@@ -74,7 +97,7 @@ public static class BitVector
     /// <returns>16-bit unsigned integer reconstructed from the bits.</returns>
     [CLSCompliant(false)]
     public static ushort ToUInt16(ReadOnlySpan<bool> bits)
-        => (ushort)VectorToScalar<uint, UInt32Builder>(bits.TrimLength(16));
+        => (ushort)VectorToScalar<uint, UInt32Vector>(bits.TrimLength(16));
 
     /// <summary>
     /// Converts bit vector to 32-bit signed integer.
@@ -82,7 +105,7 @@ public static class BitVector
     /// <param name="bits">A sequence of bits.</param>
     /// <returns>32-bit signed integer reconstructed from the bits.</returns>
     public static int ToInt32(ReadOnlySpan<bool> bits)
-        => (int)VectorToScalar<uint, UInt32Builder>(bits.TrimLength(32));
+        => (int)VectorToScalar<uint, UInt32Vector>(bits.TrimLength(32));
 
     /// <summary>
     /// Converts bit vector to 32-bit unsigned integer.
@@ -91,7 +114,7 @@ public static class BitVector
     /// <returns>32-bit unsigned integer reconstructed from the bits.</returns>
     [CLSCompliant(false)]
     public static uint ToUInt32(ReadOnlySpan<bool> bits)
-        => VectorToScalar<uint, UInt32Builder>(bits.TrimLength(32));
+        => VectorToScalar<uint, UInt32Vector>(bits.TrimLength(32));
 
     /// <summary>
     /// Converts bit vector to 64-bit signed integer.
@@ -99,7 +122,7 @@ public static class BitVector
     /// <param name="bits">A sequence of bits.</param>
     /// <returns>64-bit signed integer reconstructed from the bits.</returns>
     public static long ToInt64(ReadOnlySpan<bool> bits)
-        => (long)VectorToScalar<ulong, UInt64Builder>(bits.TrimLength(64));
+        => (long)VectorToScalar<ulong, UInt64Vector>(bits.TrimLength(64));
 
     /// <summary>
     /// Converts bit vector to 64-bit unsigned integer.
@@ -108,5 +131,84 @@ public static class BitVector
     /// <returns>64-bit unsigned integer reconstructed from the bits.</returns>
     [CLSCompliant(false)]
     public static ulong ToUInt64(ReadOnlySpan<bool> bits)
-        => VectorToScalar<ulong, UInt64Builder>(bits.TrimLength(64));
+        => VectorToScalar<ulong, UInt64Vector>(bits.TrimLength(64));
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private static void GetBits<TValue, TVector>(TValue value, Span<bool> bits)
+        where TValue : unmanaged
+        where TVector : struct, IBitVector<TValue>
+    {
+        var vector = new TVector { Value = value };
+
+        for (var position = 0; position < bits.Length; position++)
+            bits[position] = vector[position];
+    }
+
+    /// <summary>
+    /// Extracts bits from 8-bit unsigned integer.
+    /// </summary>
+    /// <param name="value">The integer value.</param>
+    /// <param name="bits">The buffer for extracted bits.</param>
+    public static void FromByte(byte value, Span<bool> bits)
+        => GetBits<uint, UInt32Vector>(value, bits.TrimLength(8));
+
+    /// <summary>
+    /// Extracts bits from 8-bit signed integer.
+    /// </summary>
+    /// <param name="value">The integer value.</param>
+    /// <param name="bits">The buffer for extracted bits.</param>
+    [CLSCompliant(false)]
+    public static void FromSByte(sbyte value, Span<bool> bits)
+        => GetBits<uint, UInt32Vector>((byte)value, bits.TrimLength(8));
+
+    /// <summary>
+    /// Extracts bits from 16-bit signed integer.
+    /// </summary>
+    /// <param name="value">The integer value.</param>
+    /// <param name="bits">The buffer for extracted bits.</param>
+    public static void FromInt16(short value, Span<bool> bits)
+        => GetBits<uint, UInt32Vector>((ushort)value, bits.TrimLength(16));
+
+    /// <summary>
+    /// Extracts bits from 16-bit unsigned integer.
+    /// </summary>
+    /// <param name="value">The integer value.</param>
+    /// <param name="bits">The buffer for extracted bits.</param>
+    [CLSCompliant(false)]
+    public static void FromUInt16(ushort value, Span<bool> bits)
+        => GetBits<uint, UInt32Vector>(value, bits.TrimLength(16));
+
+    /// <summary>
+    /// Extracts bits from 32-bit signed integer.
+    /// </summary>
+    /// <param name="value">The integer value.</param>
+    /// <param name="bits">The buffer for extracted bits.</param>
+    public static void FromInt32(int value, Span<bool> bits)
+        => GetBits<uint, UInt32Vector>((uint)value, bits.TrimLength(32));
+
+    /// <summary>
+    /// Extracts bits from 32-bit unsigned integer.
+    /// </summary>
+    /// <param name="value">The integer value.</param>
+    /// <param name="bits">The buffer for extracted bits.</param>
+    [CLSCompliant(false)]
+    public static void FromUInt32(uint value, Span<bool> bits)
+        => GetBits<uint, UInt32Vector>(value, bits.TrimLength(32));
+
+    /// <summary>
+    /// Extracts bits from 64-bit signed integer.
+    /// </summary>
+    /// <param name="value">The integer value.</param>
+    /// <param name="bits">The buffer for extracted bits.</param>
+    public static void FromInt64(long value, Span<bool> bits)
+        => GetBits<ulong, UInt64Vector>((ulong)value, bits.TrimLength(64));
+
+    /// <summary>
+    /// Extracts bits from 64-bit unsigned integer.
+    /// </summary>
+    /// <param name="value">The integer value.</param>
+    /// <param name="bits">The buffer for extracted bits.</param>
+    [CLSCompliant(false)]
+    public static void FromUInt64(ulong value, Span<bool> bits)
+        => GetBits<ulong, UInt64Vector>(value, bits.TrimLength(64));
 }
