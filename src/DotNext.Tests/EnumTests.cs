909c11927ac3fe52fcaea4e51ee5d3d378da57d6
sakno <roman.sakno@gmail.com>
2021-10-23 22:21:58 +0300

    Unify conversion behavior

--- src/DotNext.Tests/EnumTests.cs
+++ src/DotNext.Tests/EnumTests.cs
@@ -17,5 +17,23 @@ namespace DotNext
             Equal(EnvironmentVariableTarget.User, 1L.ToEnum<EnvironmentVariableTarget>());
             Equal(EnvironmentVariableTarget.Machine, ((byte)2).ToEnum<EnvironmentVariableTarget>());
         }
+
+        [Fact]
+        public static void NegativeValueConversion()
+        {
+            var expected = (EnvironmentVariableTarget)(-1);
+
+            Equal(-1, expected.ToSByte());
+            Throws<OverflowException>(() => expected.ToByte());
+
+            Equal(-1, expected.ToInt16());
+            Throws<OverflowException>(() => expected.ToUInt16());
+
+            Equal(-1, expected.ToInt32());
+            Throws<OverflowException>(() => expected.ToUInt32());
+
+            Equal(-1L, expected.ToInt64());
+            Throws<OverflowException>(() => expected.ToUInt64());
+        }
     }
 }
