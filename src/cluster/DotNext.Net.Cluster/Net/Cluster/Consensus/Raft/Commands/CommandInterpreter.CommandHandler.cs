e8730f93a8afbf6c4fa9a3f58c9c51bd5ad29ff0
sakno <roman.sakno@gmail.com>
2021-10-28 12:12:32 +0300

    Use ValueTask caching for hot path

--- src/cluster/DotNext.Net.Cluster/Net/Cluster/Consensus/Raft/Commands/CommandInterpreter.CommandHandler.cs
+++ src/cluster/DotNext.Net.Cluster/Net/Cluster/Consensus/Raft/Commands/CommandInterpreter.CommandHandler.cs
@@ -1,3 +1,5 @@
+using System.Runtime.CompilerServices;
+
 namespace DotNext.Net.Cluster.Consensus.Raft.Commands;
 
 using IO;
@@ -21,6 +23,7 @@ public partial class CommandInterpreter
             this.handler = handler;
         }
 
+        [AsyncMethodBuilder(typeof(PoolingAsyncValueTaskMethodBuilder))]
         internal override async ValueTask InterpretAsync<TReader>(TReader reader, CancellationToken token)
         {
             var command = await TCommand.ReadFromAsync(reader, token).ConfigureAwait(false);
