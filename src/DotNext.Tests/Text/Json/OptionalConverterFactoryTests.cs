9bed1da6ad5b97e855ad93487a4212705dc475ab
sakno <roman.sakno@gmail.com>
2021-11-19 00:47:20 +0200

    Added compatibility tests with JSON serialization source generator

--- src/DotNext.Tests/Text/Json/OptionalConverterFactoryTests.cs
+++ src/DotNext.Tests/Text/Json/OptionalConverterFactoryTests.cs
@@ -1,37 +1,23 @@
 using System.Diagnostics.CodeAnalysis;
 using System.Text.Json;
-using System.Text.Json.Serialization;
 
 namespace DotNext.Text.Json
 {
     [ExcludeFromCodeCoverage]
     public sealed class OptionalConverterFactoryTests : Test
     {
-        public sealed class JsonObject
-        {
-            [JsonConverter(typeof(OptionalConverterFactory))]
-            [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
-            public Optional<int> IntegerValue{ get; set; }
-
-            [JsonConverter(typeof(OptionalConverterFactory))]
-            [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
-            public Optional<string> StringField{ get; set; }
-
-            public bool BoolField{ get; set; }
-        }
-
         [Fact]
         public static void UndefinedValues()
         {
-            var expected = new JsonObject
+            var expected = new TestJsonObject
             {
                 IntegerValue = Optional.None<int>(),
                 StringField = Optional.None<string>(),
                 BoolField = true,
             };
 
-            var json = JsonSerializer.Serialize<JsonObject>(expected);
-            var actual = JsonSerializer.Deserialize<JsonObject>(json);
+            var json = JsonSerializer.Serialize<TestJsonObject>(expected);
+            var actual = JsonSerializer.Deserialize<TestJsonObject>(json);
             True(actual.IntegerValue.IsUndefined);
             True(actual.StringField.IsUndefined);
             True(actual.BoolField);
@@ -40,15 +26,15 @@ namespace DotNext.Text.Json
         [Fact]
         public static void NullValues()
         {
-            var expected = new JsonObject
+            var expected = new TestJsonObject
             {
                 IntegerValue = Optional.None<int>(),
                 StringField = Optional.Null<string>(),
                 BoolField = true,
             };
 
-            var json = JsonSerializer.Serialize<JsonObject>(expected);
-            var actual = JsonSerializer.Deserialize<JsonObject>(json);
+            var json = JsonSerializer.Serialize<TestJsonObject>(expected);
+            var actual = JsonSerializer.Deserialize<TestJsonObject>(json);
             True(actual.IntegerValue.IsUndefined);
             True(actual.StringField.IsNull);
             True(actual.BoolField);
@@ -57,15 +43,49 @@ namespace DotNext.Text.Json
         [Fact]
         public static void NormalValues()
         {
-            var expected = new JsonObject
+            var expected = new TestJsonObject
             {
                 IntegerValue = 10,
                 StringField = "Hello, world!",
                 BoolField = true,
             };
 
-            var json = JsonSerializer.Serialize<JsonObject>(expected);
-            var actual = JsonSerializer.Deserialize<JsonObject>(json);
+            var json = JsonSerializer.Serialize<TestJsonObject>(expected);
+            var actual = JsonSerializer.Deserialize<TestJsonObject>(json);
+            Equal(10, actual.IntegerValue.Value);
+            Equal("Hello, world!", actual.StringField.Value);
+            True(actual.BoolField);
+        }
+
+        [Fact]
+        public static void UndefinedValuesWithContext()
+        {
+            var expected = new TestJsonObject
+            {
+                IntegerValue = Optional.None<int>(),
+                StringField = Optional.None<string>(),
+                BoolField = true,
+            };
+
+            var json = JsonSerializer.Serialize<TestJsonObject>(expected, SerializationContext.Default.TestJsonObject);
+            var actual = JsonSerializer.Deserialize<TestJsonObject>(json);
+            True(actual.IntegerValue.IsUndefined);
+            True(actual.StringField.IsUndefined);
+            True(actual.BoolField);
+        }
+
+        [Fact]
+        public static void NormalValuesWithContext()
+        {
+            var expected = new TestJsonObject
+            {
+                IntegerValue = 10,
+                StringField = "Hello, world!",
+                BoolField = true,
+            };
+
+            var json = JsonSerializer.Serialize<TestJsonObject>(expected, SerializationContext.Default.TestJsonObject);
+            var actual = JsonSerializer.Deserialize<TestJsonObject>(json);
             Equal(10, actual.IntegerValue.Value);
             Equal("Hello, world!", actual.StringField.Value);
             True(actual.BoolField);
