4b309e60941929723e4667b3f8021847c8747267
sakno <roman.sakno@gmail.com>
2021-12-02 11:07:26 +0200

    Fixed trimming issues

--- src/cluster/DotNext.AspNetCore.Cluster/Net/Cluster/Consensus/Raft/Http/MetadataMessage.cs
+++ src/cluster/DotNext.AspNetCore.Cluster/Net/Cluster/Consensus/Raft/Http/MetadataMessage.cs
@@ -6,7 +6,6 @@ namespace DotNext.Net.Cluster.Consensus.Raft.Http;
 
 internal sealed class MetadataMessage : HttpMessage, IHttpMessageReader<MemberMetadata>, IHttpMessageWriter<MemberMetadata>
 {
-    private const JsonSerializerOptions? JsonOptions = null;
     internal new const string MessageType = "Metadata";
 
     internal MetadataMessage(in ClusterMemberId sender)
@@ -28,12 +27,12 @@ internal sealed class MetadataMessage : HttpMessage, IHttpMessageReader<MemberMe
     {
         var stream = await response.Content.ReadAsStreamAsync(token).ConfigureAwait(false);
         await using (stream.ConfigureAwait(false))
-            return await JsonSerializer.DeserializeAsync<MemberMetadata>(stream, JsonOptions, token).ConfigureAwait(false) ?? new MemberMetadata();
+            return await JsonSerializer.DeserializeAsync<MemberMetadata>(stream, MemberMetadata.TypeInfo, token).ConfigureAwait(false) ?? new MemberMetadata();
     }
 
     public Task SaveResponse(HttpResponse response, MemberMetadata metadata, CancellationToken token)
     {
         response.StatusCode = StatusCodes.Status200OK;
-        return JsonSerializer.SerializeAsync(response.Body, metadata, JsonOptions, token);
+        return JsonSerializer.SerializeAsync(response.Body, metadata, MemberMetadata.TypeInfo, token);
     }
 }
