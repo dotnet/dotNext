6df241eba8eb13c75f5c524905289c337e097343
sakno <roman.sakno@gmail.com>
2021-12-29 02:14:45 +0200

    Added Base64Encoder

--- src/DotNext.Tests/Text/Base64Tests.cs
+++ src/DotNext.Tests/Text/Base64Tests.cs
@@ -59,6 +59,27 @@ namespace DotNext.Text
             Equal(expected, actual.WrittenSpan.ToArray());
         }
 
+        [Theory]
+        [InlineData(17, 256)]
+        [InlineData(12, 256)]
+        [InlineData(32, 256)]
+        [InlineData(512, 1024)]
+        public static unsafe void DecodeBase64BytesToFunctionPointer(int chunkSize, int size)
+        {
+            var expected = RandomBytes(size);
+            var base64 = ToReadOnlySequence<byte>(Encoding.UTF8.GetBytes(Convert.ToBase64String(expected)), chunkSize);
+            var actual = new ArrayBufferWriter<byte>();
+            var decoder = new Base64Decoder();
+            foreach (var segment in base64)
+                decoder.Decode(segment.Span, &Callback, actual);
+            False(decoder.NeedMoreData);
+
+            Equal(expected, actual.WrittenSpan.ToArray());
+
+            static void Callback(ReadOnlySpan<byte> input, ArrayBufferWriter<byte> output)
+                => output.Write(input);
+        }
+
         [Fact]
         public static void DecodeInvalidBlock()
         {
@@ -75,7 +96,7 @@ namespace DotNext.Text
         [InlineData(12, 256)]
         [InlineData(32, 256)]
         [InlineData(512, 1024)]
-        public static void DecodeBase64CharsToByteBuffer(int chunkSize, int size)
+        public static void DecodeBase64CharsToBufferWriter(int chunkSize, int size)
         {
             var expected = RandomBytes(size);
             var base64 = ToReadOnlySequence<char>(Convert.ToBase64String(expected).AsMemory(), chunkSize);
@@ -119,6 +140,165 @@ namespace DotNext.Text
             False(decoder.NeedMoreData);
 
             Equal(expected, actual.WrittenSpan.ToArray());
+        }
+
+        [Theory]
+        [InlineData(25, 25)]
+        [InlineData(17, 256)]
+        [InlineData(12, 256)]
+        [InlineData(32, 256)]
+        [InlineData(512, 1024)]
+        public static unsafe void DecodeBase64CharsToFunctionPointer(int chunkSize, int size)
+        {
+            var expected = RandomBytes(size);
+            var base64 = ToReadOnlySequence<char>(Convert.ToBase64String(expected).AsMemory(), chunkSize);
+            var actual = new ArrayBufferWriter<byte>();
+            var decoder = new Base64Decoder();
+            foreach (var segment in base64)
+                decoder.Decode(segment.Span, &Callback, actual);
+            False(decoder.NeedMoreData);
+
+            Equal(expected, actual.WrittenSpan.ToArray());
+
+            static void Callback(ReadOnlySpan<byte> input, ArrayBufferWriter<byte> output)
+                => output.Write(input);
+        }
+
+        [Theory]
+        [InlineData(17)]
+        [InlineData(32)]
+        [InlineData(63)]
+        [InlineData(512)]
+        [InlineData(1024)]
+        public static void EncodeToBufferWriterUtf8(int size)
+        {
+            var expected = RandomBytes(size);
+
+            var encoder = new Base64Encoder();
+            False(encoder.HasBufferedData);
+            var writer = new ArrayBufferWriter<byte>();
+
+            encoder.EncodeToUtf8(expected.AsSpan(0, size / 2), writer, flush: false);
+            encoder.EncodeToUtf8(expected.AsSpan().Slice(size / 2), writer, flush: true);
+            Equal(0, encoder.BufferedDataSize);
+
+            var decoder = new Base64Decoder();
+            using var actual = decoder.Decode(writer.WrittenSpan);
+
+            Equal(expected, actual.Span.ToArray());
+        }
+
+        [Theory]
+        [InlineData(17)]
+        [InlineData(32)]
+        [InlineData(63)]
+        [InlineData(512)]
+        [InlineData(1024)]
+        public static void EncodeToBufferWriterChars(int size)
+        {
+            var expected = RandomBytes(size);
+
+            var encoder = new Base64Encoder();
+            var writer = new ArrayBufferWriter<char>();
+
+            encoder.EncodeToChars(expected.AsSpan(0, size / 2), writer, flush: false);
+            encoder.EncodeToChars(expected.AsSpan().Slice(size / 2), writer, flush: true);
+
+            var decoder = new Base64Decoder();
+            using var actual = decoder.Decode(writer.WrittenSpan);
+
+            Equal(expected, actual.Span.ToArray());
+        }
+
+        [Theory]
+        [InlineData(17)]
+        [InlineData(32)]
+        [InlineData(63)]
+        [InlineData(512)]
+        [InlineData(1024)]
+        public static void EncodeToBytes(int size)
+        {
+            var expected = RandomBytes(size);
+
+            var encoder = new Base64Encoder();
+            using var base64 = encoder.EncodeToUtf8(expected, flush: true);
+
+            var decoder = new Base64Decoder();
+            using var actual = decoder.Decode(base64.Span);
+
+            Equal(expected, actual.Span.ToArray());
+        }
+
+        [Theory]
+        [InlineData(17)]
+        [InlineData(32)]
+        [InlineData(63)]
+        [InlineData(512)]
+        [InlineData(1024)]
+        public static void EncodeToChars(int size)
+        {
+            var expected = RandomBytes(size);
+
+            var encoder = new Base64Encoder();
+            using var base64 = encoder.EncodeToChars(expected, flush: true);
+
+            var decoder = new Base64Decoder();
+            using var actual = decoder.Decode(base64.Span);
+
+            Equal(expected, actual.Span.ToArray());
+        }
+
+        [Fact]
+        public static void FlushToBytes()
+        {
+            var encoder = new Base64Encoder();
+            var writer = new ArrayBufferWriter<byte>();
+            byte[] expected = { 1, 2 };
+
+            encoder.EncodeToUtf8(expected, writer, flush: false);
+            True(encoder.HasBufferedData);
+
+            Span<byte> base64 = stackalloc byte[4];
+            Equal(2, encoder.GetBufferedData(base64));
+            Equal(expected, base64.Slice(0, 2).ToArray());
+
+            Equal(4, encoder.Flush(base64));
+        }
+
+        [Fact]
+        public static void FlushToChars()
+        {
+            var encoder = new Base64Encoder();
+            var writer = new ArrayBufferWriter<char>();
+            byte[] expected = { 1, 2 };
+
+            encoder.EncodeToChars(expected, writer, flush: false);
+            True(encoder.HasBufferedData);
+
+            Span<char> base64 = stackalloc char[4];
+            Equal(4, encoder.Flush(base64));
+        }
+
+        [Theory]
+        [InlineData(17)]
+        [InlineData(32)]
+        [InlineData(63)]
+        [InlineData(512)]
+        [InlineData(1024)]
+        public static void EncodeToStringBuilder(int size)
+        {
+            var expected = RandomBytes(size);
+
+            var encoder = new Base64Encoder();
+            var writer = new StringBuilder();
+
+            encoder.EncodeToChars(expected.AsSpan(0, size / 2), writer, flush: false);
+            encoder.EncodeToChars(expected.AsSpan().Slice(size / 2), writer, flush: true);
+
+            var decoder = new Base64Decoder();
+            using var actual = decoder.Decode(writer.ToString());
+
+            Equal(expected, actual.Span.ToArray());
         }
     }
 }
---
+++ src/DotNext/Text/Base64Encoder.Utf8.cs
@@ -0,0 +1,226 @@
+using System.Buffers;
+using System.Buffers.Text;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+
+namespace DotNext.Text;
+
+using Buffers;
+using StreamConsumer = IO.StreamConsumer;
+
+public partial struct Base64Encoder
+{
+    private void EncodeToUtf8Core<TWriter>(ReadOnlySpan<byte> bytes, ref TWriter writer, bool flush)
+        where TWriter : notnull, IBufferWriter<byte>
+    {
+        var produced = Base64.GetMaxEncodedToUtf8Length(bytes.Length);
+        var buffer = writer.GetSpan(produced);
+
+        switch (Base64.EncodeToUtf8(bytes, buffer, out var consumed, out produced, (bytes.Length % 3) == 0 || flush))
+        {
+            case OperationStatus.DestinationTooSmall or OperationStatus.Done:
+                reservedBufferSize = 0;
+                break;
+            case OperationStatus.NeedMoreData:
+                reservedBufferSize = bytes.Length - consumed;
+                Debug.Assert(reservedBufferSize <= MaxBufferedDataSize);
+                bytes.Slice(consumed).CopyTo(ReservedBytes);
+                break;
+        }
+
+        writer.Advance(produced);
+    }
+
+    [SkipLocalsInit]
+    private void CopyAndEncodeToUtf8<TWriter>(ReadOnlySpan<byte> bytes, ref TWriter writer, bool flush)
+        where TWriter : notnull, IBufferWriter<byte>
+    {
+        var newSize = reservedBufferSize + bytes.Length;
+        using var tempBuffer = (uint)newSize <= (uint)MemoryRental<byte>.StackallocThreshold ? stackalloc byte[newSize] : new MemoryRental<byte>(newSize);
+        ReservedBytes.Slice(0, reservedBufferSize).CopyTo(tempBuffer.Span);
+        bytes.CopyTo(tempBuffer.Span.Slice(reservedBufferSize));
+        EncodeToUtf8Core(tempBuffer.Span, ref writer, flush);
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private void EncodeToUtf8<TWriter>(ReadOnlySpan<byte> bytes, ref TWriter writer, bool flush)
+        where TWriter : notnull, IBufferWriter<byte>
+    {
+        Debug.Assert(bytes.Length <= MaxInputSize);
+
+        if (HasBufferedData)
+            CopyAndEncodeToUtf8(bytes, ref writer, flush);
+        else
+            EncodeToUtf8Core(bytes, ref writer, flush);
+    }
+
+    /// <summary>
+    /// Encodes a block of bytes to base64-encoded UTF-8 characters.
+    /// </summary>
+    /// <param name="bytes">A block of bytes to encode.</param>
+    /// <param name="output">The output buffer.</param>
+    /// <param name="flush">
+    /// <see langword="true"/> to encode the final block and insert padding if necessary;
+    /// <see langword="false"/> to encode a fragment without padding.
+    /// </param>
+    /// <exception cref="ArgumentNullException"><paramref name="output"/> is <see langword="null"/>.</exception>
+    /// <exception cref="ArgumentException">The length of <paramref name="bytes"/> is greater than <see cref="MaxInputSize"/>.</exception>
+    public void EncodeToUtf8(ReadOnlySpan<byte> bytes, IBufferWriter<byte> output, bool flush = false)
+    {
+        ArgumentNullException.ThrowIfNull(output);
+
+        if (bytes.Length > MaxInputSize)
+            throw new ArgumentException(ExceptionMessages.LargeBuffer, nameof(bytes));
+
+        EncodeToUtf8(bytes, ref output, flush);
+    }
+
+    /// <summary>
+    /// Encodes a block of bytes to base64-encoded UTF-8 characters.
+    /// </summary>
+    /// <param name="bytes">A block of bytes to encode.</param>
+    /// <param name="allocator">The allocator of the result buffer.</param>
+    /// <param name="flush">
+    /// <see langword="true"/> to encode the final block and insert padding if necessary;
+    /// <see langword="false"/> to encode a fragment without padding.
+    /// </param>
+    /// <returns>The buffer containing encoded bytes.</returns>
+    /// <exception cref="ArgumentException">The length of <paramref name="bytes"/> is greater than <see cref="MaxInputSize"/>.</exception>
+    public MemoryOwner<byte> EncodeToUtf8(ReadOnlySpan<byte> bytes, MemoryAllocator<byte>? allocator = null, bool flush = false)
+    {
+        if (bytes.Length > MaxInputSize)
+            throw new ArgumentException(ExceptionMessages.LargeBuffer, nameof(bytes));
+
+        var result = new MemoryOwnerWrapper<byte>(allocator);
+        EncodeToUtf8(bytes, ref result, flush);
+        return result.Buffer;
+    }
+
+    [SkipLocalsInit]
+    private void EncodeToUtf8Core<TConsumer>(ReadOnlySpan<byte> bytes, TConsumer output, bool flush)
+        where TConsumer : notnull, IReadOnlySpanConsumer<byte>
+    {
+        Span<byte> buffer = stackalloc byte[EncodingBufferSize];
+
+    consume_next_chunk:
+        switch (Base64.EncodeToUtf8(bytes, buffer, out var consumed, out var produced, (bytes.Length % 3) is 0))
+        {
+            case OperationStatus.DestinationTooSmall or OperationStatus.Done:
+                reservedBufferSize = 0;
+                break;
+            case OperationStatus.NeedMoreData:
+                reservedBufferSize = bytes.Length - consumed;
+                Debug.Assert(reservedBufferSize <= MaxBufferedDataSize);
+                bytes.Slice(consumed).CopyTo(ReservedBytes);
+                break;
+        }
+
+        if (produced > 0 && consumed > 0)
+        {
+            output.Invoke(buffer.Slice(0, produced));
+            bytes = bytes.Slice(consumed);
+            goto consume_next_chunk;
+        }
+
+        // flush the rest of the buffer
+        if (HasBufferedData && flush)
+        {
+            Base64.EncodeToUtf8(Span.AsReadOnlyBytes(in reservedBuffer).Slice(0, reservedBufferSize), buffer, out consumed, out produced);
+            reservedBufferSize = 0;
+            output.Invoke(buffer.Slice(0, produced));
+        }
+    }
+
+    [SkipLocalsInit]
+    private void CopyAndEncodeToUtf8<TConsumer>(ReadOnlySpan<byte> bytes, TConsumer output, bool flush)
+        where TConsumer : notnull, IReadOnlySpanConsumer<byte>
+    {
+        var newSize = reservedBufferSize + bytes.Length;
+        using var tempBuffer = (uint)newSize <= (uint)MemoryRental<byte>.StackallocThreshold ? stackalloc byte[newSize] : new MemoryRental<byte>(newSize);
+        ReservedBytes.Slice(0, reservedBufferSize).CopyTo(tempBuffer.Span);
+        bytes.CopyTo(tempBuffer.Span.Slice(reservedBufferSize));
+        EncodeToUtf8Core(tempBuffer.Span, output, flush);
+    }
+
+    /// <summary>
+    /// Encodes a block of bytes to base64-encoded UTF-8 characters.
+    /// </summary>
+    /// <typeparam name="TConsumer">The type of the consumer.</typeparam>
+    /// <param name="bytes">A block of bytes to encode.</param>
+    /// <param name="output">The consumer called for encoded portion of data.</param>
+    /// <param name="flush">
+    /// <see langword="true"/> to encode the final block and insert padding if necessary;
+    /// <see langword="false"/> to encode a fragment without padding.
+    /// </param>
+    public void EncodeToUtf8<TConsumer>(ReadOnlySpan<byte> bytes, TConsumer output, bool flush = false)
+        where TConsumer : notnull, IReadOnlySpanConsumer<byte>
+    {
+        if (HasBufferedData)
+            CopyAndEncodeToUtf8(bytes, output, flush);
+        else
+            EncodeToUtf8Core(bytes, output, flush);
+    }
+
+    /// <summary>
+    /// Encodes a block of bytes to base64-encoded UTF-8 characters.
+    /// </summary>
+    /// <typeparam name="TArg">The type of the argument to be passed to the callback.</typeparam>
+    /// <param name="bytes">A block of bytes to encode.</param>
+    /// <param name="output">The consumer called for encoded portion of data.</param>
+    /// <param name="arg">The argument to be passed to the callback.</param>
+    /// <param name="flush">
+    /// <see langword="true"/> to encode the final block and insert padding if necessary;
+    /// <see langword="false"/> to encode a fragment without padding.
+    /// </param>
+    public void EncodeToUtf8<TArg>(ReadOnlySpan<byte> bytes, ReadOnlySpanAction<byte, TArg> output, TArg arg, bool flush = false)
+        => EncodeToUtf8(bytes, new DelegatingReadOnlySpanConsumer<byte, TArg>(output, arg), flush);
+
+    /// <summary>
+    /// Encodes a block of bytes to base64-encoded UTF-8 characters.
+    /// </summary>
+    /// <typeparam name="TArg">The type of the argument to be passed to the callback.</typeparam>
+    /// <param name="bytes">A block of bytes to encode.</param>
+    /// <param name="output">The consumer called for encoded portion of data.</param>
+    /// <param name="arg">The argument to be passed to the callback.</param>
+    /// <param name="flush">
+    /// <see langword="true"/> to encode the final block and insert padding if necessary;
+    /// <see langword="false"/> to encode a fragment without padding.
+    /// </param>
+    [CLSCompliant(false)]
+    public unsafe void EncodeToUtf8<TArg>(ReadOnlySpan<byte> bytes, delegate*<ReadOnlySpan<byte>, TArg, void> output, TArg arg, bool flush = false)
+        => EncodeToUtf8(bytes, new ReadOnlySpanConsumer<byte, TArg>(output, arg), flush);
+
+    /// <summary>
+    /// Encodes a block of bytes to base64-encoded UTF-8 characters.
+    /// </summary>
+    /// <param name="bytes">A block of bytes to encode.</param>
+    /// <param name="output">The stream used as a destination for encoded data.</param>
+    /// <param name="flush">
+    /// <see langword="true"/> to encode the final block and insert padding if necessary;
+    /// <see langword="false"/> to encode a fragment without padding.
+    /// </param>
+    public void EncodeToUtf8(ReadOnlySpan<byte> bytes, Stream output, bool flush = false)
+        => EncodeToUtf8<StreamConsumer>(bytes, output, flush);
+
+    /// <summary>
+    /// Flushes the buffered data as base64-encoded UTF-8 characters to the output buffer.
+    /// </summary>
+    /// <param name="output">The output buffer of size 4.</param>
+    /// <returns>The number of written bytes.</returns>
+    public int Flush(Span<byte> output)
+    {
+        int bytesWritten;
+
+        if (reservedBufferSize is 0 || output.IsEmpty)
+        {
+            bytesWritten = 0;
+        }
+        else
+        {
+            Base64.EncodeToUtf8(Span.AsReadOnlyBytes(in reservedBuffer).Slice(0, reservedBufferSize), output, out var consumed, out bytesWritten);
+            reservedBufferSize -= consumed;
+        }
+
+        return bytesWritten;
+    }
+}
