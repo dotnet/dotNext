f3831c353beb86386ba0949e097a220a1e6e800c
sakno <roman.sakno@gmail.com>
2022-06-01 22:25:57 +0300

    Improved compatibility with init properties

--- src/cluster/DotNext.Net.Cluster/Net/Security/SslOptions.cs
+++ src/cluster/DotNext.Net.Cluster/Net/Security/SslOptions.cs
@@ -7,33 +7,44 @@ namespace DotNext.Net.Security;
 /// </summary>
 public class SslOptions
 {
+    private SslServerAuthenticationOptions? serverOptions;
+    private SslClientAuthenticationOptions? clientOptions;
+
     /// <summary>
     /// Initializes a new SSL options with preconfigured options for server and client.
     /// </summary>
     /// <param name="serverOptions">Server-side SSL options.</param>
     /// <param name="clientOptions">Client-side SSL options.</param>
     /// <exception cref="ArgumentNullException"><paramref name="clientOptions"/> or <paramref name="serverOptions"/> is <see langword="null"/>.</exception>
+    [Obsolete("Use init-only properties to initialize the instance")]
     public SslOptions(SslServerAuthenticationOptions serverOptions, SslClientAuthenticationOptions clientOptions)
     {
-        ServerOptions = serverOptions ?? throw new ArgumentNullException(nameof(serverOptions));
-        ClientOptions = clientOptions ?? throw new ArgumentNullException(nameof(clientOptions));
+        this.serverOptions = serverOptions ?? throw new ArgumentNullException(nameof(serverOptions));
+        this.clientOptions = clientOptions ?? throw new ArgumentNullException(nameof(clientOptions));
     }
 
     /// <summary>
     /// Initializes empty SSL options for server and client.
     /// </summary>
     public SslOptions()
-        : this(new SslServerAuthenticationOptions(), new SslClientAuthenticationOptions())
     {
     }
 
     /// <summary>
     /// Gets server-side options.
     /// </summary>
-    public SslServerAuthenticationOptions ServerOptions { get; }
+    public SslServerAuthenticationOptions ServerOptions
+    {
+        get => serverOptions ??= new();
+        init => serverOptions = value ?? throw new ArgumentNullException(nameof(value));
+    }
 
     /// <summary>
     /// Gets client-side options.
     /// </summary>
-    public SslClientAuthenticationOptions ClientOptions { get; }
+    public SslClientAuthenticationOptions ClientOptions
+    {
+        get => clientOptions ??= new();
+        init => clientOptions = value ?? throw new ArgumentNullException(nameof(value));
+    }
 }
