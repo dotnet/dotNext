af45da3b4038da610ead8f2c782e123b657f3a81
sakno <roman.sakno@gmail.com>
2022-06-20 21:16:50 +0300

    Excluded test infra from code coverage

--- src/DotNext.Tests/Net/Cluster/Consensus/Raft/Http/Startup.cs
+++ src/DotNext.Tests/Net/Cluster/Consensus/Raft/Http/Startup.cs
@@ -3,36 +3,35 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using System.Diagnostics.CodeAnalysis;
 
-namespace DotNext.Net.Cluster.Consensus.Raft.Http
+namespace DotNext.Net.Cluster.Consensus.Raft.Http;
+
+using Messaging;
+
+[ExcludeFromCodeCoverage]
+internal sealed class Startup
 {
-    using Messaging;
+    internal const string PersistentConfigurationPath = "persistentConfigPath";
 
-    [ExcludeFromCodeCoverage]
-    internal sealed class Startup
+    private readonly IConfiguration configuration;
+
+    public Startup(IConfiguration configuration) => this.configuration = configuration;
+
+    public void Configure(IApplicationBuilder app)
     {
-        internal const string PersistentConfigurationPath = "persistentConfigPath";
+        app.UseConsensusProtocolHandler();
+    }
 
-        private readonly IConfiguration configuration;
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddOptions()
+            .AddSingleton<IHttpMessageHandlerFactory, RaftClientHandlerFactory>()
+            .AddSingleton<IInputChannel, TestMessageHandler>()
+            .AddSingleton<IInputChannel, Mailbox>()
+            .AddSingleton<MetricsCollector, TestMetricsCollector>();
 
-        public Startup(IConfiguration configuration) => this.configuration = configuration;
+        var configPath = configuration[PersistentConfigurationPath];
 
-        public void Configure(IApplicationBuilder app)
-        {
-            app.UseConsensusProtocolHandler();
-        }
-
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddOptions()
-                .AddSingleton<IHttpMessageHandlerFactory, RaftClientHandlerFactory>()
-                .AddSingleton<IInputChannel, TestMessageHandler>()
-                .AddSingleton<IInputChannel, Mailbox>()
-                .AddSingleton<MetricsCollector, TestMetricsCollector>();
-
-            var configPath = configuration[PersistentConfigurationPath];
-
-            if (configPath is { Length: > 0 })
-                services.UsePersistentConfigurationStorage(configPath);
-        }
+        if (configPath is { Length: > 0 })
+            services.UsePersistentConfigurationStorage(configPath);
     }
 }
--- src/DotNext.Tests/TestCollections.cs
+++ src/DotNext.Tests/TestCollections.cs
@@ -1,5 +1,8 @@
+using System.Diagnostics.CodeAnalysis;
+
 namespace DotNext;
 
+[ExcludeFromCodeCoverage]
 internal static class TestCollections
 {
     internal const string Raft = "Raft";
