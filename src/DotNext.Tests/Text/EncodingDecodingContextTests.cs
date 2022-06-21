4e79ed1f30c47618d318619bb4cef804bcc90ffb
sakno <roman.sakno@gmail.com>
2022-06-21 19:30:14 +0300

    Added stream-based methods for encoding/decoding

--- src/DotNext.Tests/Text/EncodingDecodingContextTests.cs
+++ src/DotNext.Tests/Text/EncodingDecodingContextTests.cs
@@ -25,5 +25,48 @@ namespace DotNext.Text
             IsType<DecodingContext>(clone);
             Same(context.Encoding, ((DecodingContext)clone).Encoding);
         }
+
+        [Theory]
+        [InlineData("UTF-8", 1)]
+        [InlineData("UTF-8", 4)]
+        [InlineData("UTF-8", 8)]
+        [InlineData("UTF-8", 128)]
+        [InlineData("UTF-16LE", 1)]
+        [InlineData("UTF-16LE", 4)]
+        [InlineData("UTF-16LE", 8)]
+        [InlineData("UTF-16LE", 128)]
+        [InlineData("UTF-16BE", 1)]
+        [InlineData("UTF-16BE", 4)]
+        [InlineData("UTF-16BE", 8)]
+        [InlineData("UTF-16BE", 128)]
+        [InlineData("UTF-32LE", 1)]
+        [InlineData("UTF-32LE", 4)]
+        [InlineData("UTF-32LE", 8)]
+        [InlineData("UTF-32LE", 128)]
+        [InlineData("UTF-32BE", 1)]
+        [InlineData("UTF-32BE", 4)]
+        [InlineData("UTF-32BE", 8)]
+        [InlineData("UTF-32BE", 128)]
+        public static void EncodeDecode(string encodingName, int charBufferSize)
+        {
+            const string value = "Hello, world!&*(@&*(fghjwgfwffgw Привет, мир!";
+
+            var encoding = Encoding.GetEncoding(encodingName);
+            using var ms = new MemoryStream();
+
+            // encode
+            var bytesBuffer = new byte[16];
+            foreach (var chunk in new EncodingContext(encoding, true).GetBytes(value.AsMemory(), bytesBuffer))
+                ms.Write(chunk.Span);
+
+            // decode
+            var sb = new StringBuilder(charBufferSize);
+            var charBuffer = new char[charBufferSize];
+
+            foreach (var chunk in new DecodingContext(encoding, true).GetChars(ms.ToArray(), charBuffer))
+                sb.Append(chunk);
+
+            Equal(value, sb.ToString());
+        }
     }
 }
