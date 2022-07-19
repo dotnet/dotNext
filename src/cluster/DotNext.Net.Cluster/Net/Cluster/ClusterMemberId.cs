b19d0aacf7a7d9b552d40a8549bff850f5d7e396
sakno <roman.sakno@gmail.com>
2022-07-20 00:11:52 +0300

    Code cleanup

--- src/cluster/DotNext.Net.Cluster/Net/Cluster/ClusterMemberId.cs
+++ src/cluster/DotNext.Net.Cluster/Net/Cluster/ClusterMemberId.cs
@@ -3,7 +3,6 @@ using System.Net;
 using System.Runtime.InteropServices;
 using Microsoft.AspNetCore.Connections;
 using static System.Buffers.Binary.BinaryPrimitives;
-using AddressFamily = System.Net.Sockets.AddressFamily;
 
 namespace DotNext.Net.Cluster;
 
