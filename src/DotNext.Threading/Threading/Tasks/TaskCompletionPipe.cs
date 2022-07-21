2684549497a17149a7c716cfe955e0fa5eaf87b9
sakno <roman.sakno@gmail.com>
2022-07-21 23:48:18 +0300

    Code cleanup

--- src/DotNext.Threading/Threading/Tasks/TaskCompletionPipe.cs
+++ src/DotNext.Threading/Threading/Tasks/TaskCompletionPipe.cs
@@ -1,7 +1,6 @@
 using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
 using System.Runtime.CompilerServices;
-using static System.Threading.Timeout;
 
 namespace DotNext.Threading.Tasks;
 
