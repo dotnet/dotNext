a7ef47984e308efb5b6db83db138c104081f8f21
sakno <roman.sakno@gmail.com>
2022-07-31 02:04:44 +0300

    Revert "Removed deprecated benchmark"
    This reverts commit dea8175c206430b1e5879cb60cb9599ad1e9dbcd.

---
+++ src/DotNext.Benchmarks/HexConversionBenchmark.cs
@@ -0,0 +1,54 @@
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Code;
+using BenchmarkDotNet.Engines;
+using BenchmarkDotNet.Order;
+using System;
+using System.Collections.Generic;
+using static System.Globalization.CultureInfo;
+
+namespace DotNext;
+
+[SimpleJob(runStrategy: RunStrategy.Throughput, launchCount: 1)]
+[Orderer(SummaryOrderPolicy.Declared)]
+public class HexConversionBenchmark
+{
+    public readonly struct ByteArrayParam : IParam
+    {
+        public readonly byte[] Value;
+
+        internal ByteArrayParam(byte[] bytes) => Value = bytes;
+
+        object IParam.Value => Value;
+
+        string IParam.DisplayText => ToString();
+
+        string IParam.ToSourceCode() => $"new byte[{Value.LongLength}];";
+
+        public override string ToString() => $"{Value.LongLength.ToString(InvariantCulture)} bytes";
+    }
+
+    [ParamsSource(nameof(RandomArrays))]
+    public ByteArrayParam Bytes;
+
+    public static IEnumerable<ByteArrayParam> RandomArrays
+    {
+        get
+        {
+            byte[] bytes;
+            Random.Shared.NextBytes(bytes = new byte[16]);
+            yield return new ByteArrayParam(bytes);
+            Random.Shared.NextBytes(bytes = new byte[64]);
+            yield return new ByteArrayParam(bytes);
+            Random.Shared.NextBytes(bytes = new byte[128]);
+            yield return new ByteArrayParam(bytes);
+            Random.Shared.NextBytes(bytes = new byte[256]);
+            yield return new ByteArrayParam(bytes);
+        }
+    }
+
+    [Benchmark(Description = "BitConverter.ToString")]
+    public string ToHexUsingBitConverter() => BitConverter.ToString(Bytes.Value);
+
+    [Benchmark(Description = "Span.ToHex")]
+    public string ToHexUsingSpanConverter() => Span.ToHex(Bytes.Value);
+}
