8f8950763876f1a09cc4beb548ec688b67bcb603
sakno <roman.sakno@gmail.com>
2022-11-26 23:03:56 +0200

    Fix: logger source gen now supports file-scoped ns

--- src/cluster/DotNext.AspNetCore.Cluster/Net/Cluster/Consensus/Raft/Http/LogMessages.cs
+++ src/cluster/DotNext.AspNetCore.Cluster/Net/Cluster/Consensus/Raft/Http/LogMessages.cs
@@ -1,43 +1,42 @@
 using System.Net;
 using Microsoft.Extensions.Logging;
 
-namespace DotNext.Net.Cluster.Consensus.Raft.Http
+namespace DotNext.Net.Cluster.Consensus.Raft.Http;
+
+internal static partial class LogMessages
 {
-    internal static partial class LogMessages
-    {
-        private const string EventIdPrefix = "DotNext.AspNetCore.Cluster";
-        private const int EventIdOffset = 75000;
+    private const string EventIdPrefix = "DotNext.AspNetCore.Cluster";
+    private const int EventIdOffset = 75000;
 
-        [LoggerMessage(
-            EventIdOffset,
-            LogLevel.Debug,
-            "Sending request of type {MessageType} to member {Member}",
-            EventName = EventIdPrefix + "." + nameof(SendingRequestToMember)
-        )]
-        public static partial void SendingRequestToMember(this ILogger logger, EndPoint member, string messageType);
+    [LoggerMessage(
+        EventIdOffset,
+        LogLevel.Debug,
+        "Sending request of type {MessageType} to member {Member}",
+        EventName = EventIdPrefix + "." + nameof(SendingRequestToMember)
+    )]
+    public static partial void SendingRequestToMember(this ILogger logger, EndPoint member, string messageType);
 
-        [LoggerMessage(
-            EventIdOffset + 1,
-            LogLevel.Warning,
-            "Cluster member {Member} is unavailable",
-            EventName = EventIdPrefix + "." + nameof(MemberUnavailable)
-        )]
-        public static partial void MemberUnavailable(this ILogger logger, EndPoint member, Exception e);
+    [LoggerMessage(
+        EventIdOffset + 1,
+        LogLevel.Warning,
+        "Cluster member {Member} is unavailable",
+        EventName = EventIdPrefix + "." + nameof(MemberUnavailable)
+    )]
+    public static partial void MemberUnavailable(this ILogger logger, EndPoint member, Exception e);
 
-        [LoggerMessage(
-            EventIdOffset + 2,
-            LogLevel.Error,
-            "An unhandled exception has occurred while executing the request",
-            EventName = EventIdPrefix + "." + nameof(UnhandledException)
-        )]
-        public static partial void UnhandledException(this ILogger logger, Exception e);
+    [LoggerMessage(
+        EventIdOffset + 2,
+        LogLevel.Error,
+        "An unhandled exception has occurred while executing the request",
+        EventName = EventIdPrefix + "." + nameof(UnhandledException)
+    )]
+    public static partial void UnhandledException(this ILogger logger, Exception e);
 
-        [LoggerMessage(
-            EventIdOffset + 3,
-            LogLevel.Information,
-            "Failed to route message {MessageName} to leader node",
-            EventName = EventIdPrefix + "." + nameof(FailedToRouteMessage)
-        )]
-        public static partial void FailedToRouteMessage(this ILogger logger, string messageName, Exception e);
-    }
+    [LoggerMessage(
+        EventIdOffset + 3,
+        LogLevel.Information,
+        "Failed to route message {MessageName} to leader node",
+        EventName = EventIdPrefix + "." + nameof(FailedToRouteMessage)
+    )]
+    public static partial void FailedToRouteMessage(this ILogger logger, string messageName, Exception e);
 }
