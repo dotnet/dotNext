dea474d6c43cc7ecf0437a3a59920dec0e5c08fb
sakno <roman.sakno@gmail.com>
2023-03-17 17:06:22 +0200

    Correctly handle conversion of null values

--- src/DotNext.Tests/OptionalTests.cs
+++ src/DotNext.Tests/OptionalTests.cs
@@ -269,5 +269,45 @@ namespace DotNext
             output = input.Flatten();
             True(output.HasValue);
         }
+
+        [Fact]
+        public static void ConvertNoneToValueType()
+        {
+            var value = Optional<int>.None.Convert<string>(static i => i.ToString());
+            False(value.HasValue);
+            False(value.IsNull);
+        }
+
+        [Fact]
+        public static void ConvertValueTypeToValueType()
+        {
+            var value = new Optional<int>(42).Convert<int>(static i => i + 1);
+            True(value.HasValue);
+            Equal(43, value.Value);
+        }
+
+        [Fact]
+        public static unsafe void ConvertNullToValueType()
+        {
+            var value = new Optional<string>(null).Convert<int>(&int.Parse);
+            False(value.HasValue);
+            False(value.IsNull);
+        }
+
+        [Fact]
+        public static void ConvertNullToRefType()
+        {
+            var value = new Optional<string>(null).Convert<string>(Converter.Identity<string, string>());
+            False(value.HasValue);
+            True(value.IsNull);
+        }
+
+        [Fact]
+        public static void ConvertNoneToRefType()
+        {
+            var value = Optional<string>.None.Convert<string>(Converter.Identity<string, string>());
+            False(value.HasValue);
+            False(value.IsNull);
+        }
     }
 }
