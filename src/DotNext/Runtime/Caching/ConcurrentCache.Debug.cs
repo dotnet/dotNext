d68dcef6a7164b3246cca11b70a1281247830e7a
sakno <roman.sakno@gmail.com>
2023-05-23 20:35:41 +0300

    Improved debugging experience

---
+++ src/DotNext/Runtime/Caching/ConcurrentCache.Debug.cs
@@ -0,0 +1,36 @@
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+
+namespace DotNext.Runtime.Caching;
+
+[DebuggerDisplay($"Count = {{{nameof(Count)}}}, CommandQueueSize = {{{nameof(CommandQueueSize)}}}, CommandPoolSize = {{{nameof(CommandPoolSize)}}}")]
+public partial class ConcurrentCache<TKey, TValue>
+{
+    [ExcludeFromCodeCoverage]
+    private int CommandQueueSize
+    {
+        get
+        {
+            var result = 0;
+
+            for (var current = commandQueueReadPosition.Next; current is not null; current = current.Next)
+                result++;
+
+            return result;
+        }
+    }
+
+    [ExcludeFromCodeCoverage]
+    private int CommandPoolSize
+    {
+        get
+        {
+            var result = 0;
+
+            for (var current = pooledCommand; current is not null; current = current.Next)
+                result++;
+
+            return result;
+        }
+    }
+}
