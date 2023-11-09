384127b113a76040bdccc99677c7de67771fd519
sakno <roman.sakno@gmail.com>
2023-11-09 20:49:55 +0200

    Added task for migration

--- src/DotNext.Threading/Threading/AsyncTrigger.cs
+++ src/DotNext.Threading/Threading/AsyncTrigger.cs
@@ -306,7 +306,7 @@ public class AsyncTrigger : QueuedSynchronizer, IAsyncEvent
     private struct ConditionalLockManager<TCondition> : ILockManager<DefaultWaitNode>
         where TCondition : notnull, ISupplier<bool>
     {
-        internal TCondition Condition;
+        internal TCondition Condition; // TODO: Change to required in C# 12
 
         bool ILockManager.IsLockAllowed => Condition.Invoke();
 
