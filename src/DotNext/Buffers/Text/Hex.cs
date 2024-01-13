6211e4a490b976e7ec4931354d3580503bf4698c
sakno <roman.sakno@gmail.com>
2024-01-13 13:20:24 +0200

    Fixed test failure caused by Code Coverage reporter

--- src/DotNext/Buffers/Text/Hex.cs
+++ src/DotNext/Buffers/Text/Hex.cs
@@ -13,6 +13,7 @@ public static partial class Hex
 {
     private const byte NibbleMaxValue = 0B1111;
 
+    [ExcludeFromCodeCoverage]
     private static ReadOnlySpan<char> NibbleToUtf16CharLookupTable => ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'];
 
     private static ReadOnlySpan<byte> CharToNibbleLookupTable =>
