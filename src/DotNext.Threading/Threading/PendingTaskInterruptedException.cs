9cebe25aee73ab8ed7dfd2937e9dd5cac8c59a94
sakno <roman.sakno@gmail.com>
2024-01-15 10:42:13 +0200

    Migration to primary ctor

--- src/DotNext.Threading/Threading/PendingTaskInterruptedException.cs
+++ src/DotNext.Threading/Threading/PendingTaskInterruptedException.cs
@@ -5,18 +5,10 @@ namespace DotNext.Threading;
 /// it is in waiting state.
 /// </summary>
 /// <seealso cref="ThreadInterruptedException"/>
-public class PendingTaskInterruptedException : Exception
+/// <param name="message">The error message that explains the reason for the exception.</param>
+/// <param name="innerException">The exception that is the cause of the current exception.</param>
+public class PendingTaskInterruptedException(string? message = null, Exception? innerException = null) : Exception(message ?? ExceptionMessages.AsyncTaskInterrupted, innerException)
 {
-    /// <summary>
-    /// Initializes a new exception.
-    /// </summary>
-    /// <param name="message">The error message that explains the reason for the exception.</param>
-    /// <param name="innerException">The exception that is the cause of the current exception.</param>
-    public PendingTaskInterruptedException(string? message = null, Exception? innerException = null)
-        : base(message ?? ExceptionMessages.AsyncTaskInterrupted, innerException)
-    {
-    }
-
     /// <summary>
     /// Gets the reason for lock steal.
     /// </summary>
