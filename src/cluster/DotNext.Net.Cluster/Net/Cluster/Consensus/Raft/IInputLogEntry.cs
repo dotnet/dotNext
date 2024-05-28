2014007d79aaa86ce3d22362c41f4a44c8c576b3
sakno <roman.sakno@gmail.com>
2024-05-28 08:34:56 +0300

    Improved docs

--- src/cluster/DotNext.Net.Cluster/Net/Cluster/Consensus/Raft/IInputLogEntry.cs
+++ src/cluster/DotNext.Net.Cluster/Net/Cluster/Consensus/Raft/IInputLogEntry.cs
@@ -8,5 +8,10 @@ public interface IInputLogEntry : IRaftLogEntry
     /// <summary>
     /// Gets or sets runtime context associated with the log entry.
     /// </summary>
+    /// <remarks>
+    /// The value passes through <see cref="IO.Log.IAuditTrail{TEntry}.AppendAsync{TEntryImpl}(TEntryImpl, CancellationToken)"/>
+    /// to <see cref="MemoryBasedStateMachine.ApplyAsync(PersistentState.LogEntry)"/> or <see cref="DiskBasedStateMachine.ApplyAsync(PersistentState.LogEntry)"/>.
+    /// It can be retrieved by using <see cref="PersistentState.LogEntry.Context"/> property.
+    /// </remarks>
     object? Context { get; init; }
 }
