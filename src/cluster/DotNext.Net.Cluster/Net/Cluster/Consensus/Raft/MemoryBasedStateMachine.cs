f7926a451b9a76bc0c006c2fc802151d87ee49af
sakno <roman.sakno@gmail.com>
2024-05-29 20:34:47 +0300

    Improved docs

--- src/cluster/DotNext.Net.Cluster/Net/Cluster/Consensus/Raft/MemoryBasedStateMachine.cs
+++ src/cluster/DotNext.Net.Cluster/Net/Cluster/Consensus/Raft/MemoryBasedStateMachine.cs
@@ -666,6 +666,13 @@ public abstract partial class MemoryBasedStateMachine : PersistentState
     /// <summary>
     /// Applies the command represented by the log entry to the underlying database engine.
     /// </summary>
+    /// <remarks>
+    /// <see cref="PersistentState.LogEntry.Context"/> can be used to pass custom data
+    /// through WAL processing pipeline. The value of this property is not persistent.
+    /// The data can be passed as a part of log entry which is used as
+    /// the argument of <see cref="PersistentState.AppendAsync{TEntry}(TEntry, CancellationToken)"/> method.
+    /// The passed log entry type must implement <see cref="IInputLogEntry"/> interface.
+    /// </remarks>
     /// <param name="entry">The entry to be applied to the state machine.</param>
     /// <returns>The task representing asynchronous execution of this method.</returns>
     /// <seealso cref="Commands.CommandInterpreter"/>
