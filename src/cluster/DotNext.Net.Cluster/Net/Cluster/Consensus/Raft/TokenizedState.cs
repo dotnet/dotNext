e4a0a96c35e64749d49263d091a821afeb23c245
sakno <roman.sakno@gmail.com>
2024-07-25 17:16:21 +0300

    Removed unused classes

--- src/cluster/DotNext.Net.Cluster/Net/Cluster/Consensus/Raft/TokenizedState.cs
+++
@@ -1,7 +0,0 @@
-namespace DotNext.Net.Cluster.Consensus.Raft;
-
-internal abstract class TokenizedState<TMember>(IRaftStateMachine<TMember> stateMachine) : RaftState<TMember>(stateMachine)
-    where TMember : class, IRaftClusterMember
-{
-    public abstract CancellationToken Token { get; }
-}
