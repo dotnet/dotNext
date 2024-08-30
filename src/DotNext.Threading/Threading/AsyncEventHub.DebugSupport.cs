ca144b16838ffa0771f03ac08a8dcbae3ae8f8b6
sakno <roman.sakno@gmail.com>
2024-08-30 13:53:35 +0300

    Migration to ValueTask

--- src/DotNext.Threading/Threading/AsyncEventHub.DebugSupport.cs
+++ src/DotNext.Threading/Threading/AsyncEventHub.DebugSupport.cs
@@ -1,8 +1,7 @@
 using System.ComponentModel;
 using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
+using DotNext.Numerics;
 
 namespace DotNext.Threading;
 
@@ -17,25 +16,28 @@ public partial class AsyncEventHub
 
         public DebugView(AsyncEventHub hub)
         {
-            hub.CaptureState(States = new bool[hub.Count]);
+            var state = hub.CaptureState();
+            Span<bool> flags = stackalloc bool[MaxCount];
+            state.Mask.GetBits(flags);
+            States = flags[..hub.Count].ToArray();
         }
     }
 
     /// <summary>
     /// Captures the state of the events.
     /// </summary>
-    /// <remarks>
-    /// Each element of the buffer will be modified with the state of the event matching
-    /// to the index of the element.
-    /// </remarks>
-    /// <param name="states">A buffer of states to fill.</param>
+    /// <returns>A group of signaled events.</returns>
     [EditorBrowsable(EditorBrowsableState.Advanced)]
-    public void CaptureState(Span<bool> states)
+    public EventGroup CaptureState()
     {
-        lock (sources)
+        ObjectDisposedException.ThrowIf(IsDisposingOrDisposed, this);
+
+        EventGroup result;
+        lock (SyncRoot)
         {
-            for (var i = 0; i < Math.Min(states.Length, Count); i++)
-                Unsafe.Add(ref MemoryMarshal.GetReference(states), i) = Unsafe.Add(ref MemoryMarshal.GetArrayDataReference(sources), i).Task.IsCompleted;
+            result = new(state);
         }
+
+        return result;
     }
 }
--- src/DotNext.Threading/Threading/AsyncExclusiveLock.cs
+++ src/DotNext.Threading/Threading/AsyncExclusiveLock.cs
@@ -1,5 +1,4 @@
 ﻿using System.Diagnostics;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 namespace DotNext.Threading;
