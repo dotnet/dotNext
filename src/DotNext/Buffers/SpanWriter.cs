e3e617af0112c2c782da162f0148ec2421fe64e8
sakno <roman.sakno@gmail.com>
2024-12-02 17:39:53 +0200

    Fixed typo

--- src/DotNext/Buffers/SpanWriter.cs
+++ src/DotNext/Buffers/SpanWriter.cs
@@ -120,7 +120,7 @@ public ref struct SpanWriter<T>
     public void Reset() => position = 0;
 
     /// <summary>
-    /// Gets the span over written elements.
+    /// Gets the span overwritten elements.
     /// </summary>
     /// <value>The segment of underlying span containing written elements.</value>
     public readonly Span<T> WrittenSpan => MemoryMarshal.CreateSpan(ref reference, position);
@@ -164,7 +164,7 @@ public ref struct SpanWriter<T>
     /// </summary>
     /// <param name="item">The item to place.</param>
     /// <returns>
-    /// <see langword="true"/> if item has beem placed successfully;
+    /// <see langword="true"/> if item has been placed successfully;
     /// <see langword="false"/> if remaining space in the underlying span is not enough to place the item.
     /// </returns>
     public bool TryAdd(T item)
