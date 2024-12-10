03d2d4d8b61c904f73378400db603880c35e7da2
sakno <roman.sakno@gmail.com>
2024-12-10 15:16:32 +0200

    Improved test coverage

--- src/DotNext.Tests/Net/EndPointFormatterTests.cs
+++ src/DotNext.Tests/Net/EndPointFormatterTests.cs
@@ -12,14 +12,17 @@ public sealed class EndPointFormatterTests : Test
 {
     public static TheoryData<EndPoint, IEqualityComparer<EndPoint>> GetTestEndPoints()
     {
+        var ip = IPAddress.Parse("192.168.0.1");
         var rows = new TheoryData<EndPoint, IEqualityComparer<EndPoint>>();
         rows.Add(new DnsEndPoint("host", 3262), EqualityComparer<EndPoint>.Default);
-        rows.Add(new IPEndPoint(IPAddress.Parse("192.168.0.1"), 3263), EqualityComparer<EndPoint>.Default);
+        rows.Add(new IPEndPoint(ip, 3263), EqualityComparer<EndPoint>.Default);
         rows.Add(new IPEndPoint(IPAddress.Parse("2001:0db8:0000:0000:0000:8a2e:0370:7334"), 3264), EqualityComparer<EndPoint>.Default);
         rows.Add(new HttpEndPoint("192.168.0.1", 3262, true, AddressFamily.InterNetwork), EqualityComparer<EndPoint>.Default);
         rows.Add(new HttpEndPoint("192.168.0.1", 3262, false, AddressFamily.InterNetwork), EqualityComparer<EndPoint>.Default);
         rows.Add(new HttpEndPoint("2001:0db8:0000:0000:0000:8a2e:0370:7334", 3262, true, AddressFamily.InterNetworkV6),
             EqualityComparer<EndPoint>.Default);
+        rows.Add(new HttpEndPoint(ip, 8080, false), EqualityComparer<EndPoint>.Default);
+        rows.Add(new HttpEndPoint(new IPEndPoint(ip, 8080), false), EqualityComparer<EndPoint>.Default);
         rows.Add(new HttpEndPoint("host", 3262, true), EqualityComparer<EndPoint>.Default);
         rows.Add(new HttpEndPoint("host", 3262, false), EqualityComparer<EndPoint>.Default);
         rows.Add(new UriEndPoint(new Uri("http://host:3262/")), EndPointFormatter.UriEndPointComparer);
