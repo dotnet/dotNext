8d5af4673c183be14e2a4ff37c1403836dafeb09
sakno <roman.sakno@gmail.com>
2025-04-21 12:28:08 +0300

    Fixed typo

--- src/DotNext/Sentinel.cs
+++ src/DotNext/Sentinel.cs
@@ -4,7 +4,7 @@ namespace DotNext;
 
 internal static class Sentinel
 {
-    // instance has fixed address in memory which is critical to ValueReference implementation
+    // instance has a fixed address in memory which is critical to ValueReference implementation
     internal static readonly object Instance = GC.AllocateUninitializedArray<Dummy>(length: 0, pinned: true);
 
     // The struct is needed to avoid false positives caused by the type check on the variable that stores singleton
