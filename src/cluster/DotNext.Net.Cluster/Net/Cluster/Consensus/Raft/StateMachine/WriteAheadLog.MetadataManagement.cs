74452a15c8e9851f0425616dfaf574e90eca494e
sakno <roman.sakno@gmail.com>
2025-05-19 20:06:55 +0300

    Removed redundant branching

--- src/cluster/DotNext.Net.Cluster/Net/Cluster/Consensus/Raft/StateMachine/WriteAheadLog.MetadataManagement.cs
+++ src/cluster/DotNext.Net.Cluster/Net/Cluster/Consensus/Raft/StateMachine/WriteAheadLog.MetadataManagement.cs
@@ -27,19 +27,12 @@ partial class WriteAheadLog
         public uint GetEndPageIndex(long index)
             => GetPageIndex((ulong)index * LogEntryMetadata.AlignedSize + LogEntryMetadata.AlignedSize, out _);
 
-        private Span<byte> TryGetMetadata(long index)
-        {
-            var pageIndex = GetStartPageIndex(index, out var offset);
-            return TryGetValue(pageIndex, out var page)
-                ? page.GetSpan().Slice(offset)
-                : Span<byte>.Empty;
-        }
-
         internal bool TryGetMetadata(long index, out LogEntryMetadata metadata)
         {
-            if (TryGetMetadata(index) is { Length: > 0 } metadataSpan)
+            var pageIndex = GetStartPageIndex(index, out var offset);
+            if (TryGetValue(pageIndex, out var page))
             {
-                metadata = new(metadataSpan);
+                metadata = new(page.GetSpan().Slice(offset));
                 return true;
             }
 
@@ -47,17 +40,18 @@ partial class WriteAheadLog
             return false;
         }
 
+        private Span<byte> GetMetadata(long index)
+        {
+            var pageIndex = GetStartPageIndex(index, out var offset);
+            return TryGetValue(pageIndex, out var page)
+                ? page.GetSpan().Slice(offset)
+                : throw new ArgumentOutOfRangeException(nameof(index));
+        }
+
         public LogEntryMetadata this[long index]
         {
-            get => TryGetMetadata(index) is { Length: > 0 } metadata
-                ? new(metadata)
-                : throw new ArgumentOutOfRangeException(nameof(index));
-
-            set
-            {
-                var page = GetOrAdd(GetStartPageIndex(index, out var offset));
-                value.Format(page.GetSpan().Slice(offset));
-            }
+            get => new(GetMetadata(index));
+            set => value.Format(GetMetadata(index));
         }
     }
 }
