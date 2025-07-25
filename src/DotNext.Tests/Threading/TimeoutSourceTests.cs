98ed1a6934714c3dd630c9571ae29beb845be2cb
sakno <roman.sakno@gmail.com>
2025-07-25 14:32:36 +0300

    Introduced reusable timeout source

---
+++ src/DotNext.Tests/Threading/TimeoutSourceTests.cs
@@ -0,0 +1,40 @@
+namespace DotNext.Threading;
+
+public sealed class TimeoutSourceTests : Test
+{
+    [Fact]
+    public static void ImmediateCancellation()
+    {
+        using var source = new TimeoutSource(TimeProvider.System, new(canceled: true));
+        True(source.IsCanceled);
+        False(source.IsTimedOut);
+        True(source.Token.IsCancellationRequested);
+        False(source.TryStart(DefaultTimeout));
+        False(source.TryReset());
+    }
+
+    [Fact]
+    public static async Task ImmediateTimeout()
+    {
+        await using var source = new TimeoutSource(TimeProvider.System, new(canceled: false));
+        True(source.TryStart(TimeSpan.Zero));
+
+        await source.Token.WaitAsync();
+        True(source.IsTimedOut);
+        False(source.TryReset());
+    }
+
+    [Fact]
+    public static void Cancellation()
+    {
+        using var cts = new CancellationTokenSource();
+        using var source = new TimeoutSource(TimeProvider.System, cts.Token);
+        False(source.IsCanceled);
+        False(source.IsTimedOut);
+        
+        cts.Cancel();
+        True(source.Token.IsCancellationRequested);
+        True(source.IsCanceled);
+        False(source.IsTimedOut);
+    }
+}
