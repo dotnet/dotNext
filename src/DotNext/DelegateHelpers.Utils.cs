7243aa65c00fc3844f8183d4e8dcfdc0df9aee43
sakno <roman.sakno@gmail.com>
2025-09-11 12:19:11 +0300

    Removed tail call

--- src/DotNext/DelegateHelpers.Utils.cs
+++ src/DotNext/DelegateHelpers.Utils.cs
@@ -20,22 +20,22 @@ public static partial class DelegateHelpers
         object? ISupplier<Delegate, object?>.Invoke(Delegate d) => d.Target;
     }
 
-    private static TDelegate ChangeType<TDelegate, TRewriter>(this Delegate d, TRewriter rewriter)
-        where TDelegate : Delegate
+    private static TDelegate ChangeType<TDelegate, TRewriter>(this Delegate d, TRewriter rewriter) where TDelegate : Delegate
         where TRewriter : struct, ISupplier<Delegate, object?>
     {
-        var list = d.GetInvocationList();
-        if (list is [var singleDelegate])
-            return ReferenceEquals(singleDelegate, d)
-                ? d.Method.CreateDelegate<TDelegate>(rewriter.Invoke(d))
-                : ChangeType<TDelegate, TRewriter>(singleDelegate, rewriter);
+        if (d.GetInvocationList() is not [_, ..] list)
+        {
+            return d.Method.CreateDelegate<TDelegate>(rewriter.Invoke(d));
+        }
 
         // We use untyped CreateDelegate to avoid typecast inside the loop.
         // Also, it's reasonable to reuse already allocated invocation list to store
         // newly created delegates because Delegate.Combine accepts array only
         var delegateType = typeof(TDelegate);
         foreach (ref var sub in list.AsSpan())
+        {
             sub = sub.Method.CreateDelegate(delegateType, rewriter.Invoke(sub));
+        }
 
         return (TDelegate)Delegate.Combine(list)!;
     }
