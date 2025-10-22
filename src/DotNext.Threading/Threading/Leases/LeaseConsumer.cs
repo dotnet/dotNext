a87533ad7a5ebafba2fb413f3bdd5166faff5df5
sakno <roman.sakno@gmail.com>
2025-10-22 13:20:06 +0300

    Fixed propagation of the token

--- src/DotNext.Threading/Threading/Leases/LeaseConsumer.cs
+++ src/DotNext.Threading/Threading/Leases/LeaseConsumer.cs
@@ -210,9 +210,14 @@ public abstract class LeaseConsumer : Disposable, IAsyncDisposable
         {
             throw new AggregateException(exception, e);
         }
-        catch (OperationCanceledException e) when (e.CausedBy(cts, leaseToken))
+        catch (OperationCanceledException e) when (e.CancellationToken == leaseToken ||
+                                                   e.CancellationToken == cts?.Token && cts.CancellationOrigin == leaseToken)
         {
             throw new TimeoutException(ExceptionMessages.LeaseExpired, e);
+        }
+        catch (OperationCanceledException e) when (cts is not null)
+        {
+            throw new OperationCanceledException(e.Message, e, cts.CancellationOrigin);
         }
         finally
         {
