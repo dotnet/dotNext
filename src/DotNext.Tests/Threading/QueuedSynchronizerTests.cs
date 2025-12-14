f71ec8036f240d9eb0b0caa8e2a9932e2665ff23
sakno <roman.sakno@gmail.com>
2025-12-14 21:20:42 +0200

    Improved test coverage

--- src/DotNext.Tests/Threading/QueuedSynchronizerTests.cs
+++ src/DotNext.Tests/Threading/QueuedSynchronizerTests.cs
@@ -22,4 +22,64 @@ public sealed class QueuedSynchronizerTests : Test
         protected override Exception GetAcquisitionException(bool canAcquire)
             => canAcquire ? null : new ArithmeticException();
     }
+
+    [Fact]
+    public static async Task ResumeReadLockAsync()
+    {
+        using var synchronizer = new CustomReaderWriterLock();
+        await synchronizer.EnterReadLockAsync(TestToken);
+
+        using var cts = CancellationTokenSource.CreateLinkedTokenSource(TestToken);
+        var writeLockTask = synchronizer.EnterWriteLockAsync(cts.Token).AsTask();
+        False(writeLockTask.IsCompleted);
+
+        var readLockTask = synchronizer.EnterReadLockAsync(TestToken).AsTask();
+
+        await cts.CancelAsync();
+        await readLockTask;
+    }
+    
+    private sealed class CustomReaderWriterLock : QueuedSynchronizer<bool>
+    {
+        private uint readLocks;
+        private bool writeLockTaken;
+
+        protected override bool CanAcquire(bool writeLock)
+        {
+            if (writeLockTaken)
+                return false;
+
+            return !writeLock || readLocks is 0U;
+        }
+
+        public ValueTask EnterWriteLockAsync(CancellationToken token)
+            => AcquireAsync(true, token);
+
+        public ValueTask EnterReadLockAsync(CancellationToken token)
+            => AcquireAsync(false, token);
+
+        protected override void AcquireCore(bool writeLock)
+        {
+            if (writeLock)
+            {
+                writeLockTaken = true;
+            }
+            else
+            {
+                readLocks++;
+            }
+        }
+
+        protected override void ReleaseCore(bool writeLock)
+        {
+            if (writeLock)
+            {
+                writeLockTaken = false;
+            }
+            else
+            {
+                readLocks--;
+            }
+        }
+    }
 }
