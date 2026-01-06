af290a8021750d32c1a883daf392394c1192acff
sakno <roman.sakno@gmail.com>
2026-01-06 22:08:08 +0200

    Fixed regression

--- src/DotNext.Tests/Threading/ReaderWriterSpinLockTests.cs
+++ src/DotNext.Tests/Threading/ReaderWriterSpinLockTests.cs
@@ -27,24 +27,6 @@ public sealed class ReaderWriterSpinLockTests : Test
     }
 
     [Fact]
-    public static void OptimisticRead()
-    {
-        var rwLock = new ReaderWriterSpinLock();
-        var stamp = rwLock.TryOptimisticRead();
-        True(rwLock.Validate(in stamp));
-        True(rwLock.TryEnterReadLock());
-        Equal(1, rwLock.CurrentReadCount);
-        True(rwLock.Validate(stamp));
-        rwLock.ExitReadLock();
-        Equal(stamp, rwLock.TryOptimisticRead());
-        True(rwLock.TryEnterWriteLock());
-        False(rwLock.IsReadLockHeld);
-        True(rwLock.IsWriteLockHeld);
-        False(rwLock.Validate(stamp));
-        False(rwLock.TryEnterReadLock(TimeSpan.Zero, TestToken));
-    }
-
-    [Fact]
     public static async Task WriterToWriterChain()
     {
         var are = new TaskCompletionSource();
