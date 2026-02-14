f097cd62876af53db9f6230bdb2404ea97427580
sakno <roman.sakno@gmail.com>
2026-02-14 09:31:40 +0200

    Added utf-8 formatting and parsing

--- src/DotNext/Numerics/Enum.Formatting.cs
+++ src/DotNext/Numerics/Enum.Formatting.cs
@@ -34,4 +34,31 @@ partial struct Enum<T>
             return Unsafe.BitCast<T, TValue>(value).TryFormat(destination, out charsWritten, format, provider);
         }
     }
+    
+    /// <inheritdoc/>
+    public bool TryFormat(Span<byte> destination, out int charsWritten, ReadOnlySpan<char> format, IFormatProvider? provider)
+    {
+        return UnderlyingType switch
+        {
+            TypeCode.Byte => ConstrainedCall<byte>(value, destination, out charsWritten, format, provider),
+            TypeCode.SByte => ConstrainedCall<sbyte>(value, destination, out charsWritten, format, provider),
+            TypeCode.UInt16 => ConstrainedCall<ushort>(value, destination, out charsWritten, format, provider),
+            TypeCode.Int16 => ConstrainedCall<short>(value, destination, out charsWritten, format, provider),
+            TypeCode.UInt32 => ConstrainedCall<uint>(value, destination, out charsWritten, format, provider),
+            TypeCode.Int32 => ConstrainedCall<int>(value, destination, out charsWritten, format, provider),
+            TypeCode.UInt64 => ConstrainedCall<ulong>(value, destination, out charsWritten, format, provider),
+            TypeCode.Int64 => ConstrainedCall<long>(value, destination, out charsWritten, format, provider),
+            _ => EnumHelpers.Fail(out charsWritten)
+        };
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        static bool ConstrainedCall<TValue>(T value, Span<byte> destination, out int charsWritten, ReadOnlySpan<char> format,
+            IFormatProvider? provider)
+            where TValue : unmanaged, IUtf8SpanFormattable
+        {
+            AssertUnderlyingType<TValue>();
+            
+            return Unsafe.BitCast<T, TValue>(value).TryFormat(destination, out charsWritten, format, provider);
+        }
+    }
 }
