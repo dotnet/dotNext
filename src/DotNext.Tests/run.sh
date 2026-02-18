ccb669bf3a5facb5609b515c8d335c9b66e81e2a
sakno <roman.sakno@gmail.com>
2026-02-18 22:58:48 +0200

    Setup test timeout

--- src/DotNext.Tests/run.sh
+++
@@ -1,3 +0,0 @@
-# dotnet tool install -g dotnet-reportgenerator-globaltool
-# reportgenerator -reports:"coverage.info" -targetdir:"output"
-dotnet test --collect:"XPlat Code Coverage" /p:CollectCoverage=true /p:CoverletOutputFormat=lcov /p:CoverletOutput=./TestResults/lcov.info
