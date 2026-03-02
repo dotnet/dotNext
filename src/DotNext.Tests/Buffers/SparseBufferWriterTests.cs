b3f91bf1c321b7720ec18a793aa629a59b68e5b4
sakno <roman.sakno@gmail.com>
2026-03-02 10:44:13 +0200

    Fixed test coverage

--- src/DotNext.Tests/Buffers/SparseBufferWriterTests.cs
+++ src/DotNext.Tests/Buffers/SparseBufferWriterTests.cs
@@ -93,21 +93,37 @@ public sealed class SparseBufferWriterTests : Test
     }
 
     [Fact]
+    public static void CopyToBuffer()
+    {
+        using var writer = new SparseBufferWriter<int>(chunkSize: 16);
+        writer.Write(Enumerable.Range(0, 16).ToArray());
+        writer.Write(Enumerable.Range(16, 16).ToArray());
+
+        var buffer = new ArrayBufferWriter<int>();
+        writer.CopyTo(Write, buffer);
+
+        Equal(writer.Concat().ToArray(), buffer.WrittenMemory);
+
+        static void Write(ReadOnlySpan<int> span, ArrayBufferWriter<int> buffer)
+            => buffer.Write(span);
+    }
+
+    [Fact]
     public static void CopyChunksToStream()
     {
         using var writer = new SparseBufferWriter<byte>(chunkSize: 16);
-        writer.Write(new byte[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 });
+        writer.Write([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]);
 
         var middle = writer.End;
-        writer.Write(new byte[] { 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31 });
+        writer.Write([16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31]);
 
         using var dest = new MemoryStream(capacity: 32);
-        writer.CopyTo<IO.StreamConsumer>(dest, default(SequencePosition));
+        writer.CopyTo<StreamConsumer>(dest, default);
         Equal(new byte[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31 }, dest.ToArray());
 
         dest.Position = 0L;
         dest.SetLength(0L);
-        Equal(2L, writer.CopyTo<IO.StreamConsumer>(dest, ref middle, 2L));
+        Equal(2L, writer.CopyTo<StreamConsumer>(dest, ref middle, 2L));
         Equal(new byte[] { 16, 17 }, dest.ToArray());
     }
 
