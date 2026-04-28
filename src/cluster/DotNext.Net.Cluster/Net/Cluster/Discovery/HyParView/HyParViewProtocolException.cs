56ff666a77aa696d00435879df2abbabe6f5c10e
sakno <roman.sakno@gmail.com>
2026-04-29 01:06:07 +0300

    Removed Serializable attribute

--- src/cluster/DotNext.Net.Cluster/Net/Cluster/Discovery/HyParView/HyParViewProtocolException.cs
+++ src/cluster/DotNext.Net.Cluster/Net/Cluster/Discovery/HyParView/HyParViewProtocolException.cs
@@ -4,7 +4,4 @@ namespace DotNext.Net.Cluster.Discovery.HyParView;
 /// Represents violation of Raft protocol.
 /// </summary>
 /// <param name="message">Human-readable text describing problem.</param>
-[Serializable]
-public class HyParViewProtocolException(string message) : DiscoveryProtocolException(message)
-{
-}
+public class HyParViewProtocolException(string message) : DiscoveryProtocolException(message);
