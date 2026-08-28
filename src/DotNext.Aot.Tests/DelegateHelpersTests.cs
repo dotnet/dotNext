8b89146554d3996f9a269053fbc4a73687f09fdc
sakno <roman.sakno@gmail.com>
2026-08-28 17:59:00 +0300

    Added AOT compatibility tests

---
+++ src/DotNext.Aot.Tests/DelegateHelpersTests.cs
@@ -0,0 +1,14 @@
+﻿namespace DotNext;
+
+public class DelegateHelpersTests : Assert
+{
+    [Fact]
+    public static unsafe void CreateAction()
+    {
+        Throws<PlatformNotSupportedException>(static () => Action.FromPointer(&DoAction));
+
+        static void DoAction()
+        {
+        }
+    }
+}
