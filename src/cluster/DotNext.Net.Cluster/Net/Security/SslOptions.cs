472707b286325ee45389fedf7c18df4582d0e16c
sakno <roman.sakno@gmail.com>
2026-09-04 16:53:24 +0300

    Added customized TLS options for every Raft client

--- src/cluster/DotNext.Net.Cluster/Net/Security/SslOptions.cs
+++ src/cluster/DotNext.Net.Cluster/Net/Security/SslOptions.cs
@@ -1,3 +1,4 @@
+using System.Net;
 using System.Net.Security;
 
 namespace DotNext.Net.Security;
@@ -31,4 +32,12 @@ public class SslOptions
         get => field ??= new();
         init => field = value ?? throw new ArgumentNullException(nameof(value));
     }
+
+    /// <summary>
+    /// Gets client-side options for the specified server address.
+    /// </summary>
+    /// <param name="serverAddress">The address of the server.</param>
+    /// <returns>The client-side options.</returns>
+    public virtual SslClientAuthenticationOptions CreateClientOptions(EndPoint serverAddress)
+        => ClientOptions;
 }
