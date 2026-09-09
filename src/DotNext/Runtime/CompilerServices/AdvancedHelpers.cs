d4d0a3ee7b9a3e11211db460382edd1dd1f17ae0
sakno <roman.sakno@gmail.com>
2026-09-09 22:10:02 +0300

    Use unsigned offset

--- src/DotNext/Runtime/CompilerServices/AdvancedHelpers.cs
+++ src/DotNext/Runtime/CompilerServices/AdvancedHelpers.cs
@@ -26,8 +26,9 @@ public static partial class AdvancedHelpers
     private static unsafe ref readonly byte Advance<T>(this ref readonly byte address, scoped ref nuint length)
         where T : unmanaged, allows ref struct
     {
-        length -= (nuint)sizeof(T);
-        return ref Unsafe.Add(ref Unsafe.AsRef(in address), sizeof(T));
+        nuint size = (uint)sizeof(T);
+        length -= size;
+        return ref Unsafe.Add(ref Unsafe.AsRef(in address), size);
     }
 
     internal static unsafe bool IsZero(ref readonly byte address, nuint length)
