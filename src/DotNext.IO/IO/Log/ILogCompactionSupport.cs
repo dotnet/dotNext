8a4e12a98e5e4a996422604956b0b9d4b412e773
sakno <roman.sakno@gmail.com>
2026-09-12 10:21:31 +0300

    Fixed typo

--- src/DotNext.IO/IO/Log/ILogCompactionSupport.cs
+++ src/DotNext.IO/IO/Log/ILogCompactionSupport.cs
@@ -9,7 +9,7 @@ public interface ILogCompactionSupport : IAuditTrail
     /// <summary>
     /// Forces compaction of committed log entries.
     /// </summary>
-    /// <param name="token">Tje token that can be used to cancel the operation.</param>
+    /// <param name="token">The token that can be used to cancel the operation.</param>
     /// <returns>The task representing asynchronous execution of the operation.</returns>
     /// <exception cref="OperationCanceledException">The operation has been canceled.</exception>
     ValueTask ForceCompactionAsync(CancellationToken token);
